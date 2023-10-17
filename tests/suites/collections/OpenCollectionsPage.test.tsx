3d8a046f7023e443f0b5b07eeb38ac91024f8046
Yevhenii Chernovol <echernovol@yandex-team.ru>
2023-10-17 18:22:44 +0200

    (tests.suites) add OpenCollectionsPage test

---
+++ tests/suites/collections/OpenCollectionsPage.test.tsx
@@ -0,0 +1,10 @@
+import {Page} from '@playwright/test';
+
+import {openTestPage} from '../../utils';
+import datalensTest from '../../utils/playwright/globalTestDefinition';
+
+datalensTest.describe('Collections page', () => {
+    datalensTest('Open page', async ({page}: {page: Page}) => {
+        await openTestPage(page, `/collections`);
+    });
+});
