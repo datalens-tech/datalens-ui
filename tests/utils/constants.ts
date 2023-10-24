69d5bfcf147477c17a898b42e959ca30fdbebfd5
Elena Martynova <marginy@yandex-team.ru>
2023-10-24 16:38:31 +0300

    Remove DashEditPanelEnabled flag usage from code (#104)

--- tests/utils/constants.ts
+++ tests/utils/constants.ts
@@ -185,7 +185,6 @@ export const COMMON_SELECTORS = {
     ACTION_PANEL_SAVE_AS_MENU: 'action-save-menu',
     ACTION_BTN_CONNECTIONS: 'action-button-connections',
     ACTION_BTN_TABS: 'action-button-tabs',
-    ACTION_BTN_ADD: 'action-button-add',
     DASH_ENTRY_NAME: 'dash-entry-name',
     ENTRY_CONTEXT_MENU_KEY: 'entry-context-menu',
     REVISIONS_LIST: 'revisions-list',
