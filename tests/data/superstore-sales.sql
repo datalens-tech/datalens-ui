5be5a74a4d63bbb30077135b9169e64b83c571e9
Alexandr Yushkov <yeee737@nebius.com>
2023-10-25 12:40:06 +0200

    Postgres mock data for e2e tests (#105)

---
+++ tests/data/superstore-sales.sql
@@ -0,0 +1,58 @@
+--
+-- The sample data used in the superstore-sales database is taken from https://www.kaggle.com
+-- which is licensed under the Open Data Commons Public Domain Dedication and License (PDDL) v1.0
+--
+
+BEGIN;
+
+SET client_encoding = 'UTF8';
+SET datestyle = dmy;
+
+CREATE TABLE sales (
+    id integer NOT NULL,
+    order_id TEXT NOT NULL,
+    order_date DATE NOT NULL,
+    ship_date DATE NOT NULL,
+    ship_mode TEXT NOT NULL,
+    customer_id TEXT NOT NULL,
+    customer_name TEXT NOT NULL,
+    segment TEXT NOT NULL,
+    country TEXT NOT NULL,
+    city TEXT NOT NULL,
+    state TEXT NOT NULL,
+    postal_code TEXT,
+    region TEXT NOT NULL,
+    product_id TEXT NOT NULL,
+    category TEXT NOT NULL,
+    sub_category TEXT NOT NULL,
+    product_name TEXT NOT NULL,
+    sales TEXT NOT NULL 
+);
+
+COPY sales (
+    id,
+    order_id,
+    order_date,
+    ship_date,
+    ship_mode,
+    customer_id,
+    customer_name,
+    segment,
+    country,
+    city,
+    state,
+    postal_code,
+    region,
+    product_id,
+    category,
+    sub_category,
+    product_name,
+    sales
+)
+FROM './superstore_sales.csv' DELIMITER ',' CSV HEADER;
+
+ALTER TABLE ONLY sales ADD CONSTRAINT sales_pkey PRIMARY KEY (id);
+
+COMMIT;
+
+ANALYZE sales;
