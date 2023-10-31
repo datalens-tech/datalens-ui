b4d6983244dd4c22d3300d04a556acdaeff57a95
Alexander <yeee737@nebius.com>
2023-10-31 14:10:48 +0100

    Better naming

--- scripts/e2e/us-dump.sh
+++ scripts/e2e/us-dump.sh
@@ -11,15 +11,15 @@ DUMP_TMP="/tmp/us-dump-$RANDOM"
 echo "United Storage dump process"
 echo
 
-US_ID=$(docker ps -aqf name="$US_DOCKER_CONTAINER_NAME")
+US_CONTAINER_ID=$(docker ps -aqf name="$US_DOCKER_CONTAINER_NAME")
 
-if [ -z $US_ID ]; then
+if [ -z $US_CONTAINER_ID ]; then
     echo "Failed to locate $US_DOCKER_CONTAINER_NAME docker container id."
     exit 1
 fi
 
 CONNECTION_CHECK_CMD="pg_isready --dbname=$US_CONNECTION"
-CONNECTION_CHECK_RESULT=$(docker exec -it $US_ID /bin/sh -c "$CONNECTION_CHECK_CMD")
+CONNECTION_CHECK_RESULT=$(docker exec -it $US_CONTAINER_ID /bin/sh -c "$CONNECTION_CHECK_CMD")
 CONNECTION_CHECK_RESULT_STATUS_CODE=$?
 
 if [ $CONNECTION_CHECK_RESULT_STATUS_CODE -ne 0 ]; then
@@ -29,7 +29,7 @@ if [ $CONNECTION_CHECK_RESULT_STATUS_CODE -ne 0 ]; then
 fi
 
 DUMP_CMD="pg_dump --dbname=$US_CONNECTION --column-inserts -a -t workbooks -t entries -t revisions -t links"
-docker exec -it $US_ID /bin/sh -c "$DUMP_CMD" > $DUMP_TMP
+docker exec -it $US_CONTAINER_ID /bin/sh -c "$DUMP_CMD" > $DUMP_TMP
 DUMP_RESULT_STATUS_CODE=$?
 
 if [ $DUMP_RESULT_STATUS_CODE -ne 0 ]; then
