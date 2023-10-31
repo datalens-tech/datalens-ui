e74b4e04c3a0fdc809950cc0ec8187606d3a1b3b
Flunt1k <chvp00@gmail.com>
2023-10-31 16:07:44 +0100

    add helpers

--- tests/utils/opensource/helpers.ts
+++ tests/utils/opensource/helpers.ts
@@ -1,7 +1,8 @@
 import {Page} from '@playwright/test';
 import {DEFAULT_WORKBOOK_ID} from './constants/common';
 import {generateQueryString, goto, slct} from '../index';
-import {DialogCreateWorkbookEntryQa} from '../../../src/shared';
+import {ActionPanelQA, DialogCreateWorkbookEntryQa, EntryDialogQA} from '../../../src/shared';
+import {ActionPanelEntryContextMenuQa} from '../../../src/shared/constants/qa/action-panel';
 
 export const openOpensourceTestPage = async (
     page: Page,
@@ -20,14 +21,43 @@ export const openOpensourceTestPage = async (
     await goto(page, fullUrl, {isRetry: false});
 };
 
-export const deleteWorkbookEntry = () => {};
+export const saveWorkbookEntry = async (page: Page, saveButtonQa: string, entryName: string) => {
+    await page.waitForSelector(slct(saveButtonQa));
 
-export const saveWorkbookEntry = async (page: Page, entryName: string) => {
+    await page.click(slct(saveButtonQa));
+
     await page.waitForSelector(slct(DialogCreateWorkbookEntryQa.Root));
 
     await page.fill(`${slct(DialogCreateWorkbookEntryQa.Input)} input`, entryName);
 
+    const promise = page.waitForURL(() => true, {waitUntil: 'networkidle'});
+
     await page.click(slct(DialogCreateWorkbookEntryQa.ApplyButton));
 
-    await page.waitForURL(() => true, {waitUntil: 'networkidle'});
+    await promise;
+};
+
+export const deleteWorkbookEntry = async (page: Page) => {
+    await page.waitForSelector(slct(ActionPanelQA.MoreBtn));
+    await page.locator(slct(ActionPanelQA.MoreBtn)).click();
+
+    await page.waitForSelector(
+        `${slct(ActionPanelEntryContextMenuQa.Menu)} ${slct(ActionPanelEntryContextMenuQa.Remove)}`,
+    );
+
+    await page
+        .locator(
+            `${slct(ActionPanelEntryContextMenuQa.Menu)} ${slct(
+                ActionPanelEntryContextMenuQa.Remove,
+            )}`,
+        )
+        .click();
+
+    await page.waitForSelector(slct(EntryDialogQA.Apply));
+
+    const promise = page.waitForURL(() => true, {waitUntil: 'networkidle'});
+
+    await page.click(slct(EntryDialogQA.Apply));
+
+    await promise;
 };
