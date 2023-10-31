84ae76eccbb9229aacdc39b664c427c1e71dd2b8
Alexander <yeee737@nebius.com>
2023-10-31 14:04:18 +0100

    review fixes

--- scripts/e2e/us-dump.sh
+++ scripts/e2e/us-dump.sh
@@ -5,7 +5,8 @@ US_DB_PASS="us"
 US_DB_NAME="us-db-ci_purgeable"
 US_DB_PORT=5432
 US_DUMP_PATH="./tests/data/us-e2e-data"
-CONNECTION="postgres://$US_DB_USER:$US_DB_PASS@$US_DB_HOST:$US_DB_PORT/$US_DB_NAME"
+US_CONNECTION="postgres://$US_DB_USER:$US_DB_PASS@$US_DB_HOST:$US_DB_PORT/$US_DB_NAME"
+DUMP_TMP="/tmp/us-dump-$RANDOM"
 
 echo "United Storage dump process"
 echo
@@ -17,26 +18,27 @@ if [ -z $US_ID ]; then
     exit 1
 fi
 
-CONNECTION_CHECK_CMD="pg_isready --dbname=$CONNECTION"
+CONNECTION_CHECK_CMD="pg_isready --dbname=$US_CONNECTION"
 CONNECTION_CHECK_RESULT=$(docker exec -it $US_ID /bin/sh -c "$CONNECTION_CHECK_CMD")
-CONNECTION_CHECK_STATUS_CODE=$?
+CONNECTION_CHECK_RESULT_STATUS_CODE=$?
 
-if [ $CONNECTION_CHECK_STATUS_CODE -ne 0 ]; then
+if [ $CONNECTION_CHECK_RESULT_STATUS_CODE -ne 0 ]; then
     echo "Failed to establish database connection."
     echo $CONNECTION_CHECK_RESULT
     exit 1
 fi
 
-DUMP_CMD="pg_dump --dbname=$CONNECTION --column-inserts -a -t workbooks -t entries -t revisions -t links"
-DUMP_RESULT=$(docker exec -it $US_ID /bin/sh -c "$DUMP_CMD")
+DUMP_CMD="pg_dump --dbname=$US_CONNECTION --column-inserts -a -t workbooks -t entries -t revisions -t links"
+docker exec -it $US_ID /bin/sh -c "$DUMP_CMD" > $DUMP_TMP
 DUMP_RESULT_STATUS_CODE=$?
 
 if [ $DUMP_RESULT_STATUS_CODE -ne 0 ]; then
     echo "Failed to dump database."
-    echo $DUMP_RESULT
+    cat $DUMP_TMP
+    rm $DUMP_TMP
     exit 1
 fi
 
-echo $DUMP_RESULT > $US_DUMP_PATH
+mv $DUMP_TMP $US_DUMP_PATH
 echo "The dump was succesfully created in $US_DUMP_PATH."
 exit 0
