6f24bb929d9931579fd5162adf9f488f34fb8432
Artem Panchuk <artemipanchuk@gmail.com>
2023-11-08 13:35:12 +0300

    Lint fix

--- src/server/modes/charts/plugins/ql/preparers/line-time.ts
+++ src/server/modes/charts/plugins/ql/preparers/line-time.ts
@@ -258,7 +258,7 @@ export default ({
             ++colorGroup.size;
         });
 
-        if (shared.extraSettings.legendMode !== 'hide') {
+        if (shared.extraSettings?.legendMode !== 'hide') {
             ChartEditor.updateLibraryConfig({
                 legend: {
                     show: true,
