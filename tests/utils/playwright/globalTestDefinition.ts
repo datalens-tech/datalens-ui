261b0098392d7437933e3ba746a36eee54309aa1
Vladimir Chernitsyn <flunt1k@yandex-team.ru>
2023-11-08 11:54:59 +0100

    Add parametrization for e2e tests (#159)

--- tests/utils/playwright/globalTestDefinition.ts
+++ tests/utils/playwright/globalTestDefinition.ts
@@ -1,11 +1,15 @@
 import {test as baseTest} from '@playwright/test';
 
 import {generateScreenshotName, makeScreen} from './utils';
+import {TestParametrizationConfig} from '../../types/config';
+import {config} from '../../constants/config';
 
 export type DatalensTestFixtures = {
     makeScreenshot: void;
     isSmokeTest: boolean;
     screenshotName?: string;
+
+    config: TestParametrizationConfig;
 };
 
 const datalensTest = baseTest.extend<DatalensTestFixtures>({
@@ -36,6 +40,7 @@ const datalensTest = baseTest.extend<DatalensTestFixtures>({
             auto: true,
         },
     ],
+    config: [config, {option: true}],
 });
 
 export default datalensTest;
