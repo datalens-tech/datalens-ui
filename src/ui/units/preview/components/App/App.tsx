014bddba22dfed32d77fc0c6d984de0138e41249
mournfulCoroner <leutinatasya@yandex-team.ru>
2023-11-16 11:12:27 +0300

    Support param `_no_scroll` for embedded dash and preview (#200)
    * Support param _no_scroll for embedded dash and preview
    
    * Remove extra code
    
    * Use block for body classes

--- src/ui/units/preview/components/App/App.tsx
+++ src/ui/units/preview/components/App/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import block from 'bem-cn-lite';
 import once from 'lodash/once';
 import {connect} from 'react-redux';
 import {Route, RouteComponentProps, Switch, withRouter} from 'react-router-dom';
@@ -7,13 +8,18 @@ import {Dispatch, bindActionCreators} from 'redux';
 import {setCurrentPageEntry} from 'store/actions/asideHeader';
 import {DatalensGlobalState} from 'ui';
 import {MobileHeader} from 'ui/components/MobileHeader/MobileHeader';
+import Utils from 'ui/utils/utils';
 
 import {getIsAsideHeaderEnabled} from '../../../../components/AsideHeaderAdapter';
-import {isEmbeddedMode, isIframe} from '../../../../utils/embedded';
+import {isEmbeddedMode, isNoScrollMode} from '../../../../utils/embedded';
 import IndexPage from '../IndexPage/IndexPage';
 import Preview from '../Preview/Preview';
 
 import './App.scss';
+import 'ui/styles/preview.scss';
+
+const b = block('app');
+const previewBlock = block('dl-preview');
 
 type StateProps = ReturnType<typeof mapStateToProps>;
 type DispatchProps = ReturnType<typeof mapDispatchToProps>;
@@ -23,18 +29,28 @@ interface Props extends RouteComponentProps, StateProps, DispatchProps {}
 const App: React.FunctionComponent<Props> = (props) => {
     const isAsideHeaderEnabled = getIsAsideHeaderEnabled();
 
-    const embedded = isIframe() || isEmbeddedMode();
+    React.useEffect(() => {
+        const previewClasses = previewBlock({'no-scroll': true}).split(' ');
+
+        if (isNoScrollMode()) {
+            Utils.addBodyClass(...previewClasses);
+        }
+
+        return () => {
+            Utils.removeBodyClass(...previewClasses);
+        };
+    }, []);
 
     return (
-        <div className="app">
-            {embedded || isAsideHeaderEnabled ? null : <MobileHeader />}
+        <div className={b()}>
+            {isEmbeddedMode() || isAsideHeaderEnabled ? null : <MobileHeader />}
             <Switch>
                 <Route
                     path={`/preview/:idOrSource+`}
                     render={(routeProps) => (
                         <Preview
                             {...routeProps}
-                            isEmbedded={Boolean(embedded)}
+                            isEmbedded={isEmbeddedMode()}
                             asideHeaderSize={props.asideHeaderData.size}
                             setPageEntry={once(props.setCurrentPageEntry)}
                         />
