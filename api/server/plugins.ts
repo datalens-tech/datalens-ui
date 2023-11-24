0731f13f25f19116a96fee061835f6d110910d5e
Andrey Melikhov <melikhov@yandex-team.ru>
2023-11-24 14:07:18 +0300

    Add extending API (#240)
    * Add server extending API

---
+++ api/server/plugins.ts
@@ -0,0 +1,9 @@
+export {
+    configuredDashApiPlugin,
+    dashApiValidation,
+} from '../../src/server/modes/charts/plugins/dash-api';
+export {plugin as ql} from '../../src/server/modes/charts/plugins/ql';
+export {configurableRequestWithDatasetPlugin} from '../../src/server/modes/charts/plugins/request-with-dataset';
+export {plugin as loginsBlacklist} from '../../src/server/modes/charts/plugins/logins-blacklist';
+export {configurableRequestWithDatasetPlugin} from '../../src/server/modes/charts/plugins/request-with-dataset';
+export {plugin as runnerKeyAdapter} from '../../src/server/modes/charts/plugins/runner-key-adapter';
