bd9ec3de8dd8cf120952b721cb54fa8b2374d28d
Sergei Samokhvalov <samokhvalovse@yandex-team.ru>
2023-11-27 20:07:55 +0300

    change getting i18n texts

--- src/ui/components/SelectMigrationToWorkbookDialog/Body/Body.tsx
+++ src/ui/components/SelectMigrationToWorkbookDialog/Body/Body.tsx
@@ -3,10 +3,13 @@ import React, {ReactNode} from 'react';
 import {CircleQuestion} from '@gravity-ui/icons';
 import {Icon, Label, Popover} from '@gravity-ui/uikit';
 import block from 'bem-cn-lite';
+import {I18n} from 'i18n';
 
 import './Body.scss';
 
 const b = block('dl-select-migration-to-workbook-dialog-body');
+
+const i18n = I18n.keyset('component.select-migration-to-workbook-dialog');
 
 type List = {
     icon: ReactNode;
@@ -26,26 +29,26 @@ const Body: React.FC<{content: BodyProps; isActive?: boolean}> = ({content, isAc
     return (
         <div className={b({active: isActive})}>
             <div className={b('header')}>
-                <div className={b('title')}>{content.title}</div>
+                <div className={b('title')}>{i18n(content.title)}</div>
                 {content.label && (
                     <div className={b('label')}>
                         <Label size="m" theme="warning">
-                            {content.label}
+                            {i18n(content.label)}
                         </Label>
                     </div>
                 )}
 
                 <div className={b('icon')}>{content.icon}</div>
             </div>
-            <div className={b('description')}>{content.description}</div>
+            <div className={b('description')}>{i18n(content.description)}</div>
             <div className={b('content')}>
                 {content.list.map((item) => (
                     <div key={item.text} className={b('content-item')}>
                         <div className={b('content-item-icon')}>{item.icon}</div>{' '}
-                        <div className={b('content-item-text')}>{item.text}</div>
+                        <div className={b('content-item-text')}>{i18n(item.text)}</div>
                         {item.helpText && (
                             <Popover
-                                content={item.helpText}
+                                content={i18n(item.helpText)}
                                 placement="bottom"
                                 contentClassName={b('content-tooltip')}
                                 className={b('content-item-help')}
--- src/ui/components/SelectMigrationToWorkbookDialog/constants.tsx
+++ src/ui/components/SelectMigrationToWorkbookDialog/constants.tsx
@@ -10,59 +10,56 @@ import {
     Magnifier,
 } from '@gravity-ui/icons';
 import {Icon} from '@gravity-ui/uikit';
-import {I18n} from 'i18n';
 
 import CheckFolder from 'platform/src/ui/assets/icons/check-folder.svg';
 import CopyXmark from 'platform/src/ui/assets/icons/copy-xmark.svg';
 
-const i18n = I18n.keyset('component.select-migration-to-workbook-dialog');
-
 const migrateByCopy = {
-    title: i18n('title_copy'),
-    label: i18n('label'),
-    description: i18n('description_copy'),
+    title: 'title_copy',
+    label: 'label',
+    description: 'description_copy',
     icon: <Icon data={CopyArrowRight} size={32} />,
     list: [
         {
             icon: <Icon data={Magnifier} />,
-            text: i18n('list_first_item_copy'),
+            text: 'list_first_item_copy',
         },
         {
             icon: <Icon data={CheckFolder} />,
-            text: i18n('list_second_item_copy'),
+            text: 'list_second_item_copy',
         },
         {
             icon: <Icon data={Link} />,
-            text: i18n('list_third_item_copy'),
+            text: 'list_third_item_copy',
         },
         {
             icon: <Icon data={CopyCheck} />,
-            text: i18n('list_fourth_item_copy'),
+            text: 'list_fourth_item_copy',
         },
     ],
 };
 
 const migrateByTransfer = {
-    title: i18n('title_transfer'),
-    description: i18n('description_transfer'),
+    title: 'title_transfer',
+    description: 'description_transfer',
     icon: <Icon data={ArrowUturnCwRight} size={32} />,
     list: [
         {
             icon: <Icon data={FolderMagnifier} />,
-            text: i18n('list_first_item_transfer'),
-            helpText: i18n('list_first_item_transfer_help_text'),
+            text: 'list_first_item_transfer',
+            helpText: 'list_first_item_transfer_help_text',
         },
         {
             icon: <Icon data={FolderExclamation} />,
-            text: i18n('list_second_item_transfer'),
+            text: 'list_second_item_transfer',
         },
         {
             icon: <Icon data={Link} />,
-            text: i18n('list_third_item_transfer'),
+            text: 'list_third_item_transfer',
         },
         {
             icon: <Icon data={CopyXmark} />,
-            text: i18n('list_fourth_item_transfer'),
+            text: 'list_fourth_item_transfer',
         },
     ],
 };
