c62e1774d8df75fa70ffd6fe78a5f2033ee11324
imechoim <Jacksonowl93@gmail.com>
2023-11-28 16:36:26 +0100

    Move dash loading tests (#257)
    * Update us-e2e-data
    
    * Add methods to DashboardPage PO
    
    * Update test config
    
    * Move dash loading tests
    
    * Update us-e2e-data

--- tests/constants/config/index.ts
+++ tests/constants/config/index.ts
@@ -2,9 +2,11 @@ import type {TestParametrizationConfig} from '../../types/config';
 import {connections} from './connections';
 import {ql} from './ql';
 import {wizard} from './wizard';
+import {dash} from './dash';
 
 export const config: TestParametrizationConfig = {
     wizard,
     connections,
     ql,
+    dash,
 };
