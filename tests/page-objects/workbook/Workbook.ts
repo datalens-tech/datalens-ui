99d0c915f44e77cb17adfcf869588003aa448bf0
Yevhenii Chernovol <echernovol@yandex-team.ru>
2023-11-30 13:03:43 +0100

    Extend Workbook PO

--- tests/page-objects/workbook/Workbook.ts
+++ tests/page-objects/workbook/Workbook.ts
@@ -5,12 +5,15 @@ import {NavigationMinimalPopup} from './NavigationMinimalPopup';
 import {openTestPage} from 'utils';
 import {WorkbooksUrls} from 'constants/constants';
 import {EditEntityButton} from './EditEntityButton';
+import DashboardPage from 'page-objects/dashboard/DashboardPage';
 
 export class Workbook {
     createEntryButton: CreateEntryButton;
     dialogCreateEntry: DialogCreateEntry;
     navigationMinimalPopup: NavigationMinimalPopup;
     editEntityButton: EditEntityButton;
+
+    dashboardPage: DashboardPage;
 
     protected page: Page;
 
@@ -20,9 +23,23 @@ export class Workbook {
         this.createEntryButton = new CreateEntryButton(page);
         this.navigationMinimalPopup = new NavigationMinimalPopup(page);
         this.editEntityButton = new EditEntityButton(page);
+        this.dashboardPage = new DashboardPage({page});
     }
 
     async openE2EWorkbookPage() {
         await openTestPage(this.page, WorkbooksUrls.E2EWorkbook);
+    }
+
+    async createDashboard({editDash}: {editDash: () => Promise<void>}) {
+        await this.createEntryButton.createDashboard();
+
+        await editDash();
+
+        await this.dashboardPage.saveChanges();
+        await this.dialogCreateEntry.createEntryWithName();
+        await this.editEntityButton.waitForVisible();
+
+        // Important: reload the page because dash state may be different for POST(create) and GET requests.
+        this.page.reload();
     }
 }
