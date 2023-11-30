2f2b6006f35370d58c4377a65231a2e2b0505ece
Alaev Evgeny <alaev89@yandex.ru>
2023-11-30 16:40:43 +0100

    Add bases e2e for Connections

---
+++ tests/opensource-suites/connections/postgres.test.ts
@@ -0,0 +1,31 @@
+import type {Page} from '@playwright/test';
+import ConnectionsPage from '../../page-objects/connections/ConnectionsPage';
+import {openTestPage} from '../../utils';
+import {WorkbooksUrls} from 'constants/constants';
+
+import datalensTest from '../../utils/playwright/globalTestDefinition';
+
+datalensTest.describe('Postgres connector interactions', () => {
+    datalensTest('Connector should be created', async ({page}: {page: Page}) => {
+        const connectionsPage = new ConnectionsPage({page});
+        await openTestPage(page, `${WorkbooksUrls.E2EWorkbook}/connections/new/postgres`);
+        await connectionsPage.fillForm([
+            {id: 'input', name: 'host', value: 'abc.def'},
+            {id: 'input', name: 'db_name', value: 'abc'},
+            {id: 'input', name: 'username', value: 'abc'},
+            {id: 'input', name: 'password', value: 'abc'},
+        ]);
+        await connectionsPage.createConnectionInWorkbook();
+    });
+    datalensTest(
+        'Client validation errors should be displayed after creation attempt',
+        async ({page}: {page: Page}) => {
+            const connectionsPage = new ConnectionsPage({page});
+            await openTestPage(
+                page,
+                `${WorkbooksUrls.E2EWorkbook}/connections/new/postgres?_debug=1`,
+            );
+            await connectionsPage.checkClientValidationErrors();
+        },
+    );
+});
