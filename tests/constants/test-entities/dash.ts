747871f7bca9519334ac972acaaec53cd4c780f5
imechoim <Jacksonowl93@gmail.com>
2023-12-05 14:18:27 +0100

    Move tests revisionsList and saveAsCopy (#306)

--- tests/constants/test-entities/dash.ts
+++ tests/constants/test-entities/dash.ts
@@ -3,4 +3,5 @@ export enum DashUrls {
     DashboardLoadPriorityCharts = '/i2pxl812qhw27',
     DashboardLoadPrioritySelectors = '/rby9aee90m6kg',
     DashboardWithTabsAndSelectors = '/eymsqpg3m4xm3',
+    DashboardMoreThan100Revisions = '/ufvydpjatl0mj',
 }
