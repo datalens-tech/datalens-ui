5655da56ea004ea0d9ad291eeb5b8ef12263935d
Irina Kuzmina <kuzmadom@yandex-team.ru>
2023-12-07 16:24:45 +0200

    Wizard dataset e2e tests (#314)

--- tests/types/config/wizard.ts
+++ tests/types/config/wizard.ts
@@ -1,5 +1,15 @@
+type Dataset = {
+    id: string;
+    name: string;
+};
+
 export type WizardParametrizationConfig = {
+    datasets: {
+        Basic: Dataset;
+        Orders: Dataset;
+    };
     urls: {
+        NewWizardChart: string;
         WizardBasicDataset: string;
     };
 };
