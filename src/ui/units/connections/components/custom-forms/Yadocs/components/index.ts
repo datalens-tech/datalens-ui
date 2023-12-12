5bfe47de9390bf44e4d9651b71dc09df7485847a
Evgeny Alaev <alaev89@yandex-team.ru>
2023-12-12 23:20:49 +0300

    Add base UI for yadocs form

---
+++ src/ui/units/connections/components/custom-forms/Yadocs/components/index.ts
@@ -0,0 +1,3 @@
+export {AdditionalTitleContent} from './AdditionalTitleContent';
+export {DocsList} from './DocsList';
+export {Workspace} from './Workspace';
---
+++ src/ui/units/connections/components/custom-forms/Yadocs/containers/DocsListContainer.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import {DocsList} from '../components';
+
+export const DocsListContainer = () => {
+    return <DocsList />;
+};
