b8d9175a994c95cd82b9991af8df0e7f12dbdce9
Sergei Samokhvalov <samokhvalovse@yandex-team.ru>
2023-12-20 19:00:26 +0300

    Refactor

--- src/ui/units/workbooks/store/reducers/workbook-page.ts
+++ src/ui/units/workbooks/store/reducers/workbook-page.ts
@@ -317,7 +317,7 @@ export const workbooksReducer = (state: WorkbooksState = initialState, action: W
             };
         }
 
-        // Renaming the entry
+        // Change favorite the entry
         case CHANGE_FAVORITE_ENTRY_LOADING: {
             return {
                 ...state,
