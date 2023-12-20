3bca4c344343d90a7758da2cc3b0a0b65b245c4b
Anastasia Khasanova <kapibara@nebius.com>
2023-12-20 17:21:12 +0100

    Move e2e dash acceptableValues test (#363)

---
+++ tests/opensource-suites/dash/selectors/acceptableValues.test.ts
@@ -0,0 +1,84 @@
+import {Page} from '@playwright/test';
+import {ControlQA} from '../../../../src/shared/constants';
+
+import DashboardPage from '../../../page-objects/dashboard/DashboardPage';
+import {deleteEntity, openTestPage, slct} from '../../../utils';
+import datalensTest from '../../../utils/playwright/globalTestDefinition';
+import {Workbook} from '../../../page-objects/workbook/Workbook';
+import {WorkbooksUrls} from '../../../constants/constants';
+
+const PARAMS = {
+    ORIGINAL_VALUES_COUNT: 2,
+    CONTROL_TITLE: 'test-control',
+    CONTROL_ITEMS: ['1', '2'],
+    CONTROL_FIELD_NAME: 'test-control-field',
+};
+
+datalensTest.describe('Dashboards are Possible selector values', () => {
+    datalensTest.beforeEach(async ({page}: {page: Page}) => {
+        const dashboardPage = new DashboardPage({page});
+        const workbookPO = new Workbook(page);
+
+        await openTestPage(page, WorkbooksUrls.E2EWorkbook);
+
+        await workbookPO.createDashboard({
+            editDash: async () => {
+                await dashboardPage.addSelector({
+                    controlTitle: PARAMS.CONTROL_TITLE,
+                    controlItems: PARAMS.CONTROL_ITEMS,
+                    controlFieldName: PARAMS.CONTROL_FIELD_NAME,
+                });
+            },
+        });
+    });
+
+    datalensTest.afterEach(async ({page}: {page: Page}) => {
+        const dashboardPage = new DashboardPage({page});
+
+        await dashboardPage.exitEditMode();
+        await deleteEntity(page, WorkbooksUrls.E2EWorkbook);
+    });
+
+    datalensTest(
+        'Possible values of the selector-selector with manual input can be deleted',
+        async ({page}: {page: Page}) => {
+            const dashboardPage = new DashboardPage({page});
+
+            await dashboardPage.enterEditMode();
+            // go to the control settings
+            await dashboardPage.clickFirstControlSettingsButton();
+
+            // click on the button for setting possible values
+            const acceptableValuesButton = await page.waitForSelector(
+                slct(DashboardPage.selectors.acceptableValuesBtn),
+            );
+            await acceptableValuesButton.click();
+
+            // waiting for the possible values dialog to appear
+            await page.waitForSelector(slct(DashboardPage.selectors.dialogAcceptable));
+
+            // getting the number of values in the list
+            const acceptableList = await page.$$(slct(ControlQA.selectAcceptableItem));
+            expect(acceptableList.length).toBe(PARAMS.ORIGINAL_VALUES_COUNT);
+
+            // deleting values
+            const acceptablesItems = await page.$$(slct(ControlQA.selectAcceptableItem));
+
+            for (const item of acceptablesItems) {
+                await item.hover();
+                await page.click(slct(ControlQA.selectAcceptableRemoveButton));
+            }
+
+            // check that there are no values left in the list
+            const emptyAcceptableList = await page.$$(slct(ControlQA.selectAcceptableItem));
+            expect(emptyAcceptableList.length).toBe(0);
+
+            // exit the dialog, canceling the changes
+            await page.click(slct(DashboardPage.selectors.dialogCancelBtn));
+            const dialogControlCancelButton = await page.waitForSelector(
+                slct(ControlQA.dialogControlCancelBtn),
+            );
+            await dialogControlCancelButton.click();
+        },
+    );
+});
