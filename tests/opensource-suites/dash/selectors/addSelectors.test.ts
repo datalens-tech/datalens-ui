af47d1530880ef3424f7f5b350d42163cb9a2ee2
Anastasia Khasanova <kapibara@nebius.com>
2023-12-21 15:40:09 +0100

    Move e2e dash addSelectors test (#374)

---
+++ tests/opensource-suites/dash/selectors/addSelectors.test.ts
@@ -0,0 +1,56 @@
+import {Page} from '@playwright/test';
+
+import DashboardPage from '../../../page-objects/dashboard/DashboardPage';
+import {deleteEntity, openTestPage, slct} from '../../../utils';
+import datalensTest from '../../../utils/playwright/globalTestDefinition';
+import {Workbook} from '../../../page-objects/workbook/Workbook';
+import {WorkbooksUrls} from '../../../constants/constants';
+import {ControlQA} from '../../../../src/shared';
+
+const PARAMS = {
+    CONTROL_TITLE: 'test-control',
+    CONTROL_FIELD_NAME: 'test-control-field',
+    CONTROL_ITEMS: ['91000', '98800'],
+    CONTROL_ITEM_PREFIX: 'Value',
+    CHART_FIELD: 'Population String',
+};
+const CONTROL_ITEM_COUNT = 3;
+
+datalensTest.describe('Dashboards are Basic functionality', () => {
+    datalensTest(
+        'Adding a chart and selector with manual input of values and default value',
+        async ({page}: {page: Page}) => {
+            const dashboardPage = new DashboardPage({page});
+            const workbookPO = new Workbook(page);
+
+            await openTestPage(page, WorkbooksUrls.E2EWorkbook);
+
+            // filling in the values for the selector
+            const controlItems: string[] = [];
+            for (let i = 0; i < CONTROL_ITEM_COUNT; i++) {
+                controlItems.push(`${PARAMS.CONTROL_ITEM_PREFIX}-${i + 1}`);
+            }
+            const controlDefaultValue = controlItems[controlItems.length - 1];
+
+            await workbookPO.createDashboard({
+                editDash: async () => {
+                    await dashboardPage.addSelector({
+                        controlTitle: PARAMS.CONTROL_TITLE,
+                        controlItems,
+                        controlFieldName: PARAMS.CONTROL_FIELD_NAME,
+                        defaultValue: controlDefaultValue,
+                    });
+                },
+            });
+
+            // check that the default value is set correctly
+            await dashboardPage.waitForSelector(
+                `${slct(
+                    ControlQA.controlSelect,
+                )} .yc-select-control__tokens-text >> text=${controlDefaultValue}`,
+            );
+
+            await deleteEntity(page, WorkbooksUrls.E2EWorkbook);
+        },
+    );
+});
