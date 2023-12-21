bbd102d9fab8e31df2daaa21ff52f9d5208829b4
stankis <151746694+stankis@users.noreply.github.com>
2023-12-21 16:33:56 +0300

    Added functionality to rename entries in favorites (#319)
    * Added functionality to rename entries in favorites
    
    * Added functionality to rename entries in favorites
    
    * Added functionality to rename entries in favorites
    
    * Redesign with context menu
    
    * Redesign with context menu
    
    * Redesign with context menu
    
    * Redesign with context menu
    
    * Redesign with context menu
    
    * Redesign with context menu
    
    * Code review fixes
    
    * Code review fixes
    
    ---------
    
    Co-authored-by: Stanislav Kiselev <“staskis@yandex-team.ru”>

--- src/shared/types/feature.ts
+++ src/shared/types/feature.ts
@@ -92,6 +92,7 @@ export enum Feature {
     CopyEntriesToWorkbook = 'CopyEntriesToWorkbook',
     WizardChartChartFilteringAvailable = 'WizardChartChartFilteringAvailable',
     QlAutoExecuteMonitoringChart = 'QlAutoExecuteMonitoringChart',
+    EnableFavoritesNameAliases = 'EnableFavoritesNameAliases',
     HolidayNavigation = 'HolidayNavigation',
 }
 
