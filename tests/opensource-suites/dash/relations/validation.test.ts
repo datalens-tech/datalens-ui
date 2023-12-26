65ae7079e4df3a6f3a8599ed70138e2bb28e1cae
Elena Martynova <marginy@yandex-team.ru>
2023-12-26 15:31:08 +0300

    Add dash test for relations validation

---
+++ tests/opensource-suites/dash/relations/validation.test.ts
@@ -0,0 +1,57 @@
+import {Page} from '@playwright/test';
+
+import DashboardPage from '../../../page-objects/dashboard/DashboardPage';
+import {deleteEntity, slct} from '../../../utils';
+import datalensTest from '../../../utils/playwright/globalTestDefinition';
+import {ControlQA, DashCommonQa} from '../../../../src/shared';
+import {WorkbooksUrls} from '../../../constants/constants';
+import {Workbook} from '../../../page-objects/workbook/Workbook';
+import {ChartsParams} from '../../../constants/test-entities/charts';
+
+const PARAMS = {
+    WARNING_SAME_DATASET_FIELDS:
+        'You cannot create aliases between fields of the same dataset or between fields with the same ID in different datasets.',
+};
+
+datalensTest.describe('Dashboards - Relations types check (new)', () => {
+    datalensTest.beforeEach(async ({page}: {page: Page}) => {
+        const workbookPO = new Workbook(page);
+        await workbookPO.openE2EWorkbookPage();
+    });
+    datalensTest.afterEach(async ({page}: {page: Page}) => {
+        await deleteEntity(page, WorkbooksUrls.E2EWorkbook);
+    });
+    datalensTest(
+        'Check for unable adding new alias for widget on same dataset',
+        async ({page}: {page: Page}) => {
+            const workbookPO = new Workbook(page);
+            const dashboardPage = new DashboardPage({page});
+            await workbookPO.createDashboard({
+                editDash: async () => {
+                    await dashboardPage.addSelectorBySettings({});
+
+                    await dashboardPage.addChart({
+                        chartName: ChartsParams.citySalesPieChart.name,
+                        chartUrl: ChartsParams.citySalesPieChart.url,
+                        hideTitle: true,
+                    });
+                },
+            });
+            await dashboardPage.enterEditMode();
+
+            const selectorElem = await dashboardPage.getDashControlLinksIconElem(
+                ControlQA.controlLinks,
+            );
+
+            await selectorElem.click();
+            await page.hover(slct(DashCommonQa.RelationsListRow));
+            await page.locator(slct(DashCommonQa.AliasShowBtn)).click();
+
+            await dashboardPage.waitForSelector(`text=${PARAMS.WARNING_SAME_DATASET_FIELDS}`);
+
+            await dashboardPage.cancelAliasesChanges();
+            await dashboardPage.cancelRelationsChanges();
+            await dashboardPage.exitEditMode();
+        },
+    );
+});
