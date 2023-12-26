333dafd9234897bab21df6c828c2293d49328428
leutinatasya <leutinatasya@yandex-team.com>
2023-12-26 13:36:19 +0300

    Add some qa-attrs for wizard and DocSection

--- src/ui/components/FieldEditor/components/DocSection.tsx
+++ src/ui/components/FieldEditor/components/DocSection.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import {List, Loader} from '@gravity-ui/uikit';
 import block from 'bem-cn-lite';
 import SplitPane from 'react-split-pane';
+import {DocSectionQa} from 'shared';
 import type {
     DataLensFunctionsDocGroupTocItem,
     DocsTocItemLastLevel,
@@ -128,7 +129,12 @@ class DocSection extends React.Component<DocSectionProps, DocSectionState> {
                                 </div>
                             ) : (
                                 <React.Fragment>
-                                    <div className={b('doc-item-title')}>{selectedItem?.name}</div>
+                                    <div
+                                        className={b('doc-item-title')}
+                                        data-qa={DocSectionQa.Title}
+                                    >
+                                        {selectedItem?.name}
+                                    </div>
                                     <YfmWrapper
                                         className={b('doc-item')}
                                         setByInnerHtml={true}
@@ -153,8 +159,10 @@ class DocSection extends React.Component<DocSectionProps, DocSectionState> {
             selected: Boolean((item as FunctionDocListItem).selected),
         };
 
+        const qa = isGroup ? DocSectionQa.Group : DocSectionQa.Item;
+
         return (
-            <div id={item.id} className={b('doc-list-item', mods)}>
+            <div id={item.id} className={b('doc-list-item', mods)} data-qa={qa}>
                 {item.name}
             </div>
         );
