07921e8aba7977bac5bdea2e0a9688e2dca1dd8c
Evgeny Alaev <alaev89@yandex-team.ru>
2023-12-26 16:14:44 +0300

    Rework yadocs add document flow & add actions to list items

--- src/ui/units/connections/components/custom-forms/Yadocs/containers/DocsListContainer.tsx
+++ src/ui/units/connections/components/custom-forms/Yadocs/containers/DocsListContainer.tsx
@@ -1,47 +1,61 @@
 import React from 'react';
 
+import {I18n} from 'i18n';
 import get from 'lodash/get';
 import {batch, useDispatch, useSelector} from 'react-redux';
+import {InnerFieldKey} from 'ui/units/connections/constants';
 
 import {
-    addYandexDocument,
+    connectionIdSelector,
     extractYadocItemId,
     findUploadedYadoc,
+    getFilteredYadocItems,
     getYadocItemIndex,
+    handleReplacedSources,
+    innerFormSelector,
+    retryPollYadocStatus,
+    setYadocItemsAndFormSources,
     setYadocsActiveDialog,
-    setYadocsAddSectionState,
     setYadocsSelectedItemId,
+    updateYadocSource,
     yadocsActiveDialogSelector,
-    yadocsAddSectionStateSelector,
     yadocsItemsSelector,
     yadocsSelectedItemIdSelector,
 } from '../../../../store';
 import {DocsList} from '../components';
-import {HandleItemClick, UpdateAddSectionState} from '../types';
+import {HandleItemClick} from '../types';
 
-import {useYadocsDialogs} from './useGSheetDialogs';
+import {useYadocsDialogs} from './useYadocsDialogs';
+
+const i18n = I18n.keyset('connections.gsheet.view');
 
 export const DocsListContainer = () => {
     const dispatch = useDispatch();
-    const {openSourcesDialog} = useYadocsDialogs();
+    const {
+        openSourcesDialog,
+        openRenameSourceDialog,
+        openAddDocumentDialog,
+        openReplaceSourceDialog,
+    } = useYadocsDialogs();
     const activeDialog = useSelector(yadocsActiveDialogSelector);
-    const addSectionState = useSelector(yadocsAddSectionStateSelector);
+    const connectionId = useSelector(connectionIdSelector);
+    const innerForm = useSelector(innerFormSelector);
     const items = useSelector(yadocsItemsSelector);
     const selectedItemId = useSelector(yadocsSelectedItemIdSelector);
     const selectedItemIndex = React.useMemo(() => {
         return getYadocItemIndex(items, selectedItemId);
     }, [selectedItemId, items]);
+    const authorized = innerForm[InnerFieldKey.Authorized] as boolean;
 
-    const addYandexDoc = React.useCallback(() => {
-        dispatch(addYandexDocument());
+    const clickAddDocumentButton = React.useCallback(() => {
+        dispatch(
+            setYadocsActiveDialog({
+                activeDialog: {
+                    type: 'dialog-add-document',
+                },
+            }),
+        );
     }, [dispatch]);
-
-    const updateAddSectionState = React.useCallback<UpdateAddSectionState>(
-        (updates) => {
-            dispatch(setYadocsAddSectionState(updates));
-        },
-        [dispatch],
-    );
 
     const clickListItem = React.useCallback<HandleItemClick>(
         (item) => {
@@ -73,6 +87,99 @@ export const DocsListContainer = () => {
         [selectedItemId, dispatch],
     );
 
+    const deleteListItem = React.useCallback<HandleItemClick>(
+        (item) => {
+            const nextItems = getFilteredYadocItems(items, extractYadocItemId(item));
+            const replacedSourceId = extractYadocItemId(item);
+            batch(() => {
+                dispatch(setYadocItemsAndFormSources(nextItems));
+                dispatch(handleReplacedSources({action: 'filter', replacedSourceId}));
+            });
+        },
+        [items, dispatch],
+    );
+
+    const clickErrorAction = React.useCallback<HandleItemClick>(
+        (item) => {
+            switch (item.type) {
+                case 'uploadedYadoc': {
+                    dispatch(retryPollYadocStatus(item.data.file_id));
+                    break;
+                }
+                case 'yadocSourceInfo': {
+                    const fileId = get(item, ['fileId']);
+                    const sourceId = get(item, ['data', 'source_id']);
+                    dispatch(updateYadocSource({fileId, sourceId}));
+                    break;
+                }
+                case 'yadocEditableSource': {
+                    const fileId = get(item, ['data', 'file_id']);
+                    const sourceId = get(item, ['data', 'source', 'source_id']);
+                    dispatch(updateYadocSource({fileId, sourceId}));
+                }
+            }
+        },
+        [dispatch],
+    );
+
+    const clickRenameAction = React.useCallback<HandleItemClick>(
+        (item) => {
+            let sourceId: string | undefined;
+            let value: string | undefined;
+
+            switch (item.type) {
+                case 'yadocEditableSource': {
+                    sourceId = get(item, ['data', 'source', 'source_id']);
+                    value = get(item, ['data', 'source', 'title']);
+                    break;
+                }
+                case 'yadocReadonlySource': {
+                    sourceId = get(item, ['data', 'id']);
+                    value = get(item, ['data', 'title']);
+                }
+            }
+
+            if (sourceId) {
+                dispatch(
+                    setYadocsActiveDialog({
+                        activeDialog: {type: 'dialog-rename', sourceId, value},
+                    }),
+                );
+            }
+        },
+        [dispatch],
+    );
+
+    const clickReplaceAction = React.useCallback<HandleItemClick>(
+        (item) => {
+            let sourceId: string | undefined;
+
+            switch (item.type) {
+                case 'yadocEditableSource': {
+                    sourceId = get(item, ['data', 'source', 'source_id']);
+                    break;
+                }
+                case 'yadocReadonlySource': {
+                    sourceId = get(item, ['data', 'id']);
+                }
+            }
+
+            if (sourceId) {
+                dispatch(
+                    setYadocsActiveDialog({
+                        activeDialog: {
+                            type: 'dialog-replace',
+                            sourceId,
+                            connectionId,
+                            authorized,
+                        },
+                    }),
+                );
+            }
+        },
+        [connectionId, authorized, dispatch],
+    );
+
     React.useEffect(() => {
         if (activeDialog) {
             switch (activeDialog.type) {
@@ -85,9 +192,47 @@ export const DocsListContainer = () => {
 
                     break;
                 }
+                case 'dialog-add-document': {
+                    openAddDocumentDialog();
+                    break;
+                }
+                case 'dialog-rename': {
+                    const {sourceId, value} = activeDialog;
+                    const type = items[getYadocItemIndex(items, sourceId)]?.type;
+                    openRenameSourceDialog({
+                        type,
+                        sourceId,
+                        value,
+                        caption: i18n('label_replace-name'),
+                    });
+
+                    break;
+                }
+                case 'dialog-replace': {
+                    const {
+                        sourceId,
+                        connectionId: dialogConnectionId,
+                        authorized: dialogAuthorized,
+                        oauthToken: dialogOauthToken,
+                    } = activeDialog;
+                    openReplaceSourceDialog({
+                        sourceId,
+                        connectionId: dialogConnectionId,
+                        authorized: dialogAuthorized,
+                        oauthToken: dialogOauthToken,
+                        caption: i18n('label_replace-source'),
+                    });
+                }
             }
         }
-    }, [items, activeDialog, openSourcesDialog]);
+    }, [
+        items,
+        activeDialog,
+        openSourcesDialog,
+        openRenameSourceDialog,
+        openAddDocumentDialog,
+        openReplaceSourceDialog,
+    ]);
 
     React.useEffect(() => {
         const isItemInList = getYadocItemIndex(items, selectedItemId) !== -1;
@@ -105,11 +250,13 @@ export const DocsListContainer = () => {
     return (
         <DocsList
             items={items}
-            addSectionState={addSectionState}
             selectedItemIndex={selectedItemIndex}
-            addYandexDoc={addYandexDoc}
+            clickAddDocumentButton={clickAddDocumentButton}
+            clickErrorAction={clickErrorAction}
             clickListItem={clickListItem}
-            updateAddSectionState={updateAddSectionState}
+            clickRenameAction={clickRenameAction}
+            clickReplaceAction={clickReplaceAction}
+            deleteListItem={deleteListItem}
         />
     );
 };
--- src/ui/units/connections/store/actions/yadoc/index.ts
+++ src/ui/units/connections/store/actions/yadoc/index.ts
@@ -1,47 +1,61 @@
 import get from 'lodash/get';
 import {batch} from 'react-redux';
 
-import {innerAuthorizedSelector, yadocsAddSectionStateSelector} from '../../selectors';
-import type {ConnectionsReduxDispatch, GetState} from '../../typings';
-import {api} from '../api';
-import {setYadocsAddSectionState, setYadocsItems, setYadocsSelectedItemId} from '../base';
-import {showGsheetUploadingFailureToast} from '../gsheet';
+import type {ConnectionsReduxDispatch, GetState, UploadedYadoc} from '../../typings';
+import {setYadocsItems, setYadocsSelectedItemId} from '../base';
 
-import {pollYadocStatus} from './misc-actions';
-import {shapeUploadedYadocItem} from './utils';
+import {pollYadocStatus, updateYadocItem} from './misc-actions';
+import {getYadocItemIndex, shapeUploadedYadocItem} from './utils';
 
-export {yadocToSourcesInfo} from './misc-actions';
-export {extractYadocItemId, findUploadedYadoc, getYadocItemIndex} from './utils';
+export {
+    setYadocItemsAndFormSources,
+    yadocToSourcesInfo,
+    updateYadocItem,
+    updateYadocSource,
+} from './misc-actions';
+export type {UpdateYadocItemArgs} from './misc-actions';
+export {
+    extractYadocItemId,
+    findUploadedYadoc,
+    getFilteredYadocItems,
+    getYadocItemIndex,
+} from './utils';
 
-export const addYandexDocument = () => {
+/** Prepares uploaded Yandex document in case of adding new document */
+export const handleUploadedYadocBeforePolling = (data: UploadedYadoc['data']) => {
     return async (dispatch: ConnectionsReduxDispatch, getState: GetState) => {
-        const authorized = innerAuthorizedSelector(getState());
-        const {path, mode} = yadocsAddSectionStateSelector(getState());
-        dispatch(setYadocsAddSectionState({uploading: true}));
-        const {document, error} = await api.addYandexDocument({
-            authorized,
-            ...(mode === 'private' ? {privatePath: path} : {publicLink: path}),
-        });
+        const prevItems = get(getState().connections, ['yadocs', 'items']);
+        const uploadedYadoc = shapeUploadedYadocItem({data});
+        dispatch(setYadocsItems({items: [...prevItems, uploadedYadoc]}));
+        dispatch(setYadocsSelectedItemId({selectedItemId: data.file_id}));
+        pollYadocStatus({fileId: data.file_id, dispatch, getState});
+    };
+};
+
+/** Prepares uploaded Yandex document in case of replacing an existing source */
+export const handleReplacedSourceBeforePolling = (
+    data: UploadedYadoc['data'],
+    replacedSourceId: string,
+) => {
+    return async (dispatch: ConnectionsReduxDispatch, getState: GetState) => {
+        const prevItems = get(getState().connections, ['yadocs', 'items']);
+        const items = [...prevItems];
+        const fileId = data.file_id;
+        const replacedSourceIndex = getYadocItemIndex(prevItems, replacedSourceId);
+        const uploadedGSheet = shapeUploadedYadocItem({data, replacedSourceId});
+        items.splice(replacedSourceIndex, 1, uploadedGSheet);
 
         batch(() => {
-            if (error) {
-                dispatch(showGsheetUploadingFailureToast(error));
-            }
-
-            dispatch(
-                setYadocsAddSectionState({
-                    uploading: false,
-                    ...(!error && {url: '', active: false}),
-                }),
-            );
-
-            if (document) {
-                const prevItems = get(getState().connections, ['yadocs', 'items']);
-                const uploadedYadoc = shapeUploadedYadocItem({data: document});
-                dispatch(setYadocsItems({items: [...prevItems, uploadedYadoc]}));
-                dispatch(setYadocsSelectedItemId({selectedItemId: document.file_id}));
-                pollYadocStatus({fileId: document.file_id, dispatch, getState});
-            }
+            dispatch(setYadocsItems({items}));
+            dispatch(setYadocsSelectedItemId({selectedItemId: fileId}));
+            pollYadocStatus({fileId, dispatch, getState});
         });
+    };
+};
+
+export const retryPollYadocStatus = (fileId: string) => {
+    return async (dispatch: ConnectionsReduxDispatch, getState: GetState) => {
+        dispatch(updateYadocItem({id: fileId, updates: {status: 'in_progress', error: null}}));
+        pollYadocStatus({fileId, dispatch, getState});
     };
 };
