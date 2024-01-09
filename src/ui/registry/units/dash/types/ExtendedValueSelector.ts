348734804ebbe5cc1fe40b2b79e4aa3a6d56fec3
Flunt1k <chvp00@gmail.com>
2024-01-09 16:15:58 +0100

    Add extending value selector

---
+++ src/ui/registry/units/dash/types/ExtendedValueSelector.ts
@@ -0,0 +1,5 @@
+import type {SelectorElementType} from '../../../../units/dash/store/actions/dashTyped';
+
+export type ExtendedValueSelectorProps = {
+    controlType: SelectorElementType | undefined;
+};
--- src/ui/units/dash/containers/Dialogs/Control2/Sections/ValueSelector/ValueSelector.tsx
+++ src/ui/units/dash/containers/Dialogs/Control2/Sections/ValueSelector/ValueSelector.tsx
@@ -16,6 +16,7 @@ import {
 
 import {FilterValue} from '../../../../../../../../shared/modules';
 import {DATASET_FIELD_TYPES} from '../../../../../../../../shared/types';
+import {registry} from '../../../../../../../registry';
 import DateDefaultValue from '../../../Control/Date/Default/Default';
 import {CheckboxControlValue} from '../../../Control/constants';
 
@@ -176,7 +177,13 @@ const CheckboxValueControl = () => {
 const ValueSelector: React.FC = () => {
     const controlType = useSelector(selectSelectorControlType);
 
-    let inputControl = null;
+    const {ExtendedValueSelector} = registry.dash.components.getAll();
+
+    let inputControl = <ExtendedValueSelector controlType={controlType} />;
+
+    if (inputControl) {
+        return inputControl;
+    }
 
     switch (controlType) {
         case 'date': {
