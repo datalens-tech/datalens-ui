029228efba8b4f8ebcf134ccaadd0df0b14778f2
Flunt1k <chvp00@gmail.com>
2024-01-09 16:15:58 +0100

    Add extending value selector

--- src/ui/registry/units/dash/components-map.tsx
+++ src/ui/registry/units/dash/components-map.tsx
@@ -1,10 +1,12 @@
 import {makeDefaultEmpty} from '../../components/DefaultEmpty';
 
-import {DialogDashMetaProps} from './types/DialogDashMeta';
+import type {DialogDashMetaProps} from './types/DialogDashMeta';
+import type {ExtendedValueSelectorProps} from './types/ExtendedValueSelector';
 
 export const dashComponentsMap = {
     DashSelectState: makeDefaultEmpty(),
     DashSelectStateDialog: makeDefaultEmpty(),
     DialogDashMeta: makeDefaultEmpty<DialogDashMetaProps>(),
     DialogText: makeDefaultEmpty(),
+    ExtendedValueSelector: makeDefaultEmpty<ExtendedValueSelectorProps>(),
 } as const;
