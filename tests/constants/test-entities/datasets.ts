4455f533f01332e1c35836ecab11b41c726b7f37
Elena Martynova <marginy@yandex-team.ru>
2024-01-10 20:21:57 +0300

    Return button_enter-amount-rows keyset

---
+++ tests/constants/test-entities/datasets.ts
@@ -0,0 +1,10 @@
+export const Datasets = {
+    Dataset1: {
+        name: 'Dataset',
+        url: '/datasets/v9ign8imt0qkk-dataset',
+    },
+    Dataset2: {
+        name: 'Orders',
+        url: '/datasets/wiuimfkr0lful-orders',
+    },
+};
