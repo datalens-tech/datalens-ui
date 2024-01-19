bda2362a9967f986f7f7d2ae8d2f1bb578a540ad
Evgeny Alaev <alaev89@yandex-team.ru>
2024-01-19 20:46:22 +0300

    Add texts for Yandex Documents form

--- src/ui/units/connections/utils/yadocs.ts
+++ src/ui/units/connections/utils/yadocs.ts
@@ -2,44 +2,44 @@ import {I18n} from 'i18n';
 import type {DataLensApiError} from 'ui/typings';
 import {parseError} from 'ui/utils/errors/parse';
 
-import {i18n8857} from '../components/custom-forms/Yadocs/constants';
 import {ConverterErrorCode} from '../constants';
 import type {YadocItem} from '../store';
 
-const i18n = I18n.keyset('connections.gsheet.view');
+const i18nGSheet = I18n.keyset('connections.gsheet.view');
+const i18nYadocs = I18n.keyset('connections.yadocs.view');
 
 const getErrorTitle = ({type, code}: {type: YadocItem['type']; code: string}) => {
     switch (code) {
         case ConverterErrorCode.FILE_LIMIT_EXCEEDED: {
-            return i18n('label_file-limit-exceeded');
+            return i18nGSheet('label_file-limit-exceeded');
         }
         case ConverterErrorCode.INVALID_LINK: {
-            return i18n('label_invalid-link');
+            return i18nGSheet('label_invalid-link');
         }
         case ConverterErrorCode.NOT_FOUND: {
-            return i18n('label_file-not-found');
+            return i18nGSheet('label_file-not-found');
         }
         case ConverterErrorCode.NO_DATA: {
-            return i18n('label_empty-file');
+            return i18nGSheet('label_empty-file');
         }
         case ConverterErrorCode.PERMISSION_DENIED: {
-            return i18n('label_403-title');
+            return i18nGSheet('label_403-title');
         }
         case ConverterErrorCode.TOO_MANY_COLUMNS: {
-            return i18n('label_too-many-columns');
+            return i18nGSheet('label_too-many-columns');
         }
         case ConverterErrorCode.UNSUPPORTED_DOCUMENT: {
-            return i18n('label_unsupported-document');
+            return i18nGSheet('label_unsupported-document');
         }
     }
 
     // Defaults
     switch (type) {
         case 'uploadedYadoc': {
-            return i18n('label_gsheet-uploading-failure');
+            return i18nGSheet('label_gsheet-uploading-failure');
         }
         default: {
-            return i18n('label_source-info-failed');
+            return i18nGSheet('label_source-info-failed');
         }
     }
 };
@@ -47,7 +47,7 @@ const getErrorTitle = ({type, code}: {type: YadocItem['type']; code: string}) =>
 const getErrorDescription = ({code}: {code?: string}) => {
     switch (code) {
         case ConverterErrorCode.PERMISSION_DENIED: {
-            return i18n8857['label_403-not-authotized-description'];
+            return i18nYadocs('label_403-not-authotized-description');
         }
         default: {
             return '';
