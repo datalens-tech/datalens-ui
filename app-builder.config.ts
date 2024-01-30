610d17a553bb54dd32b362995bfdaa4362b2c67c
Andrey Melikhov <melikhov@yandex-team.ru>
2024-01-30 21:28:24 +0300

    Fix lodash treeshaking (#549)

--- app-builder.config.ts
+++ app-builder.config.ts
@@ -19,6 +19,15 @@ const getFileCacheConfig = () => {
     }
 };
 
+const vendors = (vendorsList: string[]) => {
+    return (
+        vendorsList
+            // @see https://github.com/gravity-ui/app-builder/pull/120
+            .filter((item) => item !== 'lodash')
+            .concat(['react-split-pane', 'react-dnd', 'react-grid-layout'])
+    );
+};
+
 const config: ServiceConfig = {
     client: {
         alias: {
@@ -34,7 +43,7 @@ const config: ServiceConfig = {
         ],
         includes: ['src/shared', 'src/i18n', 'node_modules/monaco-editor/esm/vs'],
         excludeFromClean: ['!i18n', '!i18n/**/*'],
-        vendors: ['react-split-pane', 'react-dnd', 'react-grid-layout'],
+        vendors,
         icons: ['src/ui/assets/icons', 'node_modules/@gravity-ui/icons'],
         monaco: {
             languages: ['typescript', 'javascript', 'json', 'sql', 'mysql'],
