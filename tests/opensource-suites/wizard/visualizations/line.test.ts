7cb64f87bb5a5c55070c65e670deec8fa4eb7e17
Irina Kuzmina <kuzmadom@yandex-team.ru>
2024-02-02 12:00:16 +0200

    Fix flat table gradient coloring (#559)
    * Fix flat table gradient coloring
    
    * fix test
    
    * fix e2e test

--- tests/opensource-suites/wizard/visualizations/line.test.ts
+++ tests/opensource-suites/wizard/visualizations/line.test.ts
@@ -2,7 +2,7 @@ import {expect} from '@playwright/test';
 
 import datalensTest from '../../../utils/playwright/globalTestDefinition';
 import {openTestPage, slct} from '../../../utils';
-import {SectionDatasetQA, WizardPageQa, WizardVisualizationId} from '../../../../src/shared';
+import {WizardPageQa, WizardVisualizationId} from '../../../../src/shared';
 import WizardPage from '../../../page-objects/wizard/WizardPage';
 import {PlaceholderName} from '../../../page-objects/wizard/SectionVisualization';
 
@@ -20,21 +20,12 @@ datalensTest.describe('Wizard', () => {
 
         datalensTest('Date and time on the Y axis @screenshot', async ({page}) => {
             const wizardPage = new WizardPage({page});
-            const datasetFields = page.locator(slct(SectionDatasetQA.DatasetFields));
             const chartContainer = page.locator(slct(WizardPageQa.SectionPreview));
             const chart = chartContainer.locator('.chartkit-graph,.chartkit-d3');
 
             // Create dateTime field
             const dateTimeMeasureField = 'MaxDate';
-            await wizardPage.fieldEditor.open();
-            await wizardPage.fieldEditor.setName(dateTimeMeasureField);
-            await wizardPage.fieldEditor.setFormula('MAX([Order_date])');
-            await wizardPage.fieldEditor.clickToApplyButton();
-            await expect(
-                datasetFields.locator(slct(SectionDatasetQA.ItemTitle), {
-                    hasText: dateTimeMeasureField,
-                }),
-            ).toBeVisible();
+            await wizardPage.createNewFieldWithFormula(dateTimeMeasureField, 'MAX([Order_date])');
 
             await wizardPage.sectionVisualization.addFieldByClick(PlaceholderName.X, 'Category');
             await wizardPage.sectionVisualization.addFieldByClick(
