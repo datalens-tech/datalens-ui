cb3d168953a60f7d59136bf11d2d0de6062870fb
Artem Panchuk <artemipanchuk@gmail.com>
2024-02-14 18:30:58 +0300

    Fix NodeTemplate

--- src/ui/units/editor/components/NodeTemplates/NodeTemplates.js
+++ src/ui/units/editor/components/NodeTemplates/NodeTemplates.js
@@ -5,19 +5,11 @@ import PropTypes from 'prop-types';
 
 import templates from '../../../../../shared/constants/editor-templates';
 
-// import {getSdk} from '../../../../libs/schematic-sdk';
-// import {MODULE_TYPE} from '../../constants/common';
-// import Fetch from '../Fetch/Fetch';
-
 import List from './List/List';
 
 import './NodeTemplates.scss';
 
 const b = block('node-templates');
-
-// function formatter(result) {
-//     return result.entries.filter(({type}) => type.includes('_node') || type === MODULE_TYPE);
-// }
 
 function NodeTemplates({onClick}) {
     return (
