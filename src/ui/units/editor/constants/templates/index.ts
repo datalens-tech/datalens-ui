ccce3a21df6c62d742e2888bfba646716404e3a1
Artem Panchuk <artemipanchuk@gmail.com>
2024-02-16 17:18:03 +0300

    Remove unneccessary templates

--- src/ui/units/editor/constants/templates/index.ts
+++ src/ui/units/editor/constants/templates/index.ts
@@ -1,12 +1,8 @@
 import control from './control';
-import graph from './graph';
 import markdown from './markdown';
-import metric from './metric';
 import module from './module';
 import table from './table';
-import timeseries from './timeseries';
-import yandexMap from './yandex-map';
 
 export const getEditorTemplates = () => {
-    return [control, graph, markdown, metric, module, table, timeseries, yandexMap];
+    return [control, markdown, module, table];
 };
