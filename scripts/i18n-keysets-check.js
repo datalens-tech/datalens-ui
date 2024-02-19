957d8c406af13284988e0ff9a97c48d1a3f5faad
Sergey Pavlyuk <sergeypavlyuk@yandex-team.ru>
2024-02-19 18:52:55 +0300

    Add keys consistency check for i18n keysets

---
+++ scripts/i18n-keysets-check.js
@@ -0,0 +1,73 @@
+/**
+ * Weblate create PR with changed and approved keysets with sorted keys
+ * That's why we need to decrease diff
+ */
+
+const {spawnSync} = require('child_process');
+const {readFileSync, writeFileSync} = require('fs');
+const {join, parse} = require('path');
+
+const LANGUAGES = ['ru', 'en'];
+
+const getPairedLanguageKeysets = (filePath) => {
+    const parsedData = parse(filePath);
+
+    return LANGUAGES.reduce((memo, lang) => {
+        if (lang !== parsedData.name) {
+            memo[lang] = join(parsedData.dir, `${lang}${parsedData['ext']}`);
+        }
+
+        return memo;
+    }, {});
+};
+
+const readData = (filePath) => readFileSync(filePath).toString();
+
+const readPairedKeysetsKeys = (filePath) =>
+    Object.entries(getPairedLanguageKeysets(filePath)).reduce((memo, [lang, file]) => {
+        try {
+            memo[lang] = Object.keys(JSON.parse(readData(file))).sort();
+        } catch (e) {
+            throw new Error(`${file} read error: ${e.message}`);
+        }
+
+        return memo;
+    }, {});
+
+const keysetFiles = process.argv.slice(2);
+keysetFiles.forEach((filePath) => {
+    const fileContent = readData(filePath);
+
+    try {
+        const keysets = JSON.parse(fileContent);
+        const res = {};
+        const sortedKeys = Object.keys(keysets).sort();
+        const pairedKeysets = readPairedKeysetsKeys(filePath);
+
+        sortedKeys.forEach((key) => {
+            res[key] = keysets[key];
+        });
+
+        Object.entries(pairedKeysets).forEach(([lang, keys]) => {
+            const keysLength = keys.length;
+
+            if (sortedKeys.length !== keysLength) {
+                throw Error(`Different number of keys in ${lang} for keyset ${filePath}`);
+            }
+
+            for (let i = 0; i < keysLength; i++) {
+                if (keys[i] !== sortedKeys[i]) {
+                    throw Error(`Keyset missmatch for ${lang} for keyset ${filePath}`);
+                }
+            }
+        });
+
+        writeFileSync(filePath, JSON.stringify(res, null, 2));
+
+        spawnSync('git', ['add', filePath]);
+        process.exitCode = 0;
+    } catch (e) {
+        console.error(e);
+        process.exitCode = 1;
+    }
+});
