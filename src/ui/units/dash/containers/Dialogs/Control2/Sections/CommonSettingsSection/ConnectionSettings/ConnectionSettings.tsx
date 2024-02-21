e06625d8f9f9df6487825e134c9f976c937c9ee3
Flunt1k <chvp00@gmail.com>
2024-02-21 13:57:00 +0100

    feat(control dialog): add support basic settings for connection based control

--- src/ui/units/dash/containers/Dialogs/Control2/Sections/CommonSettingsSection/ConnectionSettings/ConnectionSettings.tsx
+++ src/ui/units/dash/containers/Dialogs/Control2/Sections/CommonSettingsSection/ConnectionSettings/ConnectionSettings.tsx
@@ -3,8 +3,25 @@ import React from 'react';
 import {I18n} from 'i18n';
 
 import {SectionWrapper} from '../../../../../../../../components/SectionWrapper/SectionWrapper';
+import {ELEMENT_TYPE} from '../../../../Control/constants';
+import {ValueSelector} from '../../ValueSelector/ValueSelector';
+import {InputTypeSelector} from '../InputTypeSelector/InputTypeSelector';
+import {getElementOptions} from '../helpers/input-type-select';
 
 const i18n = I18n.keyset('dash.control-dialog.edit');
 export const ConnectionSettings: React.FC = () => {
-    return <SectionWrapper title={i18n('label_common-settings')}></SectionWrapper>;
+    const options = React.useMemo(() => {
+        const allowedOptions: Record<string, boolean> = {
+            [ELEMENT_TYPE.SELECT]: true,
+            [ELEMENT_TYPE.INPUT]: true,
+        };
+        return getElementOptions().filter((v) => allowedOptions[v.value]);
+    }, []);
+
+    return (
+        <SectionWrapper title={i18n('label_common-settings')}>
+            <InputTypeSelector options={options} />
+            <ValueSelector />
+        </SectionWrapper>
+    );
 };
