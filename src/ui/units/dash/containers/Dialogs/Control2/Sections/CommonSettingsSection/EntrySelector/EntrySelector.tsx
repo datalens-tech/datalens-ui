14f407ebbe027387f1a4707d43a16c76a6716d1e
Flunt1k <chvp00@gmail.com>
2024-02-21 15:43:47 +0100

    refactor(control dialog): extract entry selector to separate component

---
+++ src/ui/units/dash/containers/Dialogs/Control2/Sections/CommonSettingsSection/EntrySelector/EntrySelector.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import {FormRow} from '@gravity-ui/components';
+import {Button} from '@gravity-ui/uikit';
+import block from 'bem-cn-lite';
+import {I18n} from 'i18n';
+import type {GetEntryResponse} from 'shared/schema/us/types/entries';
+
+import DropdownNavigation from '../../../../../DropdownNavigation/DropdownNavigation';
+
+const i18n = I18n.keyset('dash.control-dialog.edit');
+const b = block('entry-selector');
+
+type EntrySelectorProps = {
+    label: string;
+    entryId: string;
+    scope: string;
+    handleEntryChange: (entry: GetEntryResponse) => void;
+    showOpenButton: boolean;
+    getEntryLink: (entryId: string) => string;
+};
+
+export const EntrySelector: React.FC<EntrySelectorProps> = (props: EntrySelectorProps) => {
+    const {label, getEntryLink, showOpenButton, entryId, handleEntryChange, scope} = props;
+    return (
+        <FormRow label={label}>
+            <div className={b('droplist')}>
+                <DropdownNavigation
+                    //@ts-ignore
+                    size="m"
+                    //@ts-ignore
+                    entryId={entryId}
+                    //@ts-ignore
+                    scope={scope}
+                    //@ts-ignore
+                    onClick={handleEntryChange}
+                />
+                {showOpenButton && (
+                    <Button className={b('button')} target="_blank" href={getEntryLink(entryId)}>
+                        {i18n('button_open')}
+                    </Button>
+                )}
+            </div>
+        </FormRow>
+    );
+};
