7c47d14d9a1f72c8088740aea0538073bffa9156
Flunt1k <chvp00@gmail.com>
2024-02-23 14:38:15 +0100

    feat(dialog edit query): add support checking query before save

--- src/ui/units/dash/containers/Dialogs/DialogEditQuery/DialogEditQuery.tsx
+++ src/ui/units/dash/containers/Dialogs/DialogEditQuery/DialogEditQuery.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 
 import {Dialog} from '@gravity-ui/uikit';
 import block from 'bem-cn-lite';
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
+import type {ConnectionQueryContent} from 'shared';
 
 import DialogManager from '../../../../../components/DialogManager/DialogManager';
+import {getSdk} from '../../../../../libs/schematic-sdk';
 import {closeDialog} from '../../../../../store/actions/dialog';
+import {selectWorkbookId} from '../../../../workbooks/store/selectors';
+import {setSelectorDialogItem} from '../../../store/actions/dashTyped';
+import {selectSelectorDialog} from '../../../store/selectors/dashTypedSelectors';
 
 import {QueryEditor} from './QueryEditor/QueryEditor';
 
@@ -24,10 +29,41 @@ const b = block('dialog-edit-query');
 
 const DialogEditQuery: React.FC = () => {
     const dispatch = useDispatch();
+    const {connectionQueryContent, connectionQueryType, connectionId} =
+        useSelector(selectSelectorDialog);
+    const workbookId = useSelector(selectWorkbookId);
 
-    const [query, setQuery] = React.useState<string | undefined>();
+    const [query, setQuery] = React.useState<string | undefined>(connectionQueryContent?.query);
+    const [loading, setLoading] = React.useState(false);
+    const [error, setError] = React.useState<any>();
 
     const handleClose = React.useCallback(() => dispatch(closeDialog()), []);
+    const handleApply = () => {
+        if (!connectionId || !query || !connectionQueryType) {
+            return;
+        }
+        const queryContent: ConnectionQueryContent = {query};
+        setLoading(true);
+        getSdk()
+            .bi.getConnectionTypedQueryData({
+                connectionId,
+                workbookId,
+                body: {
+                    query_type: connectionQueryType,
+                    query_content: queryContent,
+                    parameters: [],
+                },
+            })
+            .then(() => {
+                dispatch(setSelectorDialogItem({connectionQueryContent: queryContent}));
+            })
+            .catch((e) => {
+                setError(e);
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    };
     const handleQueryEditorUpdate = (v: string) => setQuery(v);
     return (
         <Dialog className={b()} open={true} hasCloseButton={true} onClose={handleClose}>
@@ -37,9 +73,12 @@ const DialogEditQuery: React.FC = () => {
             </Dialog.Body>
             <Dialog.Footer
                 preset="default"
+                loading={loading}
                 showError={false}
                 listenKeyEnter={false}
+                propsButtonApply={{disabled: Boolean(error)}}
                 onClickButtonCancel={handleClose}
+                onClickButtonApply={handleApply}
                 textButtonApply={i18nConnectionBasedControlFake('button_apply')}
                 textButtonCancel={i18nConnectionBasedControlFake('button_cancel')}
             />
