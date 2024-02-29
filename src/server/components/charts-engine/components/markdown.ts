265dcaa971f735d38f792a3d427c1c7473348c38
Elena Martynova <marginy@yandex-team.ru>
2024-02-29 10:24:33 +0300

    Add opening anchor links in self instead of blank (#689)

--- src/server/components/charts-engine/components/markdown.ts
+++ src/server/components/charts-engine/components/markdown.ts
@@ -4,13 +4,31 @@ import deflist from '@doc-tools/transform/lib/plugins/deflist';
 import imsize from '@doc-tools/transform/lib/plugins/imsize';
 import notes from '@doc-tools/transform/lib/plugins/notes';
 import table from '@doc-tools/transform/lib/plugins/table';
+import MarkdownIt from 'markdown-it';
 import katex from 'markdown-it-katex';
 import mila from 'markdown-it-link-attributes';
 
 import {registry} from '../../../registry';
 
 export function renderHTML({text = '', lang}: {text: string; lang: string}): {result: string} {
-    const plugins = [deflist, notes, cut, mila, imsize, table, katex];
+    const plugins = [
+        deflist,
+        notes,
+        cut,
+        (md: MarkdownIt) =>
+            md.use(mila, {
+                matcher(href: string) {
+                    return !href.startsWith('#');
+                },
+                attrs: {
+                    target: '_blank',
+                    rel: 'noopener noreferrer',
+                },
+            }),
+        imsize,
+        table,
+        katex,
+    ];
 
     const yfmPlugins = registry.getYfmPlugins();
     if (yfmPlugins) {
@@ -23,10 +41,6 @@ export function renderHTML({text = '', lang}: {text: string; lang: string}): {re
         plugins,
         lang,
         vars: {},
-        attrs: {
-            target: '_blank',
-            rel: 'noopener noreferrer',
-        },
         disableLiquid: true,
         needToSanitizeHtml: true,
     });
