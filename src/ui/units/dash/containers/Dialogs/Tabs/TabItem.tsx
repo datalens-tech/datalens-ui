7fa2ffeed62d7a7bf6fcef84f54add2ae25388f5
Elena Martynova <marginy@yandex-team.ru>
2024-03-04 11:03:31 +0300

    Add current tab highlight (#706)
    * Add current tab highlight
    
    * Disable close by click outside tabs
    
    * Change color for selected tab like in navigation

--- src/ui/units/dash/containers/Dialogs/Tabs/TabItem.tsx
+++ src/ui/units/dash/containers/Dialogs/Tabs/TabItem.tsx
@@ -44,6 +44,7 @@ type OwnProps = {
     setExpandedItem: (id: string) => void;
     tab: DashTabChanged;
     dashTabs: Array<DashTabChanged>;
+    selectedDashTabId: string | null;
 };
 
 type DispatchProps = ResolveThunks<typeof mapDispatchToProps>;
@@ -95,7 +96,7 @@ class TabItem extends React.PureComponent<Props> {
     dropDownMenuRef = React.createRef<HTMLDivElement>();
 
     render() {
-        const {id, isActive, title, dashTabs} = this.props;
+        const {id, isActive, title, dashTabs, selectedDashTabId} = this.props;
         const {editMode} = this.state;
         const dashTab = dashTabs.find((item) => item.id === id);
         const dashTabItems = dashTab?.items || [];
@@ -111,7 +112,10 @@ class TabItem extends React.PureComponent<Props> {
             />
         ) : (
             <div
-                className={b('row', {active: isActive || this.state.showWidgetsOrderPopup})}
+                className={b('row', {
+                    active: isActive || this.state.showWidgetsOrderPopup,
+                    selected: selectedDashTabId === id,
+                })}
                 onDoubleClick={this.setEditMode}
             >
                 <div title={title} className={b('title')}>
