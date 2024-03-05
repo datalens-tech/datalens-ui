5b90ec1c9e7d8c70768fec5d4d1165225faabf4b
melikhov <melikhov@yandex-team.ru>
2024-03-05 16:23:35 +0300

    statoscope improvements

--- scripts/ci/createStatoscopeComment.js
+++ scripts/ci/createStatoscopeComment.js
@@ -1,15 +1,15 @@
 const fs = require('fs');
 
-const Mustache = require('mustache');
+const lodashTemplate = require('lodash/template');
 
 const createPRComment = require('./createPRComment');
 
-const template = fs.readFileSync('./.github/workflows/statoscope-comment.mustache', 'utf8');
+const template = fs.readFileSync('./statoscope-comment.tmpl', 'utf8');
 
 module.exports = async ({github, context, core, downloadLink}) => {
     const data = JSON.parse(fs.readFileSync('result.json', 'utf8'));
     data.downloadLink = downloadLink;
-    const body = Mustache.render(template, data);
+    const body = lodashTemplate(template, data);
 
     await createPRComment({github, context, core, body});
 };
