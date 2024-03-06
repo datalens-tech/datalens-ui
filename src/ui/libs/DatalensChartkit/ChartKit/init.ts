a5c45838b93e079daeb1961b92408394f2f518ea
Serge Pavlyuk <sergeypavlyuk@yandex-team.ru>
2024-03-06 19:19:28 +0300

    Moved entryId getter from dash store (#729)

--- src/ui/libs/DatalensChartkit/ChartKit/init.ts
+++ src/ui/libs/DatalensChartkit/ChartKit/init.ts
@@ -74,10 +74,10 @@ export const initChartKitSettings = () => {
             }
 
             const {entryContent, dash} = getStore().getState();
-            if (entryContent.scope === Scope.Dash) {
+            if (entryContent.scope === Scope.Dash && dash) {
                 const dashInfo = {
-                    dashId: entryContent.entryId,
-                    ...(dash && dash.tabId ? {dashTabId: dash.tabId} : {}),
+                    ...(dash.entry?.entryId ? {dashId: dash?.entry?.entryId} : {}),
+                    ...(dash.tabId ? {dashTabId: dash.tabId} : {}),
                 };
 
                 request.headers[DASH_INFO_HEADER] = new URLSearchParams(dashInfo).toString();
