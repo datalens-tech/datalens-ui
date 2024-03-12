531da2114e7ffa648fb4bf923b3ec0c944aff7b5
Evgeny Alaev <alaev89@yandex-team.ru>
2024-03-12 21:14:10 +0300

    Remove separeted doc tabs in module template

--- src/ui/units/editor/configs/chart/chart-editor-types.ts
+++ src/ui/units/editor/configs/chart/chart-editor-types.ts
@@ -522,28 +522,6 @@ export function getChartEditorTypes(type: ChartEditorTypeKey) {
                         },
                     ],
                 },
-                {
-                    name: 'Doc Ru',
-                    id: 'documentation_ru',
-                    language: 'markdown',
-                    docs: [
-                        {
-                            title: 'section_common-information',
-                            path: DOCS_PATH.MODULE,
-                        },
-                    ],
-                },
-                {
-                    name: 'Doc En',
-                    id: 'documentation_en',
-                    language: 'markdown',
-                    docs: [
-                        {
-                            title: 'section_common-information',
-                            path: DOCS_PATH.MODULE,
-                        },
-                    ],
-                },
             ],
         },
         markup_node: {
