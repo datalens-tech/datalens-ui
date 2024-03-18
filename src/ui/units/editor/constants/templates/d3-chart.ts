3f9b442879fbcf6ca9b57dd55024eba112e56f81
Evgeny Alaev <alaev89@yandex-team.ru>
2024-03-18 15:19:01 +0300

    fill js tab in d3 template

--- src/ui/units/editor/constants/templates/d3-chart.ts
+++ src/ui/units/editor/constants/templates/d3-chart.ts
@@ -5,8 +5,38 @@ export default {
     name: 'd3',
     type: EDITOR_TYPE.D3_NODE,
     data: {
-        js: '// Some js code',
-        params: '',
-        shared: '',
+        js: `module.exports = {
+    chart: {
+        margin: {
+            left: 10,
+            right: 10,
+            top: 10,
+            bottom: 10,
+        }
+    },
+    series: {
+        data: [
+            {
+                type: "bar-x",
+                name: "Fruits",
+                data: [
+                    { x: "Apples", y: 3 },
+                    { x: "Oranges", y: 1 },
+                    { x: "Grapes", y: 2 },
+                ],
+            },
+        ],
+    },
+    title: {text: 'Chart title'},
+    xAxis: {
+        type: "category",
+        categories: ["Apples", "Oranges", "Grapes"],
+        title: {text: 'X axis title'},
+    },
+    yAxis: [{
+        title: {text: 'Y axis title'},
+    }],
+};
+`,
     },
 };
