c0ac4e0dbcc7eaba322594a9dbc96ff9987f4d72
Irina Kuzmina <kuzmadom@yandex-team.ru>
2024-04-04 17:53:58 +0300

    Add related issue

--- src/shared/utils/date-time.ts
+++ src/shared/utils/date-time.ts
@@ -1,5 +1,6 @@
 import dayjs from '@gravity-ui/date-utils/build/dayjs';
 
+// https://github.com/gravity-ui/date-utils/issues/49
 export function getUtcDateTime(value: string) {
     return dayjs.utc(value);
 }
