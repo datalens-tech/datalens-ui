bb302b216ae50039b71ce53b5825727f916ee1bc
Александр Краснов <a-krasnov@it-serv.ru>
2024-04-15 08:40:46 +0300

    Обновление

--- src/ui/libs/DatalensChartkit/modules/menu/menu.ts
+++ src/ui/libs/DatalensChartkit/modules/menu/menu.ts
@@ -71,7 +71,7 @@ export const getChartkitMenuByType = (props?: GetChartkitMenuByType) => {
                 onFullscreenClick,
             },
             [MenuItemsIds.GET_LINK]: {
-                isVisible: () => false
+                isVisible: () => true
             },
             ...customOptions,
         } as GetChartkitMenuByType['customOptions'],
