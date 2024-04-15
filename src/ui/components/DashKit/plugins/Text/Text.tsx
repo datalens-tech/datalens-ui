5a243b1a342be6a99adf4cdbb9d8513e9dc04f63
Elena Martynova <marginy@yandex-team.ru>
2024-04-15 13:57:12 +0300

    Add text&title widget autoheight support (#880)

--- src/ui/components/DashKit/plugins/Text/Text.tsx
+++ src/ui/components/DashKit/plugins/Text/Text.tsx
@@ -7,6 +7,8 @@ import {
     pluginText,
 } from '@gravity-ui/dashkit';
 import block from 'bem-cn-lite';
+import {adjustWidgetLayout as dashkitAdjustWidgetLayout} from 'ui/components/DashKit/utils';
+import {YFM_MARKDOWN_CLASSNAME} from 'ui/constants/yfm';
 
 import {getRandomCKId} from '../../../../libs/DatalensChartkit/ChartKit/helpers/getRandomCKId';
 import {YfmWrapper} from '../../../YfmWrapper/YfmWrapper';
@@ -31,6 +33,9 @@ const textPlugin = {
     ) {
         const [textReady, setTextReady] = React.useState<string>('');
         const [randomId, setRandomId] = React.useState<string>('');
+        const rootNodeRef = React.useRef<HTMLDivElement>(null);
+        const cutNodesRef = React.useRef<NodeList | null>(null);
+        const mutationObserver = React.useRef<MutationObserver | null>(null);
 
         /**
          * get prepared text with markdown
@@ -44,6 +49,33 @@ const textPlugin = {
             [pluginText._apiHandler],
         );
 
+        /**
+         * call common for charts & selectors adjust function for widget
+         */
+        const adjustLayout = React.useCallback(
+            (needSetDefault) => {
+                dashkitAdjustWidgetLayout({
+                    widgetId: props.id,
+                    needSetDefault,
+                    rootNode: rootNodeRef,
+                    gridLayout: props.gridLayout,
+                    layout: props.layout,
+                    cb: props.adjustWidgetLayout,
+                    mainNodeSelector: `.${YFM_MARKDOWN_CLASSNAME}.${b()}`,
+                    scrollableNodeSelector: `.${YFM_MARKDOWN_CLASSNAME} .${YFM_MARKDOWN_CLASSNAME}`,
+                });
+            },
+            [props.id, rootNodeRef, props.adjustWidgetLayout, props.layout, props.gridLayout],
+        );
+
+        /**
+         * call adjust function after all text was rendered (ketex formulas, markdown, etc)
+         * and after cut opened/closed
+         */
+        const handleTextRender = React.useCallback(() => {
+            adjustLayout(!props.data.autoHeight);
+        }, [props.data.autoHeight, adjustLayout]);
+
         const content = <PluginText {...props} apiHandler={textHandler} ref={forwardedRef} />;
 
         /**
@@ -53,11 +85,40 @@ const textPlugin = {
             setRandomId(String(getRandomCKId()));
         }, [textReady]);
 
+        /**
+         * watching content changes to check if adjustLayout needed for autoheight widgets update
+         */
+        React.useEffect(() => {
+            if (!mutationObserver) {
+                return;
+            }
+            mutationObserver.current = new MutationObserver(handleTextRender);
+
+            if (mutationObserver.current && cutNodesRef.current) {
+                cutNodesRef.current.forEach((cutNode) => {
+                    mutationObserver.current?.observe(cutNode, {
+                        attributes: true,
+                        attributeFilter: ['class'],
+                    });
+                });
+            }
+            return () => {
+                mutationObserver.current?.disconnect();
+            };
+        }, [handleTextRender, mutationObserver, rootNodeRef, cutNodesRef.current]);
+
+        const nodes = rootNodeRef.current?.querySelectorAll(`.${YFM_MARKDOWN_CLASSNAME}-cut`);
+
+        if (nodes?.length && !cutNodesRef.current) {
+            cutNodesRef.current = nodes;
+        }
+
         return (
-            <RendererWrapper type="text">
+            <RendererWrapper type="text" nodeRef={rootNodeRef}>
                 <YfmWrapper
                     content={<div className={b('content-wrap', null, randomId)}>{content}</div>}
                     className={b()}
+                    onRenderCallback={handleTextRender}
                 />
             </RendererWrapper>
         );
