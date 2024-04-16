4e5ca3fa6895ac3326b8395bb815bf94e5bea383
Elena Martynova <marginy@yandex-team.ru>
2024-04-16 17:43:39 +0300

    Add markdown colorify

--- src/shared/constants/yfm.ts
+++ src/shared/constants/yfm.ts
@@ -1,5 +1,7 @@
 export const MAGICLINK_CLASSNAME = 'magiclinks';
 
+export const YFM_COLORIFY_MARKDOWN_CLASSNAME = 'yfm-colorify';
+
 export const enum YfmQa {
     WrapperHtml = 'yfm-wrapper-html',
 }
