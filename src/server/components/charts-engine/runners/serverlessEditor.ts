af2d43f90dfc9562a574e577a457139205a0743d
Andrey Melikhov <melikhov@yandex-team.ru>
2024-05-14 15:10:20 +0300

    wip

--- src/server/components/charts-engine/runners/serverlessEditor.ts
+++ src/server/components/charts-engine/runners/serverlessEditor.ts
@@ -23,7 +23,7 @@ export const runServerlessEditor = (
 
     const iamToken = res?.locals?.iamToken ?? req.headers[ctx.config.headersMap.subjectToken];
 
-    const processorParams: Omit<ProcessorParams, 'ctx' | 'builder' | 'hooks'> = {
+    const processorParams: Omit<ProcessorParams, 'ctx' | 'builder'> = {
         chartsEngine,
         paramsOverride: params,
         actionParamsOverride: actionParams,
