12e1643c372140d55592be212a4d2fbdb0aac901
Irina V. Kuzmina <kuzmadom@yandex-team.ru>
2024-05-22 17:32:47 +0300

    change visualiztion type

--- src/ui/units/wizard/reducers/visualization.ts
+++ src/ui/units/wizard/reducers/visualization.ts
@@ -273,6 +273,10 @@ export function visualization(
                         break;
                     }
 
+                    case 'pie-d3-pie':
+                    case 'pie-pie-d3':
+                    case 'donut-d3-donut':
+                    case 'donut-donut-d3':
                     case 'pie-d3-donut-d3':
                     case 'donut-d3-pie-d3':
                     case 'pie-donut':
