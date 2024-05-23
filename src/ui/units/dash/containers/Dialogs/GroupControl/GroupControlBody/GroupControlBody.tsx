e0dbe0efa40735b8c0f7bd01b0b9a8e4fb6412f5
Irina Kuzmina <kuzmadom@yandex-team.ru>
2024-05-23 18:19:57 +0300

    Remove FieldHint flag usage (#1030)

--- src/ui/units/dash/containers/Dialogs/GroupControl/GroupControlBody/GroupControlBody.tsx
+++ src/ui/units/dash/containers/Dialogs/GroupControl/GroupControlBody/GroupControlBody.tsx
@@ -4,8 +4,6 @@ import {FormRow} from '@gravity-ui/components';
 import block from 'bem-cn-lite';
 import {I18n} from 'i18n';
 
-import {Feature} from '../../../../../../../shared';
-import {Utils} from '../../../../../../../ui';
 import {HintRow} from '../../Control2/Sections/AppearanceSection/Rows/HintRow/HintRow';
 import {InnerTitleRow} from '../../Control2/Sections/AppearanceSection/Rows/InnerTitleRow/InnerTitleRow';
 import {TitleRow} from '../../Control2/Sections/AppearanceSection/Rows/TitleRow/TitleRow';
@@ -18,8 +16,6 @@ const b = block('group-control-dialog');
 const i18n = I18n.keyset('dash.group-controls-dialog.edit');
 
 export const GroupControlBody = () => {
-    const canSetHint = Utils.isEnabledFeature(Feature.FieldHint);
-
     return (
         <React.Fragment>
             <FormRow label={i18n('label_source')}>
@@ -31,11 +27,9 @@ export const GroupControlBody = () => {
             <div className={b('section')}>
                 <InnerTitleRow />
             </div>
-            {canSetHint && (
-                <div className={b('section')}>
-                    <HintRow />
-                </div>
-            )}
+            <div className={b('section')}>
+                <HintRow />
+            </div>
             <div className={b('section')}>
                 <CommonSettingsSection />
             </div>
