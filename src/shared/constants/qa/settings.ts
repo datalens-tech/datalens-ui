f83f77b99f76d771046ccbce84a7c76e1114ecf0
Irina Kuzmina <kuzmadom@yandex-team.ru>
2024-05-29 12:36:37 +0300

    Custom palette for linear indicator (#1052)

---
+++ src/shared/constants/qa/settings.ts
@@ -0,0 +1,12 @@
+export const enum ServiceSettingsQA {
+    ColorPalettes = 'color-palettes',
+    AddPaletteButton = 'add-palette-btn',
+}
+
+export const enum PaletteEditorQA {
+    PaletteNameInput = 'palette-name',
+    RemoveColorButton = 'remove-color-btn',
+    AddColorButton = 'add-color-btn',
+    ColorTextInput = 'color-input',
+    ApplyButton = 'apply-btn',
+}
