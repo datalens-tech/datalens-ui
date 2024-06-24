8ebe280a2dc72d62ebbd797e4f0c360cf3d2492f
Irina Kuzmina <kuzmadom@yandex-team.ru>
2024-06-24 17:10:59 +0300

    Fix e2e geo test (#1164)

--- tests/suites/wizard/geolayer/geopoints/addMeasure.test.ts
+++ tests/suites/wizard/geolayer/geopoints/addMeasure.test.ts
@@ -1,39 +1,22 @@
-import {Page} from '@playwright/test';
+import {expect, Page} from '@playwright/test';
 
 import {PlaceholderName} from '../../../../page-objects/wizard/SectionVisualization';
 import WizardPage from '../../../../page-objects/wizard/WizardPage';
-import {getStylesFromString, openTestPage} from '../../../../utils';
+import {openTestPage} from '../../../../utils';
 import {RobotChartsWizardUrls} from '../../../../utils/constants';
 import datalensTest from '../../../../utils/playwright/globalTestDefinition';
 
 datalensTest.describe('Wizard - Geo Points', () => {
     datalensTest('Adding Size', async ({page}: {page: Page}) => {
-        const wizardPage = new WizardPage({
-            page,
-        });
+        const defaultPointSize = '14px';
 
+        const wizardPage = new WizardPage({page});
         await openTestPage(page, RobotChartsWizardUrls.WizardGeopoints);
 
-        let point = await page.waitForSelector(wizardPage.chartkit.geopointSelector);
-        let styleAttr = await point?.getAttribute('style');
-        const sizeBeforeDimension = getStylesFromString(styleAttr || '').height;
-
-        if (!sizeBeforeDimension) {
-            throw new Error("Couldn't get the point size BEFORE setting the size");
-        }
+        const point = page.locator(wizardPage.chartkit.geopointSelector).first();
+        await expect(point).toHaveCSS('height', defaultPointSize);
 
         await wizardPage.sectionVisualization.addFieldByClick(PlaceholderName.Size, '_someMeasure');
-
-        await wizardPage.chartkit.waitForMapReady();
-
-        point = await page.waitForSelector(wizardPage.chartkit.geopointSelector);
-        styleAttr = await point?.getAttribute('style');
-        const sizeAfterDimension = getStylesFromString(styleAttr || '').height;
-
-        if (!sizeAfterDimension) {
-            throw new Error("Couldn't get the point size AFTER setting the size");
-        }
-
-        expect(sizeBeforeDimension).not.toEqual(sizeAfterDimension);
+        await expect(point).not.toHaveCSS('height', defaultPointSize);
     });
 });
