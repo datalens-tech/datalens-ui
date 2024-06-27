7c98fa87067501021a5a8888d4eb64f050d63c09
leutinatasya <leutinatasya@yandex-team.ru>
2024-06-27 11:38:20 +0300

    Remove datasets from wizard config type

--- tests/types/config/wizard.ts
+++ tests/types/config/wizard.ts
@@ -1,13 +1,4 @@
-type Dataset = {
-    id: string;
-    name: string;
-};
-
 export type WizardParametrizationConfig = {
-    datasets?: {
-        Basic: Dataset;
-        Orders: Dataset;
-    };
     urls: {
         NewWizardChart: string;
         WizardBasicDataset: string;
