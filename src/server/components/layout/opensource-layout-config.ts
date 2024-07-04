350491af5ac6ab236654cb790410abc957e9865c
Александр Краснов <a-krasnov@it-serv.ru>
2024-07-04 16:53:47 +0300

    Исправлены ссылки на внешние ресурсы

--- src/server/components/layout/opensource-layout-config.ts
+++ src/server/components/layout/opensource-layout-config.ts
@@ -93,12 +93,12 @@ export const getOpensourceLayoutConfig: GetLayoutConfig = async (args) => {
             {
                 href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
                 rel: 'stylesheet',
-                integrity: 'sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH',
+                //integrity: 'sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH',
                 crossOrigin: 'anonymous'
             },
             {
                 href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js',
-                integrity: 'sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy',
+                //integrity: 'sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy',
                 crossOrigin: 'anonymous'
             }
         ],
