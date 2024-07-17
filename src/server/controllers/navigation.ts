e5c3a5681da454c31774244c9ac0c3cf05361fe1
Artem Panchuk <artemipanchuk@gmail.com>
2024-07-17 14:51:51 +0300

    Other navigation improvements

--- src/server/controllers/navigation.ts
+++ src/server/controllers/navigation.ts
@@ -1,6 +1,6 @@
 import type {Request, Response} from '@gravity-ui/expresskit';
 
-import {TENANT_ID_HEADER} from '../../shared';
+import {EntryScope, TENANT_ID_HEADER} from '../../shared';
 import {registry} from '../registry';
 import type {DatalensGatewaySchemas} from '../types/gateway';
 import Utils from '../utils';
@@ -46,23 +46,24 @@ export default async (req: Request, res: Response): Promise<void> => {
             ctx.logError('GET_ENTRY_BY_KEY_FAILED', error);
 
             res.send(res.renderDatalensLayout(layoutConfig));
+
             return;
         }
     }
 
     if (entry) {
         switch (entry.scope) {
-            case 'folder':
+            case EntryScope.Folder:
                 return res.redirect(`/navigation/${entry.entryId}`);
-            case 'dataset':
+            case EntryScope.Dataset:
                 return res.redirect(`/datasets/${entry.entryId}`);
-            case 'widget':
+            case EntryScope.Widget:
                 return res.redirect(`/wizard/${entry.entryId}`);
-            case 'dash':
+            case EntryScope.Dash:
                 return res.redirect(`/${entry.entryId}`);
-            case 'connection':
+            case EntryScope.Connection:
                 return res.redirect(`/connections/${entry.entryId}`);
-            case 'presentation':
+            case EntryScope.Presentation:
                 return res.redirect(`/presentations/${entry.entryId}`);
             default:
                 return res.redirect('/navigation');
