c5f609d3d74eed41d2b9ea5fb7de9e0063131922
Andrey Melikhov <melikhov@yandex-team.ru>
2024-07-19 12:09:35 +0300

    Add sync timeouts for isolated sandbox API's (#1270)

--- src/server/components/charts-engine/components/processor/isolated-sandbox/interop/libs/datalens-v3-prepare.ts
+++ src/server/components/charts-engine/components/processor/isolated-sandbox/interop/libs/datalens-v3-prepare.ts
@@ -13,15 +13,17 @@ declare const __libsDatalensV3_buildChartsConfig: LibsDatalensV3BuildChartsConfi
 declare const __libsDatalensV3_buildGraph: LibsDatalensV3BuildGraph;
 declare const __libsDatalensV3_buildHighchartsConfig: LibsDatalensV3BuildHighchartsConfig;
 declare const __libsDatalensV3_buildD3Config: LibsDatalensV3BuildD3Config;
-
+declare const __timeout: number;
 // @ts-ignore
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const datalensV3prepareAdapter: typeof datalensModule = {
     buildSources: (args) => JSON.parse(__libsDatalensV3_buildSources(JSON.stringify(args))),
     buildChartsConfig: (args) =>
-        JSON.parse(__libsDatalensV3_buildChartsConfig(JSON.stringify(args))),
-    buildGraph: (...args) => JSON.parse(__libsDatalensV3_buildGraph(JSON.stringify(args))),
+        JSON.parse(__libsDatalensV3_buildChartsConfig(JSON.stringify(args), __timeout)),
+    buildGraph: (...args) =>
+        JSON.parse(__libsDatalensV3_buildGraph(JSON.stringify(args), __timeout)),
     buildHighchartsConfig: (args) =>
-        JSON.parse(__libsDatalensV3_buildHighchartsConfig(JSON.stringify(args))),
-    buildD3Config: (args) => JSON.parse(__libsDatalensV3_buildD3Config(JSON.stringify(args))),
+        JSON.parse(__libsDatalensV3_buildHighchartsConfig(JSON.stringify(args), __timeout)),
+    buildD3Config: (args) =>
+        JSON.parse(__libsDatalensV3_buildD3Config(JSON.stringify(args), __timeout)),
 };
