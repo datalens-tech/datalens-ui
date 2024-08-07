a860d00ac9a6f1b180196b74de9ba804626339e6
leutinatasya <leutinatasya@yandex-team.ru>
2024-08-07 14:03:14 +0300

    Fix filling hint with dataset description

--- src/ui/units/dash/store/reducers/dash.js
+++ src/ui/units/dash/store/reducers/dash.js
@@ -89,7 +89,6 @@ export function getSelectorDialogInitialState(args = {}) {
         placementMode: CONTROLS_PLACEMENT_MODE.AUTO,
         width: '',
         required: false,
-        hint: '',
         showHint: false,
         draftId: getRandomKey(),
     };
