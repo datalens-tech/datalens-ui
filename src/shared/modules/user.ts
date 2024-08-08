49b045add62943cd3c41a44756bb4aaca69e318d
Irina V. Kuzmina <kuzmadom@yandex-team.ru>
2024-08-08 15:29:18 +0300

    fix

--- src/shared/modules/user.ts
+++ src/shared/modules/user.ts
@@ -6,10 +6,7 @@ export const makeUserId = (userId: string) => `uid:${userId}`;
 export const getUserId = (userIdWithPrefix: string) => userIdWithPrefix.slice(UID_PREFIX.length);
 
 export const isUserId = (userIdOrLogin = '') => {
-    return (
-        userIdOrLogin.startsWith(UID_PREFIX) &&
-        !userIdOrLogin.replace(UID_PREFIX, '').startsWith(ANONYMOUS_USER_PREFIX)
-    );
+    return userIdOrLogin.startsWith(UID_PREFIX) && !userIdOrLogin.includes(ANONYMOUS_USER_PREFIX);
 };
 
 export const filterUsersIds = (ids: string[]) => ids.filter(isUserId).map(getUserId);
