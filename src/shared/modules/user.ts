f714fb001b7d6b10d2093fa906381f72f117361c
Irina V. Kuzmina <kuzmadom@yandex-team.ru>
2024-08-08 13:01:18 +0300

    Fix the display of the user name in userInfo

--- src/shared/modules/user.ts
+++ src/shared/modules/user.ts
@@ -1,9 +1,15 @@
 const UID_PREFIX = 'uid:';
+const ANONYMOUS_USER_PREFIX = '__ANONYMOUS';
 
 export const makeUserId = (userId: string) => `uid:${userId}`;
 
 export const getUserId = (userIdWithPrefix: string) => userIdWithPrefix.slice(UID_PREFIX.length);
 
-export const isUserId = (userIdOrLogin = '') => userIdOrLogin.startsWith(UID_PREFIX);
+export const isUserId = (userIdOrLogin = '') => {
+    return (
+        userIdOrLogin.startsWith(UID_PREFIX) &&
+        !userIdOrLogin.replace(UID_PREFIX, '').startsWith(ANONYMOUS_USER_PREFIX)
+    );
+};
 
 export const filterUsersIds = (ids: string[]) => ids.filter(isUserId).map(getUserId);
