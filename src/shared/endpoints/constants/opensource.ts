93f650fde3c56145638db1137f95be5386517742
Georgy Malkov <malkovgv@yandex-team.ru>
2024-08-12 19:08:48 +0300

    Fix production docker port

--- src/shared/endpoints/constants/opensource.ts
+++ src/shared/endpoints/constants/opensource.ts
@@ -31,7 +31,7 @@ export const opensourceEndpoints = {
             us: process.env.US_ENDPOINT || 'http://us:8083',
             bi: process.env.BI_API_ENDPOINT || 'http://control-api:8080',
             biData: process.env.BI_DATA_ENDPOINT || 'http://data-api:8080',
-            biConverter: process.env.BI_FILE_UPLOADER_ENDPOINT || 'http://localhost:8084',
+            biConverter: process.env.BI_FILE_UPLOADER_ENDPOINT || 'http://file-uploader:8080',
 
             charts: process.env.CHARTS_ENDPOINT || '/',
             uploader: process.env.UI_UPLOADER_ENDPOINT || '/uploader',
