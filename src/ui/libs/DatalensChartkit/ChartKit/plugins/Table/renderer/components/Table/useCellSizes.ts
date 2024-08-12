c66f403d60ab5038c1c3cd89e85f6ddb49b06f81
Irina V. Kuzmina <kuzmadom@yandex-team.ru>
2024-08-12 18:19:08 +0300

    fix table size

--- src/ui/libs/DatalensChartkit/ChartKit/plugins/Table/renderer/components/Table/useCellSizes.ts
+++ src/ui/libs/DatalensChartkit/ChartKit/plugins/Table/renderer/components/Table/useCellSizes.ts
@@ -1,6 +1,50 @@
 import React from 'react';
 
-import findLast from 'lodash/findLast';
+function getTableSizes(rows: HTMLTableRowElement[]) {
+    const colsCount = Array.from(rows[0]?.childNodes ?? []).reduce((sum, c) => {
+        const colSpan = Number((c as Element).getAttribute('colSpan') || 1);
+        return sum + colSpan;
+    }, 0);
+    const result = new Array(rows.length).fill(null).map(() => new Array(colsCount).fill(null));
+
+    result.forEach((_r, rowIndex) => {
+        const row = rows[rowIndex];
+        let cellIndex = 0;
+        Array.from(row.childNodes ?? []).forEach((c, index) => {
+            const cell = c as Element;
+            let rowSpan = Number(cell.getAttribute('rowSpan') || 1);
+            let colSpan = Number(cell.getAttribute('colSpan') || 1);
+            const cellWidth = cell.getBoundingClientRect()?.width + (index === 0 ? 1 : 0);
+
+            if (result[rowIndex][cellIndex] !== null) {
+                cellIndex = result[rowIndex].findIndex((val, i) => i > cellIndex && val === null);
+            }
+
+            while (rowSpan - 1 > 0) {
+                rowSpan -= 1;
+                result[rowIndex + rowSpan][cellIndex] = cellWidth;
+            }
+
+            if (colSpan > 1) {
+                while (colSpan > 1) {
+                    colSpan -= 1;
+                    cellIndex += 1;
+                }
+            } else {
+                result[rowIndex][cellIndex] = cellWidth;
+            }
+
+            cellIndex += 1;
+        });
+    });
+
+    return result.reduce<number[]>((acc, row) => {
+        row.forEach((cellWidth, index) => {
+            acc[index] = acc[index] || cellWidth;
+        });
+        return acc;
+    }, []);
+}
 
 export const useCellSizes = (
     args: {
@@ -15,18 +59,14 @@ export const useCellSizes = (
         if (!cellSizes) {
             const container = tableContainerRef?.current as Element;
             const table = container?.getElementsByTagName('table')?.[0];
-            const tRows = Array.from(table?.getElementsByTagName('tr') ?? []);
-            const tRow = findLast(tRows, (r) =>
-                Array.from(r.childNodes).every(
-                    (td) => Number((td as Element).getAttribute('colSpan') || 1) <= 1,
-                ),
+            const tHeadRows = Array.from(
+                table?.getElementsByTagName('thead')?.[0]?.childNodes ?? [],
             );
 
-            const result = Array.from(tRow?.childNodes ?? []).map((tCell, index) => {
-                return (tCell as Element).getBoundingClientRect()?.width + (index === 0 ? 1 : 0);
-            });
-
-            setCellSizes(result);
+            if (tHeadRows.length) {
+                const sizes = getTableSizes(tHeadRows as HTMLTableRowElement[]);
+                setCellSizes(sizes);
+            }
         }
     }, [cellSizes, tableContainerRef]);
 
