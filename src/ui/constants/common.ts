4d0b67b524f0f1e2b39f353052726640825ac414
Artem Panchuk <artemipanchuk@gmail.com>
2024-08-13 18:50:17 +0300

    Use old Scope

--- src/ui/constants/common.ts
+++ src/ui/constants/common.ts
@@ -284,7 +284,7 @@ export enum Scope {
     Config = 'config',
 }
 
-export const ALL_SCOPES: string[] = [...Object.values(EntryScope), EntryScope.Presentation];
+export const ALL_SCOPES: string[] = [...Object.values(Scope), EntryScope.Presentation];
 
 export const SCOPE_WITH_EXECUTE: string[] = [Scope.Connection, Scope.Dataset];
 
