70c8968efaf546422f7288998cec2d724914a5e3
Andrey Melikhov <melikhov@yandex-team.ru>
2024-08-19 17:36:01 +0300

    Move params into isolate (#1374)

--- src/server/components/charts-engine/components/processor/types.ts
+++ src/server/components/charts-engine/components/processor/types.ts
@@ -149,6 +149,7 @@ export type ChartBuilder = {
     }) => Promise<Record<string, ChartBuilderResult>>;
     buildParams: (args: {
         params: StringParams;
+        usedParams: StringParams;
         actionParams: StringParams;
         hooks: ProcessorHooks;
     }) => Promise<ChartBuilderResult>;
@@ -173,12 +174,14 @@ export type ChartBuilder = {
         data: unknown;
         sources?: Record<string, DataFetcherResult>;
         params: StringParams;
+        usedParams: StringParams;
         actionParams: StringParams;
         hooks: ProcessorHooks;
     }) => Promise<ChartBuilderResult>;
     buildUI: (args: {
         data?: unknown;
         params: StringParams;
+        usedParams: StringParams;
         actionParams: StringParams;
         hooks: ProcessorHooks;
     }) => Promise<ChartBuilderResult>;
