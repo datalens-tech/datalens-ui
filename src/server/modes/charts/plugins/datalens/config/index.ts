c5fbb90dba783c73d6b5f8f21590eab2ef371667
Irina V. Kuzmina <kuzmadom@yandex-team.ru>
2024-08-19 11:56:56 +0300

    Clean the Wizard modules from registry

---
+++ src/server/modes/charts/plugins/datalens/config/index.ts
@@ -0,0 +1,32 @@
+import type {DashWidgetConfig, ServerChartsConfig, StringParams} from '../../../../../../shared';
+import {getServerFeatures} from '../../../../../../shared';
+import {registry} from '../../../../../registry';
+
+import {buildChartsConfigPrivate} from './config';
+import type {BuildChartConfigArgs} from './types';
+
+export const buildChartsConfig = (
+    args: BuildChartConfigArgs | ServerChartsConfig,
+    _params?: StringParams,
+) => {
+    let shared;
+    let params: StringParams;
+    let widgetConfig: DashWidgetConfig['widgetConfig'];
+
+    if ('shared' in args) {
+        shared = args.shared;
+        params = args.params as StringParams;
+        widgetConfig = args.widgetConfig;
+    } else {
+        shared = args;
+        params = _params as StringParams;
+    }
+
+    const app = registry.getApp();
+    return buildChartsConfigPrivate({
+        shared,
+        params,
+        widgetConfig,
+        features: getServerFeatures(app.nodekit.ctx),
+    });
+};
