c87d0918d0fe1b6269b293bbe93402819f97d451
leutinatasya <leutinatasya@yandex-team.ru>
2024-08-21 15:47:30 +0300

    Support bold in Interpolated text

--- src/ui/components/InterpolatedText/InterpolatedText.tsx
+++ src/ui/components/InterpolatedText/InterpolatedText.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 
 import {Link} from '@gravity-ui/uikit';
+import block from 'bem-cn-lite';
 
 import {Interpolate} from '../Interpolate/Interpolate';
+
+import './InterpolatedText.scss';
+
+const b = block('interpolated-text');
 
 type InterpolatedTextProps = {
     text: string;
     href?: string;
     br?: boolean;
+    bold?: boolean;
 };
 
-const InterpolatedTextComponent = ({text, href, br}: InterpolatedTextProps) => {
+const InterpolatedTextComponent = ({text, href, br, bold}: InterpolatedTextProps) => {
     const matches: Record<string, (match: string) => React.ReactNode> = {};
 
     if (href) {
@@ -26,6 +32,12 @@ const InterpolatedTextComponent = ({text, href, br}: InterpolatedTextProps) => {
     if (br) {
         matches.br = () => {
             return <br />;
+        };
+    }
+
+    if (bold) {
+        matches.b = (match) => {
+            return <span className={b('text-selected')}>{match}</span>;
         };
     }
 
