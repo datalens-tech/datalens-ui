71dc19c47f41e474322bc9355cacea9ccd7758b1
Elena Martynova <marginy@yandex-team.ru>
2024-08-27 12:15:55 +0300

    Add dash control runner (#1406)

---
+++ src/server/components/charts-engine/components/processor/control-builder.ts
@@ -0,0 +1,150 @@
+import type {StringParams} from '../../../../../shared';
+import {buildUI} from '../../../../modes/charts/plugins/control/controls';
+import {buildGraph} from '../../../../modes/charts/plugins/control/js';
+import type {SourceResponseData} from '../../../../modes/charts/plugins/control/js/types';
+import type {ControlShared} from '../../../../modes/charts/plugins/control/types';
+import {buildSources} from '../../../../modes/charts/plugins/control/url';
+
+import {getChartApiContext} from './chart-api-context';
+import type {ChartBuilder} from './types';
+
+type ChartBuilderArgs = {
+    config: {
+        data: {
+            shared: string;
+        };
+    };
+};
+
+export const getControlBuilder = async (args: ChartBuilderArgs): Promise<ChartBuilder> => {
+    const {config} = args;
+    let shared: ControlShared;
+
+    // Nothing happens here - just for compatibility with the editor
+    const emptyStep = (name: string) => async (options: {params: StringParams}) => {
+        const {params} = options;
+        const timeStart = process.hrtime();
+        const context = getChartApiContext({
+            name,
+            shared,
+            params,
+            actionParams: {} as Record<string, string | string[]>,
+            userLang: null,
+        });
+
+        return {
+            exports: {},
+            executionTiming: process.hrtime(timeStart),
+            name,
+            runtimeMetadata: context.__runtimeMetadata,
+        };
+    };
+
+    const ChartBuilder: ChartBuilder = {
+        buildShared: async () => {
+            if (typeof config.data.shared === 'string') {
+                shared = JSON.parse(config.data.shared);
+            } else {
+                shared = config.data.shared;
+            }
+        },
+        buildModules: async () => {
+            return {};
+        },
+        buildParams: async (options) => {
+            const {params} = options;
+            const timeStart = process.hrtime();
+            const context = getChartApiContext({
+                name: 'Params',
+                shared,
+                params,
+                actionParams: {} as Record<string, string | string[]>,
+                userLang: null,
+            });
+            return {
+                exports: {},
+                executionTiming: process.hrtime(timeStart),
+                name: 'Params',
+                runtimeMetadata: context.__runtimeMetadata,
+            };
+        },
+        buildUrls: async (options) => {
+            const {params} = options;
+            const timeStart = process.hrtime();
+
+            const context = getChartApiContext({
+                name: 'Urls',
+                shared,
+                params,
+                actionParams: {} as Record<string, string | string[]>,
+                userLang: null,
+            });
+
+            return {
+                exports: buildSources({
+                    shared,
+                    params,
+                }),
+                executionTiming: process.hrtime(timeStart),
+                name: 'Urls',
+                runtimeMetadata: context.__runtimeMetadata,
+            };
+        },
+
+        buildChartLibraryConfig: emptyStep('Highcharts'),
+
+        buildChartConfig: emptyStep('Config'),
+
+        buildChart: async (options) => {
+            const {data, params} = options;
+            const timeStart = process.hrtime();
+
+            const context = getChartApiContext({
+                name: 'JavaScript',
+                shared,
+                params,
+                actionParams: {} as Record<string, string | string[]>,
+                userLang: null,
+            });
+
+            buildGraph({
+                data: data as unknown as SourceResponseData,
+                shared,
+                params,
+                ChartEditor: context.ChartEditor,
+            });
+
+            return {
+                exports: {},
+                executionTiming: process.hrtime(timeStart),
+                name: 'JavaScript',
+                runtimeMetadata: context.__runtimeMetadata,
+            };
+        },
+
+        buildUI: async (options) => {
+            const {params} = options;
+            const timeStart = process.hrtime();
+
+            const context = getChartApiContext({
+                name: 'UI',
+                shared: shared,
+                params: params,
+                actionParams: {} as Record<string, string | string[]>,
+                userLang: null,
+            });
+
+            return {
+                exports: buildUI({
+                    shared: shared,
+                }),
+                executionTiming: process.hrtime(timeStart),
+                name: 'UI',
+                runtimeMetadata: context.__runtimeMetadata,
+            };
+        },
+        dispose: () => {},
+    };
+
+    return ChartBuilder;
+};
