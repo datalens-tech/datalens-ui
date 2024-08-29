72faf18404a5bab5e082074ff76fceec03926904
Irina V. Kuzmina <kuzmadom@yandex-team.ru>
2024-08-29 17:59:25 +0300

    Enable EnableEditHistory

--- src/server/components/features/features-list/EnableEditHistory.ts
+++ src/server/components/features/features-list/EnableEditHistory.ts
@@ -5,6 +5,6 @@ export default createFeatureConfig({
     name: Feature.EnableEditHistory,
     state: {
         development: true,
-        production: false,
+        production: true,
     },
 });
