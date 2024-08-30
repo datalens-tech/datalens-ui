c9d251a002cab6ce469c61800523f3315b1a8460
Andrey Melikhov <melikhov@yandex-team.ru>
2024-08-30 12:57:02 +0300

    Fix jsonfn arrow

--- src/server/components/charts-engine/components/processor/isolated-sandbox/utils.ts
+++ src/server/components/charts-engine/components/processor/isolated-sandbox/utils.ts
@@ -27,12 +27,23 @@ export const safeStringify = (
             return replaced;
         }
 
-        if (typeof value === 'function') {
+        if (value instanceof Function || typeof value == 'function') {
             if (isConsole) {
                 return;
             }
-            return jsonFn ? value.toString() : undefined;
+            if (jsonFn) {
+                // JSONfn compatibility mode
+                // https://github.com/vkiryukhin/jsonfn/blob/299b4beb4818f7ca98f5e01f0d2a6675b8b2e4ac/jsonfn.js#L38
+                const fnBody = value.toString();
+                //this is ES6 Arrow Function
+                if (fnBody.length < 8 || fnBody.substring(0, 8) !== 'function') {
+                    return '_NuFrRa_' + fnBody;
+                }
+                return fnBody;
+            }
+            return undefined;
         }
+
         return value;
     }
     return replaceNonTransferable(value);
