2e89de6120cd3b35913874505b5ea90028287e72
Irina Kuzmina <kuzmadom@yandex-team.ru>
2024-09-05 13:48:50 +0300

    Fix QL chart runner params (#1464)

---
+++ src/server/modes/charts/plugins/ql/utils/identify-params.ts
@@ -0,0 +1,40 @@
+import type {QlExtendedConfig, StringParams} from '../../../../../../shared';
+import {QLChartType} from '../../../../../../shared';
+import {mapQlConfigToLatestVersion} from '../../../../../../shared/modules/config/ql';
+import type {GetTranslationFn} from '../../../../../../shared/types/language';
+
+export function identifyParams(args: {getTranslation: GetTranslationFn; chart: QlExtendedConfig}) {
+    const {chart, getTranslation} = args;
+
+    const config = mapQlConfigToLatestVersion(chart, {
+        i18n: getTranslation,
+    });
+    const {chartType, params} = config;
+
+    const availableParams: StringParams = {};
+
+    if (params) {
+        params.forEach((param) => {
+            if (
+                param.type.includes('interval') &&
+                typeof param.defaultValue === 'object' &&
+                param.defaultValue !== null
+            ) {
+                const fromName = `${param.name}_from`;
+                const toName = `${param.name}_to`;
+
+                availableParams[`${param.name}`] = '';
+                availableParams[fromName] = '';
+                availableParams[toName] = '';
+            } else {
+                availableParams[param.name] = '';
+            }
+        });
+    }
+
+    if (chartType === QLChartType.Monitoringql) {
+        availableParams['interval'] = '';
+    }
+
+    return availableParams;
+}
