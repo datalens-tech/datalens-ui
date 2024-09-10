1326c7096d0583af3b26a28ff42d486970e8cf80
Andrey Melikhov <melikhov@yandex-team.ru>
2024-09-10 14:59:31 +0300

    Add libs/qlchart/v1 bundle

--- src/server/components/charts-engine/components/processor/isolated-sandbox/interop/bundled-libs.ts
+++ src/server/components/charts-engine/components/processor/isolated-sandbox/interop/bundled-libs.ts
@@ -1,4 +1,6 @@
 import controlModule from '../../../../../../modes/charts/plugins/control';
 import datalensModule from '../../../../../../modes/charts/plugins/datalens/chart-editor-module';
 import datasetModule from '../../../../../../modes/charts/plugins/dataset/v2';
-export {datasetModule, controlModule, datalensModule};
+import qlModule from '../../../../../../modes/charts/plugins/ql/module/chart-editor-module';
+
+export {datasetModule, controlModule, datalensModule, qlModule};
