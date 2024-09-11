3abde632b3efbf6e2650e78d0e22fb07e376f16b
Artem Panchuk <artemipanchuk@gmail.com>
2024-09-11 17:09:28 +0300

    Move routine to migrate

--- src/ui/utils/migrateItemDataOnPaste.ts
+++ src/ui/utils/migrateItemDataOnPaste.ts
@@ -1,5 +1,35 @@
+import type {ConfigItemData} from '@gravity-ui/dashkit';
+import update from 'immutability-helper';
+import type {EntryScope} from 'shared';
+import {ITEM_TYPE} from 'ui/constants/dialogs';
 import type {CopiedConfigData} from 'ui/units/dash/modules/helpers';
 
-export const migrateItemDataOnPaste = ({itemData}: {itemData: CopiedConfigData}) => {
-    return itemData;
+export const migrateItemDataOnPaste = ({
+    itemData,
+}: {
+    itemData: CopiedConfigData;
+    toScope: EntryScope;
+}) => {
+    const migratedItemData = {
+        ...itemData,
+        data: update(itemData.data, {$unset: ['id']}),
+    };
+
+    if (itemData.type === ITEM_TYPE.WIDGET) {
+        migratedItemData.data.tabs =
+            itemData.data.tabs &&
+            itemData.data.tabs.map((tab) => {
+                return update(tab, {
+                    $unset: ['id'],
+                });
+            });
+    }
+
+    if (itemData.type === ITEM_TYPE.GROUP_CONTROL) {
+        migratedItemData.data.group = (itemData as ConfigItemData).group?.map((item) => {
+            return update(item, {$unset: ['id']});
+        });
+    }
+
+    return migratedItemData;
 };
