7814fb9af3f21646aed57bfd68894234e11cd91f
Irina V. Kuzmina <kuzmadom@yandex-team.ru>
2024-09-11 15:50:02 +0300

    Fixed incorrect application of color(markup) in the table when updating filters

--- src/ui/libs/DatalensChartkit/ChartKit/plugins/Table/renderer/components/MarkupCell/MarkupCell.tsx
+++ src/ui/libs/DatalensChartkit/ChartKit/plugins/Table/renderer/components/MarkupCell/MarkupCell.tsx
@@ -9,20 +9,24 @@ type MarkupCellProps = {
 
 export const MarkupCell = (props: MarkupCellProps) => {
     const {cell} = props;
+    const key = JSON.stringify(cell?.value ?? {});
 
     return (
-        <Markup
-            item={cell?.value as MarkupItem}
-            externalProps={{
-                url: {
-                    onClick: (event: React.SyntheticEvent) => {
-                        // need to stop propagation for link components because it works incorrect with sorting by rows
-                        // user click by link it leads to call both actions at the same time
-                        // now clicking on the link will only open it without sorting table
-                        event.stopPropagation();
+        <React.Fragment>
+            <Markup
+                key={key}
+                item={cell?.value as MarkupItem}
+                externalProps={{
+                    url: {
+                        onClick: (event: React.SyntheticEvent) => {
+                            // need to stop propagation for link components because it works incorrect with sorting by rows
+                            // user click by link it leads to call both actions at the same time
+                            // now clicking on the link will only open it without sorting table
+                            event.stopPropagation();
+                        },
                     },
-                },
-            }}
-        />
+                }}
+            />
+        </React.Fragment>
     );
 };
