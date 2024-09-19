542af0f58171b7620d8a68b0fcd266cd067915ba
Serge Pavlyuk <sergeypavlyuk@yandex-team.ru>
2024-09-19 18:36:40 +0300

    Add isRendered check added for charts and update on background change (#1566)

--- src/ui/components/Widgets/Chart/hooks/useLoadingChartWidget.ts
+++ src/ui/components/Widgets/Chart/hooks/useLoadingChartWidget.ts
@@ -121,6 +121,7 @@ export const useLoadingChartWidget = (props: LoadingChartWidgetHookProps) => {
     const [scrollOffset, setScrollOffset] = React.useState<number | null>(null);
     const [loadedWidgetType, setLoadedWidgetType] = React.useState<string>('');
     const [isLoadedWidgetWizard, setIsLoadedWidgetWizard] = React.useState(false);
+    const [isRendered, setIsRendered] = React.useState(false);
 
     const resolveMetaDataRef = React.useRef<ResolveMetaDataRef>();
     const resolveWidgetDataRef = React.useRef<ResolveWidgetDataRef>();
@@ -166,6 +167,7 @@ export const useLoadingChartWidget = (props: LoadingChartWidgetHookProps) => {
                     : false;
 
             adjustLayout(!newAutoHeight);
+            setIsRendered(true);
         },
         [dataProvider, tabs, tabIndex, adjustLayout, loadedWidgetType],
     );
@@ -353,7 +355,7 @@ export const useLoadingChartWidget = (props: LoadingChartWidgetHookProps) => {
      * debounced call of chartkit reflow
      */
     const isReadyToReflowRef = React.useRef(false);
-    isReadyToReflowRef.current = isInit && !isLoading;
+    isReadyToReflowRef.current = isInit && !isLoading && isRendered;
     const debouncedChartReflow = React.useCallback(
         debounce(() => {
             handleChartkitReflow();
@@ -397,11 +399,11 @@ export const useLoadingChartWidget = (props: LoadingChartWidgetHookProps) => {
      */
     const currentLayout = layout.find(({i}) => i === widgetId);
     React.useEffect(() => {
-        if (isInit && !isLoading) {
+        if (isInit && !isLoading && isRendered) {
             handleUpdate?.();
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [currentLayout?.x, currentLayout?.y, isLoading, isInit, handleUpdate]);
+    }, [currentLayout?.x, currentLayout?.y, isLoading, isInit, isRendered, handleUpdate]);
 
     /**
      * updating widget description by markdown
