befddc7911dd1c6231f55402a3489006e9698954
Stepan Pavlenko <stpavlenko@yandex-team.ru>
2024-09-20 13:36:44 +0300

    Removed React.FC

--- src/ui/units/datasets/components/DatasetTable/components/BatchActionPanel/components/DialogChangeDatasetFields/DialogChangeDatasetFields.tsx
+++ src/ui/units/datasets/components/DatasetTable/components/BatchActionPanel/components/DialogChangeDatasetFields/DialogChangeDatasetFields.tsx
@@ -37,7 +37,7 @@ export interface DialogChangeDatasetFieldsProps {
     aggregations?: DatasetFieldAggregation[];
 }
 
-export const DialogChangeDatasetFields: React.FC<DialogChangeDatasetFieldsProps> = ({
+export const DialogChangeDatasetFields = ({
     open,
     onClose,
     warningMessage,
@@ -48,7 +48,7 @@ export const DialogChangeDatasetFields: React.FC<DialogChangeDatasetFieldsProps>
     onApply,
     types = [],
     aggregations = [],
-}) => {
+}: DialogChangeDatasetFieldsProps) => {
     const typeItems = getTypeSelectOptions(types);
     const aggregationsItems: SelectOption[] = aggregations.map((aggr) => ({
         value: aggr,
