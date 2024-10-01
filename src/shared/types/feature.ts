2fe323b76e8e34af7b442d68ab6587d639c3f98f
Andrey Melikhov <melikhov@yandex-team.ru>
2024-10-01 23:42:36 +0300

    Cleanup new sandbox engine flags (#1635)

--- src/shared/types/feature.ts
+++ src/shared/types/feature.ts
@@ -69,8 +69,6 @@ export enum Feature {
     PinnedColumns = 'PinnedColumns',
     PinnedColumnsForPivotTables = 'PinnedColumnsForPivotTables',
     EnableFooter = 'EnableFooter',
-    /** Enable Isolated VM Sandbox */
-    EnableIsolatedSandbox = 'EnableIsolatedSandbox',
     MassRemoveCollectionsWorkbooks = 'MassRemoveCollectionsWorkbooks',
     /** Disable setting min-height: unset for all embedded dashes by default. Enbale min-height: unset only on message event with EMBEDDED_DASH_MESSAGE_NAME */
     RemoveEmbedUnsetDashHeight = 'RemoveEmbedUnsetDashHeight',
@@ -78,8 +76,6 @@ export enum Feature {
     EnableDashFixedHeader = 'EnableDashFixedHeader',
     /** Use BI handles for getting oauth applications tokens */
     EnableBIOAuth = 'EnableBIOAuth',
-    NewSandbox_100p = 'NewSandbox_100p',
-    SandboxEngineSelectSwitch = 'SandboxEngineSelectSwitch',
     EnableEmbedsInDialogShare = 'EnableEmbedsInDialogShare',
     EnableEntryMenuItemShare = 'EnableEntryMenuItemShare',
 }
