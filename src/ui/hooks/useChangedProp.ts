9e201f73bba6c66d41ecd0a8917f6b4fc47ee07a
Taya Leutina <leutinatasya@yandex-team.ru>
2024-10-02 13:24:31 +0300

    Support generating new requestId in selectors after inner params changed (#1632)

---
+++ src/ui/hooks/useChangedProp.ts
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import isEqual from 'lodash/isEqual';
+
+export function useChangedValue<T>(value: T, comparator: (a: T, b: T) => boolean = isEqual) {
+    const prevValueRef = React.useRef<T>(value);
+
+    const isChanged = !comparator(prevValueRef.current, value);
+
+    if (isChanged) {
+        prevValueRef.current = value;
+    }
+
+    return isChanged;
+}
