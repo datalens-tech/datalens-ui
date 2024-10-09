5d0b628ffc955b4970ab034b4d25d77074d7a8cd
Irina V. Kuzmina <kuzmadom@yandex-team.ru>
2024-10-09 13:27:26 +0300

    fix

--- src/ui/libs/DatalensChartkit/ChartKit/plugins/Table/renderer/components/Table/cell-width.ts
+++ src/ui/libs/DatalensChartkit/ChartKit/plugins/Table/renderer/components/Table/cell-width.ts
@@ -10,8 +10,10 @@ export function getCellsWidth(args: {
         (sum, col) => sum + Math.max(col.min, col.fixed ?? 0),
         0,
     );
-    const baseMinWidth = cols.reduce((sum, col) => sum + col.min, 0) + 1;
-    const k = Math.max(1, (tableMinWidth - fixedColsWidth) / (baseMinWidth - fixedColsWidth));
+    const baseMinWidth = cols.reduce((sum, col) => sum + col.min, 0);
+    // subtract 1 pixel for the right border of the table and an additional one for compatibility with the old widget
+    // (for some reason, it was not drawn to the full width, but one pixel less)
+    const k = Math.max(1, (tableMinWidth - fixedColsWidth - 2) / (baseMinWidth - fixedColsWidth));
 
     return cols.map((col) => {
         if (!shouldIgnoreFixedWidth && col.fixed) {
