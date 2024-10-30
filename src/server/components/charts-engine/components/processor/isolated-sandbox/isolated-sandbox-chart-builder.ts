7af5728a98b1ec6fd720fa8da9f0bd44c3ee360d
Andrey Melikhov <amel.truelife@gmail.com>
2024-10-30 13:17:39 +0300

    Remove registry from the chart editor

--- src/server/components/charts-engine/components/processor/isolated-sandbox/isolated-sandbox-chart-builder.ts
+++ src/server/components/charts-engine/components/processor/isolated-sandbox/isolated-sandbox-chart-builder.ts
@@ -2,9 +2,13 @@ import fs from 'fs';
 
 import ivm from 'isolated-vm';
 
-import type {DashWidgetConfig, FeatureConfig} from '../../../../../../shared';
+import type {
+    ConnectorType,
+    DashWidgetConfig,
+    FeatureConfig,
+    Palette,
+} from '../../../../../../shared';
 import {EDITOR_TYPE_CONFIG_TABS, Feature} from '../../../../../../shared';
-import {registry} from '../../../../../registry';
 import type {ChartsEngine} from '../../../index';
 import type {ResolvedConfig} from '../../storage/types';
 import {Processor} from '../index';
@@ -38,6 +42,8 @@ type IsolatedSandboxChartBuilderArgs = {
         key: string,
         params?: Record<string, string | number>,
     ) => string;
+    palettesMap: Record<string, Palette>;
+    getQLConnectionTypeMap: Record<string, ConnectorType>;
 };
 
 export const getIsolatedSandboxChartBuilder = async (
@@ -53,16 +59,14 @@ export const getIsolatedSandboxChartBuilder = async (
         workbookId,
         serverFeatures,
         getTranslation,
+        palettesMap,
+        getQLConnectionTypeMap,
     } = args;
     const type = config.meta.stype;
     let shared: Record<string, any>;
-    const getAvailablePalettesMap = registry.common.functions.get('getAvailablePalettesMap');
-
-    const palettes = getAvailablePalettesMap();
 
     const isolate = new ivm.Isolate({memoryLimit: 1024});
     const context = isolate.createContextSync();
-    const getQLConnectionTypeMap = registry.getQLConnectionTypeMap();
     context.evalSync(
         `
          // I do not know why, but this is not exists in V8 Isolate.
@@ -72,7 +76,7 @@ export const getIsolatedSandboxChartBuilder = async (
          let __usedParams;
          let __runtimeMetadata = {userParamsOverride: undefined};
          let __features = JSON.parse('${JSON.stringify(serverFeatures)}');
-         let __palettes = JSON.parse('${JSON.stringify(palettes)}');
+         let __palettes = JSON.parse('${JSON.stringify(palettesMap)}');
          let __qlConnectionTypeMap = JSON.parse('${JSON.stringify(getQLConnectionTypeMap)}');
     `,
     );
