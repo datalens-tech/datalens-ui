24a382e9e68b995d31bdeae632db0ba59a85eda6
Andrey Melikhov <melikhov@yandex-team.ru>
2024-11-08 16:16:47 +0300

    Cleanup CE files (#1791)

--- src/server/components/charts-engine/components/processor/isolated-sandbox/interop/bundled-libs.ts
+++
@@ -1,6 +0,0 @@
-import controlModule from '../../../../../../modes/charts/plugins/control';
-import datalensModule from '../../../../../../modes/charts/plugins/datalens/chart-editor-module';
-import datasetModule from '../../../../../../modes/charts/plugins/dataset/v2';
-import qlModule from '../../../../../../modes/charts/plugins/ql/module/chart-editor-module';
-
-export {datasetModule, controlModule, datalensModule, qlModule};
