203a75275a8df220ee56c4c043b93b7b12d9582b
Irina Kuzmina <kuzmadom@yandex-team.ru>
2024-11-15 14:42:52 +0300

    Convert transparent background to opaque(table cell background)

--- src/ui/libs/DatalensChartkit/ChartKit/plugins/Table/renderer/components/Table/TableBody.tsx
+++ src/ui/libs/DatalensChartkit/ChartKit/plugins/Table/renderer/components/Table/TableBody.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import block from 'bem-cn-lite';
 import {ChartKitTableQa} from 'shared';
 
-import type {BodyRowViewData, RowRef} from './types';
+import type {BodyCellViewData, BodyRowViewData, RowRef} from './types';
 
 const b = block('dl-table');
 
@@ -12,6 +12,43 @@ type Props = {
     style?: React.CSSProperties;
     onCellClick?: (event: React.MouseEvent, cell: unknown, rowId: string) => void;
     rowRef?: RowRef;
+};
+
+const TableBodyCell = (props: {
+    cell: BodyCellViewData;
+    onClick?: (event: React.MouseEvent) => void;
+}) => {
+    const {cell, onClick} = props;
+
+    return (
+        <td
+            className={b(
+                'td',
+                {
+                    type: cell.type,
+                    pinned: cell.pinned,
+                },
+                cell.className,
+            )}
+            style={{
+                ...cell.style,
+                gridRow: cell.rowSpan ? `span ${cell.rowSpan}` : undefined,
+                gridColumn: cell.colSpan ? `span ${cell.colSpan}` : undefined,
+                maxHeight: cell.maxHeight,
+            }}
+            onClick={onClick}
+            rowSpan={cell.rowSpan}
+            colSpan={cell.colSpan}
+        >
+            <div
+                className={b('cell-content', {type: cell.contentType})}
+                data-qa={ChartKitTableQa.CellContent}
+                style={cell.contentStyle}
+            >
+                {cell.content}
+            </div>
+        </td>
+    );
 };
 
 export const TableBody = React.memo<Props>((props: Props) => {
@@ -24,40 +61,15 @@ export const TableBody = React.memo<Props>((props: Props) => {
                     <tr data-index={row.index} key={row.id} className={b('tr')} ref={rowRef}>
                         {row.cells.map((cell) => {
                             return (
-                                <td
+                                <TableBodyCell
                                     key={cell.id}
-                                    className={b(
-                                        'td',
-                                        {
-                                            type: cell.type,
-                                            pinned: cell.pinned,
-                                        },
-                                        cell.className,
-                                    )}
-                                    style={{
-                                        ...cell.style,
-                                        gridRow: cell.rowSpan ? `span ${cell.rowSpan}` : undefined,
-                                        gridColumn: cell.colSpan
-                                            ? `span ${cell.colSpan}`
-                                            : undefined,
-                                        maxHeight: cell.maxHeight,
-                                    }}
+                                    cell={cell}
                                     onClick={(event) => {
                                         if (onCellClick) {
                                             onCellClick(event, cell.data, row.id);
                                         }
                                     }}
-                                    rowSpan={cell.rowSpan}
-                                    colSpan={cell.colSpan}
-                                >
-                                    <div
-                                        className={b('cell-content', {type: cell.contentType})}
-                                        data-qa={ChartKitTableQa.CellContent}
-                                        style={cell.contentStyle}
-                                    >
-                                        {cell.content}
-                                    </div>
-                                </td>
+                                />
                             );
                         })}
                     </tr>
