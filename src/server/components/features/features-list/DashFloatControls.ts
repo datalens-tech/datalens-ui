94db10a0e53879a5925126238314187fa18d93c3
Elena Martynova <marginy@yandex-team.ru>
2024-11-24 11:41:50 +0300

    Disable DashFloatControls in os

--- src/server/components/features/features-list/DashFloatControls.ts
+++ src/server/components/features/features-list/DashFloatControls.ts
@@ -4,7 +4,7 @@ import {createFeatureConfig} from '../utils';
 export default createFeatureConfig({
     name: Feature.DashFloatControls,
     state: {
-        development: true,
+        development: false,
         production: false,
     },
 });
