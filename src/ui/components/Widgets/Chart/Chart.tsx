d4ccbea3f6fe2f95534d6da418bda6616b25b927
Elena Martynova <marginy@yandex-team.ru>
2024-11-24 14:08:28 +0300

    Fix render single chart controls

--- src/ui/components/Widgets/Chart/Chart.tsx
+++ src/ui/components/Widgets/Chart/Chart.tsx
@@ -8,11 +8,9 @@ import isEqual from 'lodash/isEqual';
 import omit from 'lodash/omit';
 import pick from 'lodash/pick';
 import type {StringParams} from 'shared';
-import {Feature} from 'shared/types/feature';
 import {getDataProviderData} from 'ui/libs/DatalensChartkit/components/ChartKitBase/helpers';
 import {CHART_RELOAD_EVENT} from 'ui/units/preview/modules/constants/constants';
 import {isEmbeddedMode} from 'ui/utils/embedded';
-import Utils from 'ui/utils/utils';
 
 import settings from '../../../libs/DatalensChartkit/modules/settings/settings';
 import DebugInfoTool from '../../DashKit/plugins/DebugInfoTool/DebugInfoTool';
@@ -83,7 +81,7 @@ export const Chart = (props: ChartNoWidgetProps) => {
         onBeforeChartLoad,
     } = props;
 
-    const showFloatControls = Utils.isEnabledFeature(Feature.DashFloatControls);
+    //const showFloatControls = Utils.isEnabledFeature(Feature.DashFloatControls);
 
     const innerParamsRef = React.useRef<DataProps['params'] | null>(null);
     const prevInnerParams = usePrevious(innerParamsRef?.current);
@@ -290,7 +288,7 @@ export const Chart = (props: ChartNoWidgetProps) => {
                 paneSplitOrientation={paneSplitOrientation}
                 widgetDashState={widgetDashState}
                 rootNodeRef={rootNodeRef}
-                needRenderContentControls={showFloatControls}
+                needRenderContentControls={true}
             />
         </div>
     );
