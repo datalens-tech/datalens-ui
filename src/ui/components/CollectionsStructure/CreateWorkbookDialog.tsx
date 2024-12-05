81886cf051c5b5578dfdb7b537367a2b5914e46a
Maksim Sitnikov <msitnikov1@yandex-team.ru>
2024-12-05 20:46:55 +0300

    Fix loader in workbook creation dialog (#1889)

--- src/ui/components/CollectionsStructure/CreateWorkbookDialog.tsx
+++ src/ui/components/CollectionsStructure/CreateWorkbookDialog.tsx
@@ -29,39 +29,50 @@ type Props = {
     onApply?: (result: CreateWorkbookResponse | null) => void | Promise<void>;
 };
 
-export const CreateWorkbookDialog: React.FC<Props> = (props) => {
+export const CreateWorkbookDialog: React.FC<Props> = ({
+    collectionId,
+    open,
+    dialogTitle,
+    defaultWorkbookTitle,
+    onClose,
+    onApply,
+}) => {
     const dispatch: AppDispatch = useDispatch();
-    const {open, dialogTitle, defaultWorkbookTitle, onClose} = props;
-
-    const handleApply = async ({title, description}: {title: string; description?: string}) => {
-        const {collectionId, onApply} = props;
-
-        const result = await dispatch(
-            createWorkbook({
-                title,
-                description: description ?? '',
-                collectionId,
-            }),
-        );
-
-        if (onApply) {
-            const promise = onApply(result);
-            if (promise) {
-                await promise;
-            }
-        }
-
-        return result;
-    };
 
     const isLoading = useSelector(selectCreateWorkbookIsLoading);
+
+    const [isExternalLoading, setIsExternalLoading] = React.useState(false);
+
+    const handleApply = React.useCallback(
+        async ({title, description}: {title: string; description?: string}) => {
+            const result = await dispatch(
+                createWorkbook({
+                    title,
+                    description: description ?? '',
+                    collectionId,
+                }),
+            );
+
+            if (onApply) {
+                const promise = onApply(result);
+                if (promise) {
+                    setIsExternalLoading(true);
+                    await promise;
+                    setIsExternalLoading(false);
+                }
+            }
+
+            return result;
+        },
+        [collectionId, dispatch, onApply],
+    );
 
     return (
         <WorkbookDialog
             title={dialogTitle ?? i18n('action_create-workbook')}
             textButtonApply={i18n('action_create')}
             open={open}
-            isLoading={isLoading}
+            isLoading={isLoading || isExternalLoading}
             titleValue={defaultWorkbookTitle}
             onApply={handleApply}
             onClose={onClose}
