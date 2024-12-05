7c81a3d672c965bb65a025fe012f4345b792c142
Evgeny Alaev <alaev89@yandex-team.ru>
2024-12-05 13:43:18 +0300

    Suppor wrappedHtml in Table cell formattedValue

--- src/ui/libs/DatalensChartkit/ChartKit/components/Widget/components/Table/WrappedHTMLNode.tsx
+++ src/ui/libs/DatalensChartkit/ChartKit/components/Widget/components/Table/WrappedHTMLNode.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import {WRAPPED_HTML_KEY} from 'shared';
 import type {WrappedHTML} from 'shared';
 
 import {generateHtml} from '../../../../../modules/html-generator';
@@ -9,6 +10,14 @@ type Props<T extends React.ElementType> = {
     as?: T;
     className?: string;
 };
+
+export function isWrappedHTML(value: unknown): value is WrappedHTML {
+    if (!value || typeof value !== 'object') {
+        return false;
+    }
+
+    return Object.keys(value).length === 1 && WRAPPED_HTML_KEY in value;
+}
 
 export function WrappedHTMLNode<T extends React.ElementType = 'div'>(props: Props<T>) {
     const {as, className, value} = props;
