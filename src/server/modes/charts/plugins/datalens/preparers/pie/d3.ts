4447f3b2230b91744a5c744bdd42b7245bab519f
Irina Kuzmina <kuzmadom@yandex-team.ru>
2024-12-05 17:30:19 +0300

    Some pie chart improvements (#1832)

--- src/server/modes/charts/plugins/datalens/preparers/pie/d3.ts
+++ src/server/modes/charts/plugins/datalens/preparers/pie/d3.ts
@@ -1,110 +1,108 @@
-import type {
-    ChartKitWidgetData,
-    PieSeries,
-    PieSeriesData,
-} from '@gravity-ui/chartkit/build/types/widget-data';
+import type {PieSeries, PieSeriesData} from '@gravity-ui/chartkit';
 
-import type {SeriesExportSettings, ServerField} from '../../../../../../../shared';
+import type {SeriesExportSettings} from '../../../../../../../shared';
 import {WizardVisualizationId, formatNumber, getFormatOptions} from '../../../../../../../shared';
-import {getFormattedLabel} from '../../d3/utils/dataLabels';
+import {getFakeTitleOrTitle} from '../../../../../../../shared/modules/fields';
+import {isMarkdownField, isMarkupField} from '../../../../../../../shared/types/index';
 import {getExportColumnSettings} from '../../utils/export-helpers';
-import type {PrepareFunctionArgs} from '../types';
+import type {PiePoint, PrepareFunctionArgs} from '../types';
 
-import type {PieConfig} from './prepare-pie-data';
 import preparePieData from './prepare-pie-data';
+import {getFormattedValue, isColoringByMeasure} from './utils';
 
-type MapPieSeriesArgs = {
-    graph: PieConfig;
-    isLabelsEnabled?: boolean;
-    labelField?: ServerField;
-    measureField?: ServerField;
-    visualizationId: WizardVisualizationId;
-    totals?: string | null;
-    ChartEditor: PrepareFunctionArgs['ChartEditor'];
-};
-
-type ExtendedPieSeriesData = PieSeriesData & {
+type ExtendedPieSeriesData = Omit<PieSeriesData, 'label'> & {
     drillDownFilterValue?: string;
+    formattedValue: string | null;
+    percentage: number;
+    label?: PiePoint['label'];
 };
 
-type ExtendedPieSeries = PieSeries & {
+type ExtendedPieSeries = Omit<PieSeries, 'data'> & {
     custom?: {
         totals?: string;
         exportSettings?: SeriesExportSettings;
     };
+    data: ExtendedPieSeriesData[];
 };
 
-function mapPieSeries(args: MapPieSeriesArgs) {
-    const {graph, isLabelsEnabled, measureField, labelField, visualizationId, totals, ChartEditor} =
-        args;
-
-    const seriesConfig: ExtendedPieSeries = {
-        type: 'pie',
-        dataLabels: {
-            enabled: isLabelsEnabled,
-        },
-        data:
-            graph.data?.map<PieSeriesData>((item) => {
-                const dataItem: ExtendedPieSeriesData = {
-                    value: item.y,
-                    color: String(item.color),
-                    name: item.name,
-                    custom: item.custom,
-                    drillDownFilterValue: item.drillDownFilterValue,
-                };
-
-                if (isLabelsEnabled) {
-                    dataItem.label = getFormattedLabel(item.label as string | number, labelField);
-                }
-
-                return dataItem;
-            }) || [],
-    };
-
-    seriesConfig.custom = {
-        exportSettings: {
-            columns: [
-                {
-                    name: ChartEditor.getTranslation('chartkit.data-provider', 'categories'),
-                    field: 'name',
-                },
-                getExportColumnSettings({path: 'value', field: measureField}),
-            ],
-        },
-    };
-
-    if (visualizationId === WizardVisualizationId.DonutD3) {
-        seriesConfig.innerRadius = '50%';
-
-        if (measureField && totals) {
-            seriesConfig.custom = {
-                ...seriesConfig.custom,
-                totals: formatNumber(Number(totals), getFormatOptions(measureField)),
-            };
-        }
-    }
-
-    return seriesConfig;
-}
-
-export function prepareD3Pie(args: PrepareFunctionArgs): ChartKitWidgetData {
-    const {labels, visualizationId, ChartEditor} = args;
+export function prepareD3Pie(args: PrepareFunctionArgs) {
+    const {labels, visualizationId, ChartEditor, colorsConfig, idToDataType} = args;
     const {graphs, label, measure, totals} = preparePieData(args);
     const isLabelsEnabled = Boolean(labels?.length && label && measure?.hideLabelMode !== 'hide');
+    const isMarkdownLabel = isMarkdownField(label);
+    const isMarkupLabel = isMarkupField(label);
+
+    let data: ExtendedPieSeries[] = [];
+
+    if (measure && graphs.length > 0) {
+        const graph = graphs[0];
+        const total = graph.data?.reduce((sum, d) => sum + (d.y || 0), 0) ?? 0;
+        const seriesConfig: ExtendedPieSeries = {
+            type: 'pie',
+            dataLabels: {
+                enabled: isLabelsEnabled,
+                html: isMarkdownLabel || isMarkupLabel,
+            },
+            data:
+                graph.data?.map((item) => {
+                    return {
+                        ...item,
+                        value: item.y,
+                        color: String(item.color),
+                        formattedValue: getFormattedValue(String(item.y), {
+                            ...measure,
+                            data_type: idToDataType[measure.guid],
+                        }),
+                        percentage: item.y / total,
+                    };
+                }) ?? [],
+        };
+
+        seriesConfig.custom = {
+            exportSettings: {
+                columns: [
+                    {
+                        name: ChartEditor.getTranslation('chartkit.data-provider', 'categories'),
+                        field: 'name',
+                    },
+                    getExportColumnSettings({path: 'value', field: measure}),
+                ],
+            },
+        };
+
+        if (visualizationId === WizardVisualizationId.DonutD3) {
+            seriesConfig.innerRadius = '50%';
+
+            if (measure && totals) {
+                seriesConfig.custom = {
+                    ...seriesConfig.custom,
+                    totals: formatNumber(Number(totals), getFormatOptions(measure)),
+                };
+            }
+        }
+
+        data.push(seriesConfig);
+    } else {
+        data = [];
+    }
+
+    let legend;
+    if (graphs.length && isColoringByMeasure(args)) {
+        legend = {
+            enabled: true,
+            type: 'continuous',
+            title: {text: getFakeTitleOrTitle(measure)},
+            colorScale: {
+                colors: colorsConfig.gradientColors,
+                stops: colorsConfig.gradientColors.length === 2 ? [0, 1] : [0, 0.5, 1],
+            },
+        };
+    }
 
     return {
         series: {
-            data: graphs.map((graph) =>
-                mapPieSeries({
-                    graph,
-                    labelField: label,
-                    isLabelsEnabled,
-                    visualizationId: visualizationId as WizardVisualizationId,
-                    totals,
-                    measureField: measure,
-                    ChartEditor,
-                }),
-            ),
+            data,
         },
+        legend,
     };
 }
