d9562e97a2ffce274eaf36245a3fdf0f2729e421
Evgeny Alaev <alaev89@yandex-team.ru>
2024-12-23 20:30:16 +0300

    Remove mocks

--- src/ui/units/connections/store/actions/index.ts
+++ src/ui/units/connections/store/actions/index.ts
@@ -128,7 +128,6 @@ function setDefaultFormData(args: {type: ConnectorType; schema: FormSchema}) {
             ...defaults.form,
             ...queryForm,
             [FieldKey.Type]: type,
-            secret_headers: {entries: {'api-1-key': ''}},
         };
 
         if (Object.keys(defaults.form).length) {
