d29da1d35f681bc4fb776b8a700228f1889ac60c
Daria Larionova <darialari@yandex-team.ru>
2025-01-14 14:04:02 +0300

    Add custom fontSize preset

--- src/ui/components/DialogTitleWidget/DialogTitleWidget.tsx
+++ src/ui/components/DialogTitleWidget/DialogTitleWidget.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 
 import {FormRow} from '@gravity-ui/components';
+import {TITLE_DEFAULT_SIZES} from '@gravity-ui/dashkit';
+import {ChevronDown} from '@gravity-ui/icons';
 import type {RadioButtonOption} from '@gravity-ui/uikit';
-import {Checkbox, Dialog, RadioButton, Text, TextInput} from '@gravity-ui/uikit';
+import {
+    Button,
+    Checkbox,
+    Dialog,
+    Icon,
+    RadioButton,
+    Select,
+    Text,
+    TextInput,
+} from '@gravity-ui/uikit';
 import {unstable_NumberInput as NumberInput} from '@gravity-ui/uikit/unstable';
 import block from 'bem-cn-lite';
 import {FieldWrapper} from 'components/FieldWrapper/FieldWrapper';
@@ -38,13 +49,26 @@ const CUSTOM_FONT_SIZE_OPTION: RadioButtonOption<RadioButtonFontSizeOption> = {
     content: 'Custom',
 };
 
+const presetFontSizeOptions = ['15', '17', '20', '24', '32', '40', '48', '56', '72', '96'].map(
+    (opt) => ({value: opt, content: opt}),
+);
+
+function isDashTabItemTitleSize(size: RadioButtonFontSizeOption): size is DashTabItemTitleSize {
+    return Object.values(DashTabItemTitleSizes).includes(size as DashTabItemTitleSize);
+}
+
+function getNumericFontSize(size: DashTabItemTitleSize) {
+    return Number(TITLE_DEFAULT_SIZES[size].fontSize.replace('px', ''));
+}
+
 const b = block('dialog-title');
 
 interface DialogTitleWidgetState {
-    validation?: {text?: string; customFontSize?: string; customLineHeight?: string};
+    validation?: {text?: string};
     text?: string;
     fontSize: RadioButtonFontSizeOption;
     customFontSize?: number | null;
+    previousSelectedFontSize: number;
     showInTOC?: boolean;
     autoHeight?: boolean;
     backgroundColor?: string;
@@ -91,17 +115,31 @@ function DialogTitleWidget(props: DialogTitleWidgetProps) {
         validation: {},
         text: openedItemData.text,
         ...(typeof openedItemData.size === 'string'
-            ? {fontSize: openedItemData.size, customFontSize: null}
+            ? {
+                  fontSize: openedItemData.size,
+                  customFontSize: null,
+                  previousSelectedFontSize: getNumericFontSize(openedItemData.size),
+              }
             : {
                   fontSize: 'custom',
                   customFontSize: openedItemData.size.fontSize ?? null,
+                  previousSelectedFontSize:
+                      openedItemData.size.fontSize ?? getNumericFontSize('xl'),
               }),
         showInTOC: openedItemData.showInTOC,
         autoHeight: Boolean(openedItemData.autoHeight),
         backgroundColor: openedItemData.background?.color || '',
     });
-    const {text, fontSize, customFontSize, showInTOC, validation, autoHeight, backgroundColor} =
-        state;
+    const {
+        text,
+        fontSize,
+        customFontSize,
+        showInTOC,
+        validation,
+        autoHeight,
+        backgroundColor,
+        previousSelectedFontSize,
+    } = state;
 
     const enableCustomFontSize =
         props.enableCustomFontSize || typeof openedItemData?.size === 'object';
@@ -120,35 +158,60 @@ function DialogTitleWidget(props: DialogTitleWidgetProps) {
         [],
     );
 
-    const onSizeChange = React.useCallback(
-        (newSize: RadioButtonFontSizeOption) =>
-            setState((prevState) => ({...prevState, fontSize: newSize, validation: {}})),
-        [],
-    );
+    const onSizeChange = React.useCallback((newSize: RadioButtonFontSizeOption) => {
+        setState((prevState) => {
+            const newNumericSize =
+                isDashTabItemTitleSize(newSize) &&
+                Object.keys(TITLE_DEFAULT_SIZES).includes(newSize)
+                    ? Number(TITLE_DEFAULT_SIZES[newSize].fontSize.replace('px', ''))
+                    : undefined;
+
+            return {
+                ...prevState,
+                fontSize: newSize,
+                ...(newSize === 'custom'
+                    ? {customFontSize: prevState.previousSelectedFontSize}
+                    : {
+                          previousSelectedFontSize:
+                              newNumericSize ?? prevState.previousSelectedFontSize,
+                      }),
+                validation: {},
+            };
+        });
+    }, []);
 
     const onCustomFontSizeChange = React.useCallback(
         (newFontSize: number | null) =>
             setState((prevState) => ({
                 ...prevState,
-                customFontSize: newFontSize ?? undefined,
+                customFontSize: newFontSize,
+                validation: {},
+            })),
+        [],
+    );
+
+    const onCustomFontSizePresetSelectChange = React.useCallback(
+        (newFontSize: string[]) =>
+            setState((prevState) => ({
+                ...prevState,
+                customFontSize: newFontSize[0] ? Number(newFontSize[0]) : prevState.customFontSize,
                 validation: {},
             })),
         [],
     );
 
     const onApply = React.useCallback(() => {
-        const validationErrors = {
+        const validationErrors: DialogTitleWidgetState['validation'] = {
             text: text?.trim() ? undefined : i18n('dash.title-dialog.edit', 'toast_required-field'),
-            customFontSize:
-                fontSize === 'custom' && !customFontSize
-                    ? i18n('dash.title-dialog.edit', 'toast_required-field')
-                    : undefined,
         };
         if (Object.values(validationErrors).filter(Boolean).length === 0) {
             setItemData({
                 data: {
                     text,
-                    size: fontSize === 'custom' ? {fontSize: customFontSize} : fontSize,
+                    size:
+                        fontSize === 'custom'
+                            ? {fontSize: customFontSize ?? previousSelectedFontSize}
+                            : fontSize,
                     showInTOC,
                     autoHeight,
                     background: {
@@ -166,9 +229,10 @@ function DialogTitleWidget(props: DialogTitleWidgetProps) {
         }
     }, [
         text,
+        setItemData,
         fontSize,
         customFontSize,
-        setItemData,
+        previousSelectedFontSize,
         showInTOC,
         autoHeight,
         backgroundColor,
@@ -217,22 +281,41 @@ function DialogTitleWidget(props: DialogTitleWidgetProps) {
                             onUpdate={onSizeChange}
                         />
                         {enableCustomFontSize && fontSize === 'custom' && (
-                            <React.Fragment>
+                            <div>
                                 <NumberInput
                                     className={b('number-input')}
                                     value={customFontSize}
                                     onUpdate={onCustomFontSizeChange}
+                                    onBlur={() => {
+                                        if (!customFontSize) {
+                                            setState((prevState) => ({
+                                                ...prevState,
+                                                customFontSize: prevState.previousSelectedFontSize,
+                                            }));
+                                        }
+                                    }}
                                     min={1}
                                     max={200}
                                     hiddenControls
-                                    validationState={
-                                        validation?.customFontSize ? 'invalid' : undefined
+                                    endContent={
+                                        <Select
+                                            className={b('unit-select')}
+                                            value={[customFontSize]
+                                                .filter(Number.isInteger)
+                                                .map(String)}
+                                            onUpdate={onCustomFontSizePresetSelectChange}
+                                            options={presetFontSizeOptions}
+                                            renderControl={(selectControlProps) => (
+                                                <Button view="flat" {...selectControlProps}>
+                                                    <Icon data={ChevronDown} />
+                                                </Button>
+                                            )}
+                                        />
                                     }
-                                    errorMessage={validation?.customFontSize}
-                                    errorPlacement="inside"
                                 />
+
                                 <Text className={b('unit')}>px</Text>
-                            </React.Fragment>
+                            </div>
                         )}
                     </div>
                 </FormRow>
