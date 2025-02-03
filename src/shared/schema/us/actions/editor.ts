bfa1810ef40d5afb844c988b58b978b31563dec1
Mr.Dr.Professor Patrick <alaev89@yandex.ru>
2025-02-03 15:48:31 +0100

    Set links from meta tab to editor entry (#2094)

--- src/shared/schema/us/actions/editor.ts
+++ src/shared/schema/us/actions/editor.ts
@@ -15,7 +15,7 @@ export const editorActions = {
         method: 'POST',
         path: () => `${PATH_PREFIX}/entries`,
         params: (
-            {type, key, data, meta = {}, name, workbookId, mode = EntryUpdateMode.Publish},
+            {type, key, data, meta = {}, name, workbookId, mode = EntryUpdateMode.Publish, links},
             headers,
         ) => {
             return {
@@ -28,6 +28,7 @@ export const editorActions = {
                     name,
                     workbookId,
                     mode,
+                    links,
                 },
                 headers,
             };
@@ -36,7 +37,7 @@ export const editorActions = {
     _updateEditorChart: createAction<UpdateEditorChartResponse, UpdateEditorChartArgs>({
         method: 'POST',
         path: ({entryId}) => `${PATH_PREFIX}/entries/${filterUrlFragment(entryId)}`,
-        params: ({data, mode, revId, meta = {}, links = {}}, headers) => {
+        params: ({data, mode, revId, meta = {}, links}, headers) => {
             return {
                 body: {
                     mode,
