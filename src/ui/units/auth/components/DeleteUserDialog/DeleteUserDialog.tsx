1a33cd2461974028c7bb8d37d66207ffe421f489
Daria Larionova <darialari@yandex-team.ru>
2025-02-18 15:14:43 +0300

    Add translations

--- src/ui/units/auth/components/DeleteUserDialog/DeleteUserDialog.tsx
+++ src/ui/units/auth/components/DeleteUserDialog/DeleteUserDialog.tsx
@@ -1,32 +1,14 @@
 import React from 'react';
 
 import {ConfirmDialog} from '@gravity-ui/components';
+import {I18n} from 'i18n';
 import {useDispatch, useSelector} from 'react-redux';
 import type {AppDispatch} from 'ui/store';
 
 import {deleteUserProfile} from '../../store/actions/userProfile';
 import {selectDeleteUserProfileIsLoading} from '../../store/selectors/userProfile';
-// import {I18n, i18n} from 'i18n';
 
-// TODO: add translations
-// const i18n = I18n.keyset('auth.dialog-delete-user');
-
-const i18n = (key: string) => {
-    switch (key) {
-        case 'title_delete-user':
-            return 'Delete user';
-        case 'label_delete-user-confirmation':
-            return 'The user will be deleted and will no longer be able to use DataLens. Are you sure?';
-        case 'label_failed-to-delete-user':
-            return 'Failed to delete user';
-        case 'action_user-profile-deletion-cancel':
-            return 'Cancel';
-        case 'action_user-profile-deletion-confirm':
-            return 'Delete user';
-        default:
-            return key;
-    }
-};
+const i18n = I18n.keyset('auth.dialog-delete-user');
 
 interface DeleteUserDialogProps {
     userId: string;
