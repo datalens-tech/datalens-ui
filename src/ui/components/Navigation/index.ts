a33ea3dd485584c27e2306a0a4f61477715db61f
Taya Leutina <leutinatasya@yandex-team.ru>
2025-02-26 16:11:15 +0300

    Return export

--- src/ui/components/Navigation/index.ts
+++ src/ui/components/Navigation/index.ts
@@ -1,3 +1,4 @@
 export {default as NavigationMinimal} from './NavigationMinimal';
+export {default as NavigationModal} from './NavigationModal';
 export {QUICK_ITEMS, PLACE, USER_FOLDER_PLACE, ROOT_PATH} from './constants';
 export {getPathDisplayName} from './util';
