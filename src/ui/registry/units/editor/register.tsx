12262dcc8865bb36b1627a1023372897b3b77b11
Elena Martynova <marginy@yandex-team.ru>
2025-02-27 16:38:13 +0300

    Add texts & plugin for choose template

--- src/ui/registry/units/editor/register.tsx
+++ src/ui/registry/units/editor/register.tsx
@@ -1,10 +1,17 @@
 import {registry} from '../../../registry';
 import {exampleFunction} from '../../../registry/functions/example-function';
 import {EXAMPLE_FUNCTION} from '../../../registry/units/common/constants/functions';
+import {NodeTemplates} from '../../../units/editor/components/NodeTemplates/NodeTemplates';
 import {getEmptyTemplateType} from '../../../units/editor/constants/common';
 import {getEditorTemplates} from '../../../units/editor/constants/templates';
 
+import {EDITOR_CHOOSE_TPL} from './constants/components';
+
 export const registerEditorPlugins = () => {
+    registry.editor.components.registerMany({
+        [EDITOR_CHOOSE_TPL]: NodeTemplates,
+    });
+
     registry.editor.functions.register({
         [EXAMPLE_FUNCTION]: exampleFunction,
         getEditorTemplates,
