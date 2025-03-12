99b8571c52e660e11ed87d2ad13f28cbd14f73c6
Serge Pavlyuk <sergeypavlyuk@yandex-team.ru>
2025-03-12 18:41:25 +0300

    Add dash margin setting

--- src/ui/units/dash/utils/dashkitProps.ts
+++ src/ui/units/dash/utils/dashkitProps.ts
@@ -1,6 +1,39 @@
-import type {DashKitGroup} from '@gravity-ui/dashkit';
+import type {DashKitGroup, ReactGridLayoutProps} from '@gravity-ui/dashkit';
+import {
+    DEFAULT_DASH_MARGINS,
+    FIXED_HEADER_GROUP_COLS,
+    FIXED_HEADER_GROUP_LINE_MAX_ROWS,
+    MIN_AUTO_HEIGHT_PX,
+    MIN_AUTO_HEIGHT_ROWS,
+} from 'ui/components/DashKit/constants';
 
-export const getPropertiesWithResizeHandles: DashKitGroup['gridProperties'] = (props) => ({
-    ...props,
-    resizeHandles: ['sw', 'se'],
-});
+export const calculateRowHeight = (props: ReactGridLayoutProps) => {
+    if (props.margin && props.margin[1] !== DEFAULT_DASH_MARGINS[1]) {
+        props.rowHeight =
+            (MIN_AUTO_HEIGHT_PX - props.margin[1] * (MIN_AUTO_HEIGHT_ROWS - 1)) /
+            MIN_AUTO_HEIGHT_ROWS;
+    }
+
+    return props;
+};
+
+export const fixedHeaderGridProperties =
+    (extendedProps: ReactGridLayoutProps = {}) =>
+    (props: ReactGridLayoutProps) =>
+        calculateRowHeight({
+            ...props,
+            ...extendedProps,
+            cols: FIXED_HEADER_GROUP_COLS,
+            maxRows: FIXED_HEADER_GROUP_LINE_MAX_ROWS,
+            autoSize: false,
+            compactType: 'horizontal-nowrap',
+        });
+
+export const getPropertiesWithResizeHandles =
+    (extendedProps: Partial<DashKitGroup['gridProperties']> | undefined = {}) =>
+    (props: ReactGridLayoutProps): ReactGridLayoutProps =>
+        calculateRowHeight({
+            ...props,
+            ...extendedProps,
+            resizeHandles: ['sw', 'se'],
+        });
