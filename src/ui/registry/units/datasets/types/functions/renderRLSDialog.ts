86feab51a9e1a5ac24458308f1b37006d01a7c81
Mr.Dr.Professor Patrick <alaev89@yandex.ru>
2025-03-13 22:26:26 +0100

    Add RLSDialogUser type (#2283)

--- src/ui/registry/units/datasets/types/functions/renderRLSDialog.ts
+++ src/ui/registry/units/datasets/types/functions/renderRLSDialog.ts
@@ -1,7 +1,7 @@
 import type React from 'react';
 
-import type {RLSDialogProps} from 'ui/units/datasets/components/RLSDialog/RLSDialog';
+import type {RLSDialogProps, RLSDialogUser} from 'ui/units/datasets/components/RLSDialog/RLSDialog';
 
-export type RenderRLSDialog<T1 = string, T2 = unknown> = (
+export type RenderRLSDialog<T1 = string, T2 extends RLSDialogUser = RLSDialogUser> = (
     props: RLSDialogProps<T1, T2>,
 ) => React.ReactNode;
