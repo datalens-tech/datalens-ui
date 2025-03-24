95d7d3df655a6a87fb6a48248a3ddc8680127fd0
Irina Kuzmina <kuzmadom@yandex-team.ru>
2025-03-24 13:08:04 +0300

    Client palette test

---
+++ tests/opensource-suites/wizard/visualizations/metric/color.test.ts
@@ -0,0 +1,70 @@
+import {Page, expect} from '@playwright/test';
+
+import {WizardPageQa} from '../../../../../src/shared';
+import {WizardVisualizationId} from '../../../../../src/shared/constants/visualization';
+import {PlaceholderName} from '../../../../page-objects/wizard/SectionVisualization';
+import WizardPage from '../../../../page-objects/wizard/WizardPage';
+import {getUniqueTimestamp, slct} from '../../../../utils';
+import {openTestPage} from '../../../../../../../tests/utils';
+import {datalensTest} from '../../../../../../../tests/utils/playwright/globalTestDefinition';
+import {addCustomPalette} from '../../../utils';
+
+datalensTest.describe('Wizard', () => {
+    datalensTest.describe('Metric chart', () => {
+        const customPaletteNames: Record<string, string> = {};
+
+        datalensTest.beforeEach(async ({page, config}, testInfo) => {
+            const customPaletteName = getUniqueTimestamp();
+            await addCustomPalette(page, {name: customPaletteName});
+            customPaletteNames[testInfo.testId] = customPaletteName;
+
+            const wizardPage = new WizardPage({page});
+            await openTestPage(page, config.wizard.urls.WizardBasicDataset);
+
+            const ordersCountMeasureField = 'OrdersCount';
+            await wizardPage.createNewFieldWithFormula(
+                ordersCountMeasureField,
+                'countd(date([Order_date]))',
+            );
+
+            await wizardPage.setVisualization([WizardVisualizationId.Metric]);
+        });
+
+        datalensTest('Client palette', async ({page}, testInfo) => {
+            const wizardPage = new WizardPage({page});
+            const chartContainer = page.locator(slct(WizardPageQa.SectionPreview));
+            const chart = chartContainer.locator(wizardPage.chartkit.metricItemSelector);
+
+            await wizardPage.sectionVisualization.addFieldByClick(
+                PlaceholderName.Measures,
+                'OrdersCount',
+            );
+            await expect(chart).toBeVisible();
+
+            const newPalette = customPaletteNames[testInfo.testId];
+
+            await wizardPage.metricSettingsDialog.open();
+            const defaultPalette = await wizardPage.metricSettingsDialog.getSelectedPalette();
+            expect(defaultPalette).not.toBe(newPalette);
+
+            await wizardPage.metricSettingsDialog.selectPalette(newPalette);
+            await wizardPage.metricSettingsDialog.apply();
+            await wizardPage.saveWizardEntry(wizardPage.getUniqueEntryName('test'));
+            await wizardPage.page.reload();
+
+            await expect(chart).toBeVisible();
+
+            await wizardPage.metricSettingsDialog.open();
+
+            const selectedPalette = await wizardPage.metricSettingsDialog.getSelectedPalette();
+
+            expect(selectedPalette).toBe(newPalette);
+        });
+
+        datalensTest.afterEach(async ({page}: {page: Page}) => {
+            const wizardPage = new WizardPage({page});
+            await wizardPage.page.reload();
+            await wizardPage.deleteEntry();
+        });
+    });
+});
