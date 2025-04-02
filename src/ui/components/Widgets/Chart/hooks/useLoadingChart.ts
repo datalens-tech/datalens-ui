d8969c29fb90b38bd4daf8826bcf55b44a1cae75
Irina Kuzmina <kuzmadom@yandex-team.ru>
2025-04-02 18:30:47 +0300

    Fix drilldown filtering with quotes in the filter value (#2331)

--- src/ui/components/Widgets/Chart/hooks/useLoadingChart.ts
+++ src/ui/components/Widgets/Chart/hooks/useLoadingChart.ts
@@ -11,6 +11,7 @@ import isEmpty from 'lodash/isEmpty';
 import isEqual from 'lodash/isEqual';
 import omit from 'lodash/omit';
 import pick from 'lodash/pick';
+import unescape from 'lodash/unescape';
 import type {DashChartRequestContext, StringParams} from 'shared';
 import {DashTabItemControlSourceType, SHARED_URL_OPTIONS} from 'shared';
 import {isEmbeddedMode} from 'ui/utils/embedded';
@@ -825,7 +826,16 @@ export const useLoadingChart = (props: LoadingChartHookProps) => {
                             _page: String(START_PAGE),
                         };
                     }
-                    newParams = {...changedData.data.params, ...additionalParams};
+
+                    newParams = Object.entries(changedData.data.params).reduce(
+                        (acc, [key, value]) => {
+                            acc[key] = Array.isArray(value) ? value.map(unescape) : unescape(value);
+                            return acc;
+                        },
+                        {} as StringParams,
+                    );
+
+                    newParams = {...newParams, ...additionalParams};
                     if (!enableActionParams) {
                         newParams = pickExceptActionParamsFromParams(newParams);
                     }
