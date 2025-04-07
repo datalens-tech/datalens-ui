4138cca428a4a885443716a84093334f0684ecf0
Irina Kuzmina <kuzmadom@yandex-team.ru>
2025-04-07 13:22:21 +0300

    Remove Editor from platform (#2353)

--- src/ui/units/editor/store/actions/editor/index.ts
+++
@@ -1,9 +0,0 @@
-import type {ChartActions} from './chart';
-import type {CodeActions} from './code';
-import type {PanesActions} from './panes';
-
-export * from './code';
-export * from './panes';
-export * from './chart';
-
-export type EditorActions = CodeActions | PanesActions | ChartActions;
--- src/ui/units/editor/types/revisions.ts
+++
@@ -1,9 +0,0 @@
-import type {GetRevisionsEntry} from '../../../../shared/schema';
-
-export type RevisionEntry = GetRevisionsEntry;
-
-export enum RevisionAction {
-    Open = 'open',
-    Publish = 'publish',
-    Reset = 'reset',
-}
