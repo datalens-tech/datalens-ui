696c1f09db55c01057656aee59e453562443d5ef
Serge Pavlyuk <sergeypavlyuk@yandex-team.ru>
2025-04-22 15:05:04 +0300

    Add meta-manager capabilities

--- src/shared/constants/workbook-transfer.ts
+++ src/shared/constants/workbook-transfer.ts
@@ -1,3 +1,5 @@
+import {EntryScope} from '..';
+
 export const TRANSFER_UNKNOWN_ENTRY_ID = 'UNKNOWN_ENTRY';
 
 export enum TransferErrorCode {
@@ -7,3 +9,18 @@ export enum TransferErrorCode {
     TransferInvalidEntryScope = 'ERR.UI_API.TRANSFER_INVALID_ENTRY_SCOPE',
     TransferInvalidToken = 'ERR.UI_API.TRANSFER_INVALID_TOKEN',
 }
+
+export const TransferCapabilities = {
+    exportOrder: {
+        [EntryScope.Connection]: 4,
+        [EntryScope.Dataset]: 3,
+        [EntryScope.Widget]: 2,
+        [EntryScope.Dash]: 1,
+    },
+    importOrder: {
+        [EntryScope.Connection]: 1,
+        [EntryScope.Dataset]: 2,
+        [EntryScope.Widget]: 3,
+        [EntryScope.Dash]: 4,
+    },
+};
