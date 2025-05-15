ba5b8f08cf1366cb0984f667320d4d7ddc436d3e
Andrey Melikhov <melikhov@yandex-team.ru>
2025-05-15 12:32:24 +0300

    Add guard to expresskit routes (#2486)

---
+++ src/server/expresskit.ts
@@ -0,0 +1,24 @@
+import type {AppRoutes} from '@gravity-ui/expresskit';
+import {ExpressKit} from '@gravity-ui/expresskit';
+import type {NodeKit} from '@gravity-ui/nodekit';
+
+import type {ExtendedAppRouteDescription} from './types/controllers';
+
+export function getExpressKit({
+    extendedRoutes,
+    nodekit,
+}: {
+    extendedRoutes: Record<string, ExtendedAppRouteDescription>;
+    nodekit: NodeKit;
+}) {
+    const routes: AppRoutes = {};
+    Object.keys(extendedRoutes).forEach((key) => {
+        const {route, guard, ...params} = extendedRoutes[key];
+        if (guard) {
+            params.afterAuth = [...params.afterAuth, guard];
+        }
+        routes[route] = params;
+    });
+
+    return new ExpressKit(nodekit, routes);
+}
