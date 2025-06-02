a92b529821379993bcc48dadf1f2a8d9ab6d8ad8
Irina Kuzmina <kuzmadom@yandex-team.ru>
2025-06-02 12:22:20 +0300

    Remove unused flags (#2571)

--- src/shared/types/feature.ts
+++ src/shared/types/feature.ts
@@ -67,8 +67,6 @@ export enum Feature {
     NewTableWidgetForCE = 'NewTableWidgetForCE',
     /** Enable undo/redo buttons & hotkeys in datasets */
     EnableEditHistoryDatasets = 'EnableEditHistoryDataset',
-    /** An empty chart for drawing something unusual */
-    BlankChart = 'BlankChart',
     /** Additional chart config for making requests by widget events */
     ChartActions = 'ChartActions',
     /** Disable the use of html and function in chart configs */
@@ -94,8 +92,6 @@ export enum Feature {
     EnableDashUndoRedo = 'EnableDashUndoRedo',
     /** Enable public gallery unit */
     EnablePublicGallery = 'EnablePublicGallery',
-    /** Setting in the table to preserve spaces and line breaks */
-    PreWrapTableSetting = 'PreWrapTableSetting',
     /** Enable new secure parameters behavior */
     EnableSecureParamsV2 = 'EnableSecureParamsV2',
 }
