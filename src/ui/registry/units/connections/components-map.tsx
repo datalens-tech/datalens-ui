5617d80fc8389a2869933a8bde4c6f37b9c28f1a
Daria Larionova <darialari@yandex-team.ru>
2025-06-06 15:37:15 +0300

    Fix ts

--- src/ui/registry/units/connections/components-map.tsx
+++ src/ui/registry/units/connections/components-map.tsx
@@ -1,11 +1,9 @@
 import {makeDefaultEmpty} from '../../components/DefaultEmpty';
 
-import type {AdditionalConnectionFormActionsProps} from './types/AdditionalConnectionFormActions';
 import type {CreateEditorChartButtonProps} from './types/CreateEditorChartButtonProps';
 import type {PreparedRowItemProps} from './types/PreparedRowItem';
 
 export const connectionsComponentsMap = {
     CreateEditorChartButton: makeDefaultEmpty<CreateEditorChartButtonProps>(),
     PreparedRowItem: makeDefaultEmpty<PreparedRowItemProps>(),
-    AdditionalConnectionFormActions: makeDefaultEmpty<AdditionalConnectionFormActionsProps>(),
 } as const;
