7d1775a2b87823d6dc2c218fb43c21f4bf1f68b9
Irina Kuzmina <kuzmadom@yandex-team.ru>
2025-06-20 15:25:21 +0300

    fix gravity-charts selectots

--- tests/opensource-suites/ql/visualizations/treemap/basic.test.ts
+++ tests/opensource-suites/ql/visualizations/treemap/basic.test.ts
@@ -27,7 +27,7 @@ datalensTest.describe('QL', () => {
 
         datalensTest('Visualization of the graph without errors', async ({page}) => {
             const previewLoader = page.locator('.grid-loader');
-            const chart = page.locator('.gcharts-d3');
+            const chart = page.locator('.gcharts');
 
             await expect(previewLoader).not.toBeVisible();
             await expect(chart).toBeVisible();
--- tests/opensource-suites/wizard/visualizations/pie/basic.test.ts
+++ tests/opensource-suites/wizard/visualizations/pie/basic.test.ts
@@ -22,7 +22,7 @@ datalensTest.describe('Wizard', () => {
         datalensTest('Auto coloring @screenshot', async ({page}) => {
             const wizardPage = new WizardPage({page});
             const chartContainer = page.locator(slct(WizardPageQa.SectionPreview));
-            const chart = chartContainer.locator('.chartkit-graph,.gcharts-d3');
+            const chart = chartContainer.locator('.chartkit-graph,.gcharts');
             const previewLoader = chartContainer.locator(slct(ChartKitQa.Loader));
 
             // 1. Only Measures placeholder is filled
