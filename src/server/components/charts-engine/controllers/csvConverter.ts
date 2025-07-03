a437d45fbc96832e34bc09e990b94c8f65fa1895
Andrey Melikhov <melikhov@yandex-team.ru>
2025-07-03 16:20:51 +0300

    Fix csv number conversion (#2634)

--- src/server/components/charts-engine/controllers/csvConverter.ts
+++ src/server/components/charts-engine/controllers/csvConverter.ts
@@ -61,6 +61,8 @@ export function csvConverter(
         formSettings.delValues = ' ';
     }
 
+    const delValues = formSettings.delValues || ';';
+
     let lines = [];
     const header = [];
 
@@ -101,9 +103,10 @@ export function csvConverter(
 
                 if (typeof currentValue === 'number') {
                     value = value.replace('.', formSettings.delNumbers || ',');
-                }
-
-                if (typeof currentValue === 'string') {
+                    if (value.includes(delValues)) {
+                        value = `"${value}"`;
+                    }
+                } else if (typeof currentValue === 'string') {
                     value = `"${value.replace(/"/g, '""')}"`;
                 }
             }
@@ -115,7 +118,7 @@ export function csvConverter(
     });
 
     lines = lines.map((line) => {
-        return line.join(formSettings.delValues || ';');
+        return line.join(delValues);
     });
 
     let csvContent: Buffer | string = lines.join('\n');
