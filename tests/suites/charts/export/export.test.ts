0cc978758ca9822d3ed15a79c1ca64ef1fe0569e
Daria Larionova <darialari@yandex-team.ru>
2025-07-07 18:19:02 +0300

    Add export test on chart preview page

--- tests/suites/charts/export/export.test.ts
+++ tests/suites/charts/export/export.test.ts
@@ -3,50 +3,63 @@ import {ChartkitMenuDialogsQA} from '../../../../src/shared/constants';
 
 import WizardPage from '../../../page-objects/wizard/WizardPage';
 import {openTestPage, slct} from '../../../utils';
-import {RobotChartsWizardUrls} from '../../../utils/constants';
+import {RobotChartsPreviewUrls, RobotChartsWizardUrls} from '../../../utils/constants';
 import datalensTest from '../../../utils/playwright/globalTestDefinition';
+import {ChartPage} from '../../../page-objects/ChartPage';
+import ChartKit from '../../../page-objects/wizard/ChartKit';
+
+async function checkCsvExport(page: Page, chartkit: ChartKit) {
+    const exportPromise = page.waitForRequest('/api/export');
+
+    // open the csv download modal
+    await chartkit.openCsvExportDialog();
+
+    // check that the value separator selector has been rendered
+    let select = await page.$(`${slct(ChartkitMenuDialogsQA.chartMenuExportCsvSelectDelimiter)}`);
+    let selectText = (await select?.innerText()) || '';
+    let selectTextLength = selectText.length;
+    // has some text
+    expect(selectTextLength).toBeGreaterThan(0);
+
+    // check that the separator selector of the fractional part has been rendered
+    select = await page.$(`${slct(ChartkitMenuDialogsQA.chartMenuExportCsvSelectFloat)}`);
+    selectText = (await select?.innerText()) || '';
+    selectTextLength = selectText.length;
+    // has some text
+    expect(selectTextLength).toBeGreaterThan(0);
+
+    // check that the encoding selector has been rendered
+    select = await page.$(`${slct(ChartkitMenuDialogsQA.chartMenuExportCsvSelectCharset)}`);
+    selectText = (await select?.innerText()) || '';
+    selectTextLength = selectText.length;
+    // has some text
+    expect(selectTextLength).toBeGreaterThan(0);
+
+    // click on the Download button
+    await page.click(slct(ChartkitMenuDialogsQA.chartMenuExportModalApplyBtn));
+
+    // waiting for a download request
+    await exportPromise;
+}
 
 datalensTest.describe('Chart export', () => {
-    datalensTest(
+    datalensTest.describe(
         'Clicking on the Export menu opens the modal, the Download request goes away',
-        async ({page}: {page: Page}) => {
-            const wizardPage = new WizardPage({page});
-            await openTestPage(page, RobotChartsWizardUrls.WizardWithComments);
-            await wizardPage.waitForSuccessfulResponse('/api/run');
+        () => {
+            datalensTest('wizard page', async ({page}: {page: Page}) => {
+                const wizardPage = new WizardPage({page});
+                await openTestPage(page, RobotChartsWizardUrls.WizardWithComments);
+                await wizardPage.waitForSuccessfulResponse('/api/run');
 
-            const exportPromise = page.waitForRequest('/api/export');
+                await checkCsvExport(page, wizardPage.chartkit);
+            });
+            datalensTest('preview page', async ({page}: {page: Page}) => {
+                const chartPage = new ChartPage({page});
 
-            // open the csv download modal
-            await wizardPage.chartkit.openCsvExportDialog();
+                await openTestPage(page, RobotChartsPreviewUrls.PreviewChartWithDate);
 
-            // check that the value separator selector has been rendered
-            let select = await page.$(
-                `${slct(ChartkitMenuDialogsQA.chartMenuExportCsvSelectDelimiter)}`,
-            );
-            let selectText = (await select?.innerText()) || '';
-            let selectTextLength = selectText.length;
-            // has some text
-            expect(selectTextLength).toBeGreaterThan(0);
-
-            // check that the separator selector of the fractional part has been rendered
-            select = await page.$(`${slct(ChartkitMenuDialogsQA.chartMenuExportCsvSelectFloat)}`);
-            selectText = (await select?.innerText()) || '';
-            selectTextLength = selectText.length;
-            // has some text
-            expect(selectTextLength).toBeGreaterThan(0);
-
-            // check that the encoding selector has been rendered
-            select = await page.$(`${slct(ChartkitMenuDialogsQA.chartMenuExportCsvSelectCharset)}`);
-            selectText = (await select?.innerText()) || '';
-            selectTextLength = selectText.length;
-            // has some text
-            expect(selectTextLength).toBeGreaterThan(0);
-
-            // click on the Download button
-            await page.click(slct(ChartkitMenuDialogsQA.chartMenuExportModalApplyBtn));
-
-            // waiting for a download request
-            await exportPromise;
+                await checkCsvExport(page, chartPage.chartkit);
+            });
         },
     );
 });
