63f0182869fad831c1232d59e5c4f2dadcacd7f6
Taya Leutina <leutinatasya@yandex-team.ru>
2025-07-15 13:21:57 +0300

    Refactoring of chartkit refs types (#2576)

--- src/ui/libs/DatalensChartkit/ChartKit/ChartKitAdapter.tsx
+++ src/ui/libs/DatalensChartkit/ChartKit/ChartKitAdapter.tsx
@@ -18,118 +18,120 @@ import type {ChartKitAdapterProps} from './types';
 
 type ChartkitWidgetProps = Omit<ChartKitAdapterProps, 'rootNodeRef'>;
 
-const ChartkitWidget = React.forwardRef<ChartKit, ChartkitWidgetProps>((props, ref) => {
-    const {
-        loadedData,
-        lang,
-        isMobile,
-        splitTooltip,
-        nonBodyScroll,
-        onLoad,
-        onError,
-        onChange,
-        onRender,
-        onChartLoad,
-        renderPluginLoader,
-        paneSplitOrientation,
-        widgetDashState,
-        backgroundColor,
-    } = props;
-
-    const chartkitType = React.useMemo(() => {
-        const getChartkitType = registry.chart.functions.get('getChartkitType');
-        return getChartkitType(loadedData);
-    }, [loadedData]);
-
-    const opensourceChartKitProps = React.useMemo(() => {
-        const getFormatNumber = registry.common.functions.get('getFormatNumber');
-        const {getChartkitHolidays, getChartkitPlugins} = registry.chart.functions.getAll();
-
-        if (!chartkitType) {
-            return undefined;
-        }
-
-        const holidays = getChartkitHolidays();
-
-        settings.set({
-            plugins: getChartkitPlugins(),
-            extra: {holidays},
-        });
-
-        const additionalProps = getAdditionalProps(chartkitType);
-
-        return {
-            type: chartkitType,
-            data: getOpensourceChartKitData({type: chartkitType, loadedData, onChange}),
+const ChartkitWidget = React.forwardRef<ChartKit | ChartKitRef | undefined, ChartkitWidgetProps>(
+    (props, ref) => {
+        const {
+            loadedData,
             lang,
-            splitTooltip,
             isMobile,
-            formatNumber: getFormatNumber,
-            onChange,
-            onLoad: onLoad as ChartKitProps<typeof chartkitType>['onLoad'],
-            onChartLoad,
-            onRender,
+            splitTooltip,
+            nonBodyScroll,
+            onLoad,
             onError,
+            onChange,
+            onRender,
+            onChartLoad,
             renderPluginLoader,
             paneSplitOrientation,
             widgetDashState,
             backgroundColor,
-            ...additionalProps,
-        } as ChartKitProps<typeof chartkitType>;
-    }, [
-        onChange,
-        onLoad,
-        onChartLoad,
-        onRender,
-        onError,
-        renderPluginLoader,
-        lang,
-        loadedData,
-        splitTooltip,
-        isMobile,
-        chartkitType,
-        paneSplitOrientation,
-        widgetDashState,
-    ]);
+        } = props;
 
-    React.useEffect(() => {
-        if (lang) {
-            settings.set({lang: lang as ChartKitLang});
-            modulesI18n.setLang(lang);
+        const chartkitType = React.useMemo(() => {
+            const getChartkitType = registry.chart.functions.get('getChartkitType');
+            return getChartkitType(loadedData);
+        }, [loadedData]);
+
+        const opensourceChartKitProps = React.useMemo(() => {
+            const getFormatNumber = registry.common.functions.get('getFormatNumber');
+            const {getChartkitHolidays, getChartkitPlugins} = registry.chart.functions.getAll();
+
+            if (!chartkitType) {
+                return undefined;
+            }
+
+            const holidays = getChartkitHolidays();
+
+            settings.set({
+                plugins: getChartkitPlugins(),
+                extra: {holidays},
+            });
+
+            const additionalProps = getAdditionalProps(chartkitType);
+
+            return {
+                type: chartkitType,
+                data: getOpensourceChartKitData({type: chartkitType, loadedData, onChange}),
+                lang,
+                splitTooltip,
+                isMobile,
+                formatNumber: getFormatNumber,
+                onChange,
+                onLoad: onLoad as ChartKitProps<typeof chartkitType>['onLoad'],
+                onChartLoad,
+                onRender,
+                onError,
+                renderPluginLoader,
+                paneSplitOrientation,
+                widgetDashState,
+                backgroundColor,
+                ...additionalProps,
+            } as ChartKitProps<typeof chartkitType>;
+        }, [
+            onChange,
+            onLoad,
+            onChartLoad,
+            onRender,
+            onError,
+            renderPluginLoader,
+            lang,
+            loadedData,
+            splitTooltip,
+            isMobile,
+            chartkitType,
+            paneSplitOrientation,
+            widgetDashState,
+        ]);
+
+        React.useEffect(() => {
+            if (lang) {
+                settings.set({lang: lang as ChartKitLang});
+                modulesI18n.setLang(lang);
+            }
+        }, [lang]);
+
+        if (opensourceChartKitProps) {
+            return <OpensourceChartKit {...opensourceChartKitProps} ref={ref} />;
         }
-    }, [lang]);
 
-    if (opensourceChartKitProps) {
         return (
-            <OpensourceChartKit
-                {...opensourceChartKitProps}
-                ref={ref as React.ForwardedRef<ChartKitRef | undefined>}
+            <ChartKit
+                // ref is mainly used to access the reflow method
+                // undefined can only be passed in the os version of chartkit
+                ref={ref as React.ForwardedRef<ChartKit>}
+                loadedData={loadedData}
+                lang={lang}
+                isMobile={isMobile}
+                splitTooltip={splitTooltip}
+                nonBodyScroll={nonBodyScroll}
+                onLoad={onLoad}
+                onChartLoad={onChartLoad}
+                onRender={onRender}
+                onError={onError}
+                onChange={onChange}
+                paneSplitOrientation={paneSplitOrientation}
+                widgetDashState={widgetDashState}
+                backgroundColor={backgroundColor}
             />
         );
-    }
-
-    return (
-        <ChartKit
-            ref={ref}
-            loadedData={loadedData}
-            lang={lang}
-            isMobile={isMobile}
-            splitTooltip={splitTooltip}
-            nonBodyScroll={nonBodyScroll}
-            onLoad={onLoad}
-            onChartLoad={onChartLoad}
-            onRender={onRender}
-            onError={onError}
-            onChange={onChange}
-            paneSplitOrientation={paneSplitOrientation}
-            widgetDashState={widgetDashState}
-            backgroundColor={backgroundColor}
-        />
-    );
-});
+    },
+);
 ChartkitWidget.displayName = 'ChartkitWidget';
 
-export const ChartKitAdapter = React.forwardRef<ChartKit, ChartKitAdapterProps>((props, ref) => {
+export const ChartKitAdapter = React.forwardRef<
+    ChartKit | ChartKitRef | undefined,
+    ChartKitAdapterProps
+>((props, ref) => {
     const {rootNodeRef, ...restProps} = props;
     const tooltipRef = React.useRef<ChartKitTooltipRef>(null);
 
