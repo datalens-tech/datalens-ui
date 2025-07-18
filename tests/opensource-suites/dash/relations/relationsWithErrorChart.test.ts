e10b39e2bfbab8a3d8e735e574711a5d9bae226f
Irina Kuzmina <kuzmadom@yandex-team.ru>
2025-07-18 16:26:40 +0300

    fix test

--- tests/opensource-suites/dash/relations/relationsWithErrorChart.test.ts
+++ tests/opensource-suites/dash/relations/relationsWithErrorChart.test.ts
@@ -1,33 +1,34 @@
 import {Page} from '@playwright/test';
 
 import DashboardPage from '../../../page-objects/dashboard/DashboardPage';
-import {openTestPage} from '../../../utils';
 import datalensTest from '../../../utils/playwright/globalTestDefinition';
 
-import {Workbook} from '../../../page-objects/workbook/Workbook';
 import {TestParametrizationConfig} from '../../../types/config';
+import {openTestPage} from '../../../utils';
 
 const PARAMS = {
     UNKNOWN: 'Unspecified',
 };
 
 datalensTest.describe('Dashboards - Relations (new)', () => {
+    datalensTest.beforeEach(async ({page}: {page: Page}) => {
+        // some page need to be loaded so we can get data of feature flag from DL var
+        await openTestPage(page, '/');
+    });
+
     datalensTest.afterEach(async ({page}: {page: Page}) => {
         const dashboardPage = new DashboardPage({page});
         await dashboardPage.cancelRelationsChanges();
         await dashboardPage.exitEditMode();
         await dashboardPage.deleteDash();
     });
+
     datalensTest(
         'Pop-up opening for chart with error (removed dataset) and there is possibility to set ignore link',
         async ({page, config}: {page: Page; config: TestParametrizationConfig}) => {
-            const workbookPO = new Workbook(page);
-            await workbookPO.openE2EWorkbookPage();
-
             // copy the original dashboard with delayed widget loading,
             // so that the tests do not collapse due to the transition to editing and locks
             const dashboardPage = new DashboardPage({page});
-            await openTestPage(page, config.dash.urls.DashboardWithErrorChart);
             await dashboardPage.duplicateDashboard({
                 dashId: config.dash.urls.DashboardWithErrorChart,
             });
@@ -40,13 +41,12 @@ datalensTest.describe('Dashboards - Relations (new)', () => {
     datalensTest(
         'Pop-up opening for chart with error (removed dataset but old fields) and there is possibility to set ignore link',
         async ({page, config}: {page: Page; config: TestParametrizationConfig}) => {
-            const workbookPO = new Workbook(page);
-            await workbookPO.openE2EWorkbookPage();
+            // some page need to be loaded so we can get data of feature flag from DL var
+            await openTestPage(page, '/');
 
             // copy the original dashboard with delayed widget loading,
             // so that the tests do not collapse due to the transition to editing and locks
             const dashboardPage = new DashboardPage({page});
-            await openTestPage(page, config.dash.urls.DashboardWithAPIErrorChart);
             await dashboardPage.duplicateDashboard({
                 dashId: config.dash.urls.DashboardWithAPIErrorChart,
             });
