0aaa0e64578c8de5c122a065a6c06d533f047784
Sevastian Dvortsov <fane28@mail.ru>
2025-07-24 17:00:43 +0300

    Disable and hide chart table pagination (#2686)

--- src/server/modes/charts/plugins/datalens/js/js.ts
+++ src/server/modes/charts/plugins/datalens/js/js.ts
@@ -988,6 +988,20 @@ export const buildGraphPrivate = (args: {
         });
     }
 
+    const isTableChart = [
+        WizardVisualizationId.FlatTable,
+        WizardVisualizationId.PivotTable,
+    ].includes(shared.visualization.id as WizardVisualizationId);
+
+    if (isTableChart) {
+        const page = ChartEditor.getCurrentPage();
+        const limit = shared.extraSettings?.limit;
+        const shouldDisablePaginator = page === 1 && limit && limit > (result.rows?.length ?? 0);
+        if (shouldDisablePaginator) {
+            ChartEditor.updateConfig({paginator: {enabled: false}});
+        }
+    }
+
     log('RESULT:');
     log(result);
 
