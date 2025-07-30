04a4718351d650c40dcacafb72c0c3deb7e65492
Irina Kuzmina <kuzmadom@yandex-team.ru>
2025-07-30 18:55:16 +0300

    Fix categories (combined chart) (#2681)

--- src/server/modes/charts/plugins/datalens/js/js.ts
+++ src/server/modes/charts/plugins/datalens/js/js.ts
@@ -384,6 +384,7 @@ type PrepareSingleResultArgs = {
     layerChartMeta?: LayerChartMeta;
     usedColors?: (string | undefined)[];
     features: FeatureConfig;
+    categories?: (string | number)[];
 };
 
 // eslint-disable-next-line complexity
@@ -402,6 +403,7 @@ function prepareSingleResult({
     usedColors,
     palettes,
     features,
+    categories,
 }: PrepareSingleResultArgs) {
     const isVisualizationWithLayers = Boolean(
         (visualization as ServerVisualizationLayer).layerSettings,
@@ -723,6 +725,7 @@ function prepareSingleResult({
         layerChartMeta,
         usedColors,
         features,
+        categories,
     };
 
     return (prepare as PrepareFunction)(prepareFunctionArgs);
@@ -896,6 +899,7 @@ export const buildGraphPrivate = (args: {
     let result: any = [];
     let bounds: null | any = null;
 
+    let categories: string[] = [];
     if (layers) {
         const legendValues: Record<string, string> = {};
         const layerChartMeta = getLayerChartMeta({
@@ -922,7 +926,10 @@ export const buildGraphPrivate = (args: {
                 usedColors,
                 palettes,
                 features,
+                categories,
             });
+
+            categories = categories?.length ? categories : localResult.categories;
 
             if (localResult && localResult[0] && localResult[0].bounds) {
                 const {bounds: localBounds} = localResult[0];
