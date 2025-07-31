be62263818d9b34f35c8f5aa3e599a1824da284f
Aliya Safina <safina-aliia@yandex-team.ru>
2025-07-31 16:00:33 +0300

    Refactor getTopOffsetBySize

--- src/ui/components/DashKit/plugins/Title/utils.ts
+++ src/ui/components/DashKit/plugins/Title/utils.ts
@@ -1,10 +1,7 @@
 import {RECCOMMENDED_LINE_HEIGHT_MULTIPLIER, TITLE_DEFAULT_SIZES} from '@gravity-ui/dashkit';
 import {type DashTitleSize} from 'shared';
 
-import {
-    HINT_SIZE,
-    TITLE_WITH_COLOR_PADDING_TOP as TITLE_WITH_BG_COLOR_PADDING_TOP,
-} from './constants';
+import {HINT_SIZE, TITLE_WITH_BG_COLOR_PADDING_TOP} from './constants';
 
 export const getFontStyleBySize = (size: DashTitleSize) => {
     if (typeof size === 'object' && 'fontSize' in size) {
@@ -25,17 +22,18 @@ export const getTopOffsetBySize = (size: DashTitleSize, showBgColor: boolean) =>
     const defaultPadding = showBgColor ? TITLE_WITH_BG_COLOR_PADDING_TOP : 0;
 
     if (typeof size === 'object' && 'fontSize' in size) {
-        return (
-            (size.fontSize * RECCOMMENDED_LINE_HEIGHT_MULTIPLIER - HINT_SIZE) / 2 + defaultPadding
-        );
+        const lineHeight = size.fontSize * RECCOMMENDED_LINE_HEIGHT_MULTIPLIER;
+
+        return (lineHeight - HINT_SIZE) / 2 + defaultPadding;
     }
 
     if (typeof size === 'string') {
         const fontStyles = TITLE_DEFAULT_SIZES[size];
-        return (
-            (parseInt(fontStyles.lineHeight ?? fontStyles.fontSize, 10) - HINT_SIZE) / 2 +
-            defaultPadding
-        );
+        const lineHeight = fontStyles.lineHeight
+            ? parseInt(fontStyles.lineHeight, 10)
+            : parseInt(fontStyles.fontSize, 10) * RECCOMMENDED_LINE_HEIGHT_MULTIPLIER;
+
+        return (lineHeight - HINT_SIZE) / 2 + defaultPadding;
     }
 
     return defaultPadding;
