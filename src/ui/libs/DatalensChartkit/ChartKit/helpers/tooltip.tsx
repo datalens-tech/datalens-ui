103ce7e3ef90ac22cca5ba325d93301372fc32d3
Irina Kuzmina <kuzmadom@yandex-team.ru>
2025-07-31 15:00:58 +0300

    fix

--- src/ui/libs/DatalensChartkit/ChartKit/helpers/tooltip.tsx
+++ src/ui/libs/DatalensChartkit/ChartKit/helpers/tooltip.tsx
@@ -14,7 +14,7 @@ import {formatNumber} from 'shared/modules/format-units/index';
 
 import type {PointCustomData, ScatterSeriesCustomData} from '../../../../../shared/types/chartkit';
 
-const b = block('g-charts-tooltip');
+const b = block('dl-chart-tooltip-content');
 
 type CustomScatterSeries = ScatterSeries<PointCustomData> & {custom: ScatterSeriesCustomData};
 type TooltipRenderer = NonNullable<ChartTooltip['renderer']>;
