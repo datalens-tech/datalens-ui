d864bbc1de854c6c46122e25889b7cac75504025
Irina Kuzmina <kuzmadom@yandex-team.ru>
2025-08-21 22:42:09 +0300

    fix

--- tests/opensource-suites/wizard/visualizations/pie/markup.test.ts
+++ tests/opensource-suites/wizard/visualizations/pie/markup.test.ts
@@ -15,36 +15,30 @@ datalensTest.describe('Wizard', () => {
             await wizardPage.setVisualization(WizardVisualizationId.PieD3);
         });
 
-        datalensTest(
-            'Markup with special characters in dataLabels with  @screenshot',
-            async ({page}) => {
-                const wizardPage = new WizardPage({page});
+        datalensTest('Markup with special characters in dataLabels @screenshot', async ({page}) => {
+            const wizardPage = new WizardPage({page});
 
-                const chartContainer = page.locator(slct(WizardPageQa.SectionPreview));
-                const chart = chartContainer.locator('.chartkit-graph,.gcharts-chart');
-                const previewLoader = chartContainer.locator(slct(ChartKitQa.Loader));
+            const chartContainer = page.locator(slct(WizardPageQa.SectionPreview));
+            const chart = chartContainer.locator('.chartkit-graph,.gcharts-chart');
+            const previewLoader = chartContainer.locator(slct(ChartKitQa.Loader));
 
-                // Create markup field
-                const measureField = 'markup';
-                await wizardPage.createNewFieldWithFormula(
-                    measureField,
-                    `markup(max('& <>'), br(), color('*', 'salmon'))`,
-                );
-                await wizardPage.sectionVisualization.addFieldByClick(
-                    PlaceholderName.Labels,
-                    measureField,
-                );
-                await wizardPage.sectionVisualization.addFieldByClick(
-                    PlaceholderName.Colors,
-                    'region',
-                );
-                await wizardPage.sectionVisualization.addFieldByClick(
-                    PlaceholderName.Measures,
-                    'Sales',
-                );
-                await expect(previewLoader).not.toBeVisible();
-                await expect(chart).toHaveScreenshot();
-            },
-        );
+            // Create markup field
+            const measureField = 'markup';
+            await wizardPage.createNewFieldWithFormula(
+                measureField,
+                `markup(max('& <>'), br(), color('*', 'salmon'))`,
+            );
+            await wizardPage.sectionVisualization.addFieldByClick(
+                PlaceholderName.Labels,
+                measureField,
+            );
+            await wizardPage.sectionVisualization.addFieldByClick(PlaceholderName.Colors, 'region');
+            await wizardPage.sectionVisualization.addFieldByClick(
+                PlaceholderName.Measures,
+                'Sales',
+            );
+            await expect(previewLoader).not.toBeVisible();
+            await expect(chart).toHaveScreenshot();
+        });
     });
 });
