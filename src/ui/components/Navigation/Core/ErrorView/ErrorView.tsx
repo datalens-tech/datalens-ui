3727c69d557ee31136e84e9f69c353b876a1710d
Damir Battalov <damirbattalov@yandex-team.ru>
2025-08-22 16:01:55 +0300

    Used PlaceholderIllustration component to display Navigation block errors

--- src/ui/components/Navigation/Core/ErrorView/ErrorView.tsx
+++ src/ui/components/Navigation/Core/ErrorView/ErrorView.tsx
@@ -4,6 +4,8 @@ import {Button} from '@gravity-ui/uikit';
 import block from 'bem-cn-lite';
 import {I18n} from 'i18n';
 import type {DataLensApiError} from 'typings';
+import {PlaceholderIllustration} from 'ui/components/PlaceholderIllustration/PlaceholderIllustration';
+import type {PlaceholderIllustrationProps} from 'ui/components/PlaceholderIllustration/types';
 import {parseError} from 'utils/errors/parse';
 
 import './ErrorView.scss';
@@ -19,27 +21,32 @@ type Props = {
 export default function ErrorView({error, onRetryClick}: Props) {
     const {status} = parseError(error);
 
-    let text;
-    let hasButton = true;
+    let errorViewConfig: PlaceholderIllustrationProps;
 
     switch (status) {
         case 403: {
-            text = i18n('label_access-denied-folder');
-            hasButton = false;
+            errorViewConfig = {
+                title: i18n('label_access-denied-folder'),
+                renderAction: () => (
+                    <Button view="action" size="l" onClick={onRetryClick}>
+                        {i18n('button_retry')}
+                    </Button>
+                ),
+                name: 'noAccess',
+            };
             break;
         }
-        default:
-            text = i18n('label_failed-load-navigation');
+        default: {
+            errorViewConfig = {
+                title: i18n('label_failed-load-navigation'),
+                name: 'error',
+            };
+        }
     }
 
     return (
         <div className={b()}>
-            <div className={b('message')}>{text}</div>
-            {hasButton && (
-                <Button view="action" size="l" onClick={onRetryClick}>
-                    {i18n('button_retry')}
-                </Button>
-            )}
+            <PlaceholderIllustration {...errorViewConfig} direction="column" />
         </div>
     );
 }
