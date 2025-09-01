3a066ab3659e348f1ba641a66e6cc2a76bde2149
Sevastian Dvortsov <dvortsovs@yandex-team.ru>
2025-09-01 17:56:16 +0300

    Replace table in QL table preview (#2808)

--- src/ui/units/ql/containers/PaneTablePreview/PaneTablePreview.tsx
+++ src/ui/units/ql/containers/PaneTablePreview/PaneTablePreview.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 
-import DataTable from '@gravity-ui/react-data-table';
 import block from 'bem-cn-lite';
-import {i18n} from 'i18n';
-import _ from 'lodash';
 import {connect} from 'react-redux';
 import type {DatalensGlobalState} from 'ui';
+import TableWidget from 'ui/libs/DatalensChartkit/ChartKit/plugins/Table/renderer/TableWidget';
 
 import type {QlConfigPreviewTableData} from '../../../../../shared';
+import {ChartKitQlPreviewTableQa} from '../../../../../shared';
 import {getTablePreviewData} from '../../store/reducers/ql';
 
 import './PaneTablePreview.scss';
@@ -21,7 +20,6 @@ interface PreviewTableProps {
 class PreviewTable extends React.PureComponent<PreviewTableProps> {
     render() {
         const {tablePreviewData} = this.props;
-
         const {columns, data} = tablePreviewData;
 
         return (
@@ -29,18 +27,19 @@ class PreviewTable extends React.PureComponent<PreviewTableProps> {
                 {typeof columns === 'undefined' || typeof data === 'undefined' ? (
                     <div></div>
                 ) : (
-                    <DataTable
-                        columns={columns}
-                        data={data}
-                        emptyDataMessage={i18n('dataset.dataset-editor.modify', 'label_no-data')}
-                        settings={{
-                            highlightRows: true,
-                            stripedRows: false,
-                            displayIndices: false,
-                            dynamicRender: true,
+                    <TableWidget
+                        qa={ChartKitQlPreviewTableQa.Widget}
+                        isQlPreviewTable
+                        id="ql-preview-table"
+                        data={{
+                            type: 'table',
+                            controls: null,
+                            params: {},
+                            data: {
+                                head: columns,
+                                rows: data,
+                            },
                         }}
-                        rowClassName={() => b('row')}
-                        theme={'preview-dataset'}
                     />
                 )}
             </div>
