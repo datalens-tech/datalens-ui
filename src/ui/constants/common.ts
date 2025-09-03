fcaf605b40e050391009674115da1e10f6dfb65a
Taya Leutina <leutinatasya@yandex-team.ru>
2025-09-03 14:42:28 +0300

    Isolate yfm styles only for main app (#2837)

--- src/ui/constants/common.ts
+++ src/ui/constants/common.ts
@@ -433,3 +433,5 @@ export const EMBEDDED_DASH_MESSAGE_NAME = 'subscribe-for-embed-height-dash';
 export const SYSTEM_GROUP_IDS = ['allUsers', 'allAuthenticatedUsers'];
 
 export const CLIPBOARD_TIMEOUT = 1000;
+
+export const APP_ROOT_CLASS = 'app-root';
