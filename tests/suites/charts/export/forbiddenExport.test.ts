54308fde46db30010ce3c4df99af98b28c2a7718
Daria Larionova <darialari@yandex-team.ru>
2025-09-05 16:26:18 +0300

    Check dataset/connection export setting

--- tests/suites/charts/export/forbiddenExport.test.ts
+++ tests/suites/charts/export/forbiddenExport.test.ts
@@ -3,7 +3,11 @@ import {ChartkitMenuDialogsQA} from '../../../../src/shared/constants';
 
 import WizardPage from '../../../page-objects/wizard/WizardPage';
 import {openTestPage, slct} from '../../../utils';
-import {RobotChartsWizardUrls} from '../../../utils/constants';
+import {
+    RobotChartsPreviewUrls,
+    RobotChartsSQLEditorUrls,
+    RobotChartsWizardUrls,
+} from '../../../utils/constants';
 import datalensTest from '../../../utils/playwright/globalTestDefinition';
 import {MenuItemsIds} from '../../../../src/shared';
 
@@ -13,11 +17,56 @@ const PARAMS = {
 };
 
 datalensTest.describe('Chart with forbidden export', () => {
+    datalensTest.describe('Wizard', () => {
+        datalensTest(
+            'Export menu item is disabled if export is forbidden on connection',
+            async ({page}: {page: Page}) => {
+                const wizardPage = new WizardPage({page});
+                await openTestPage(
+                    page,
+                    RobotChartsWizardUrls.WizardWithForbiddenOnConnectionExport,
+                );
+                await wizardPage.waitForSuccessfulResponse('/api/run');
+
+                await wizardPage.chartkit.openChartMenu();
+
+                const menuItemsCount = await page
+                    .locator(`${slct(ChartkitMenuDialogsQA.chartMenuDropDown)} > li`)
+                    .count();
+
+                const exportMenuItem = await page.locator(slct(MenuItemsIds.EXPORT));
+
+                expect(menuItemsCount).toEqual(PARAMS.WIZARD_MENU_ITEMS_COUNT);
+                expect(await exportMenuItem.getAttribute('tabindex')).toBe('-1');
+            },
+        );
+
+        datalensTest(
+            'Export menu item is disabled if export is forbidden on dataset',
+            async ({page}: {page: Page}) => {
+                const wizardPage = new WizardPage({page});
+                await openTestPage(page, RobotChartsWizardUrls.WizardWithForbiddenOnDatasetExport);
+                await wizardPage.waitForSuccessfulResponse('/api/run');
+
+                await wizardPage.chartkit.openChartMenu();
+
+                const menuItemsCount = await page
+                    .locator(`${slct(ChartkitMenuDialogsQA.chartMenuDropDown)} > li`)
+                    .count();
+
+                const exportMenuItem = await page.locator(slct(MenuItemsIds.EXPORT));
+
+                expect(menuItemsCount).toEqual(PARAMS.WIZARD_MENU_ITEMS_COUNT);
+                expect(await exportMenuItem.getAttribute('tabindex')).toBe('-1');
+            },
+        );
+    });
+
     datalensTest(
-        'Export menu item is disabled in wizard chart on connection with forbidden export',
+        'Ql - Export menu item is disabled if export is forbidden on connection',
         async ({page}: {page: Page}) => {
             const wizardPage = new WizardPage({page});
-            await openTestPage(page, RobotChartsWizardUrls.WizardWithForbiddenExport);
+            await openTestPage(page, RobotChartsSQLEditorUrls.QLWithForbiddenExport);
             await wizardPage.waitForSuccessfulResponse('/api/run');
 
             await wizardPage.chartkit.openChartMenu();
@@ -32,4 +81,72 @@ datalensTest.describe('Chart with forbidden export', () => {
             expect(await exportMenuItem.getAttribute('tabindex')).toBe('-1');
         },
     );
+
+    datalensTest.describe('Preview', () => {
+        datalensTest(
+            'Wizard chart - Export menu item is disabled if export is forbidden on connection',
+            async ({page}: {page: Page}) => {
+                const wizardPage = new WizardPage({page});
+                await openTestPage(
+                    page,
+                    RobotChartsPreviewUrls.PreviewWizardWithForbiddenOnConnectionExport,
+                );
+                await wizardPage.waitForSuccessfulResponse('/api/run');
+
+                await wizardPage.chartkit.openChartMenu();
+
+                const menuItemsCount = await page
+                    .locator(`${slct(ChartkitMenuDialogsQA.chartMenuDropDown)} > li`)
+                    .count();
+
+                const exportMenuItem = await page.locator(slct(MenuItemsIds.EXPORT));
+
+                expect(menuItemsCount).toEqual(PARAMS.WIZARD_MENU_ITEMS_COUNT);
+                expect(await exportMenuItem.getAttribute('tabindex')).toBe('-1');
+            },
+        );
+
+        datalensTest(
+            'Wizard chart - Export menu item is disabled if export is forbidden on dataset',
+            async ({page}: {page: Page}) => {
+                const wizardPage = new WizardPage({page});
+                await openTestPage(
+                    page,
+                    RobotChartsPreviewUrls.PreviewWizardWithForbiddenOnDatasetExport,
+                );
+                await wizardPage.waitForSuccessfulResponse('/api/run');
+
+                await wizardPage.chartkit.openChartMenu();
+
+                const menuItemsCount = await page
+                    .locator(`${slct(ChartkitMenuDialogsQA.chartMenuDropDown)} > li`)
+                    .count();
+
+                const exportMenuItem = await page.locator(slct(MenuItemsIds.EXPORT));
+
+                expect(menuItemsCount).toEqual(PARAMS.WIZARD_MENU_ITEMS_COUNT);
+                expect(await exportMenuItem.getAttribute('tabindex')).toBe('-1');
+            },
+        );
+
+        datalensTest(
+            'Ql chart - Export menu item is disabled if export is forbidden on connection',
+            async ({page}: {page: Page}) => {
+                const wizardPage = new WizardPage({page});
+                await openTestPage(page, RobotChartsPreviewUrls.PreviewQLWithForbiddenExport);
+                await wizardPage.waitForSuccessfulResponse('/api/run');
+
+                await wizardPage.chartkit.openChartMenu();
+
+                const menuItemsCount = await page
+                    .locator(`${slct(ChartkitMenuDialogsQA.chartMenuDropDown)} > li`)
+                    .count();
+
+                const exportMenuItem = await page.locator(slct(MenuItemsIds.EXPORT));
+
+                expect(menuItemsCount).toEqual(PARAMS.WIZARD_MENU_ITEMS_COUNT);
+                expect(await exportMenuItem.getAttribute('tabindex')).toBe('-1');
+            },
+        );
+    });
 });
