71c119f7627e449bd577280c7b390fce403524c8
Georgy Malkov <malkovgv@yandex-team.ru>
2025-09-19 23:37:36 +0300

    fix

--- scripts/e2e/us-dump.sh
+++ scripts/e2e/us-dump.sh
@@ -8,6 +8,28 @@ set -eo pipefail
 
 SCRIPT_DIR=$(dirname -- "$(readlink -f -- "$0")")
 
+IS_CLEAR_DELETED="false"
+IS_CLEAR_E2E="false"
+
+# parse args
+for _ in "$@"; do
+    case ${1} in
+    --clear-deleted)
+        IS_CLEAR_DELETED="true"
+        shift # past argument with no value
+        ;;
+    --clear-e2e)
+        IS_CLEAR_E2E="true"
+        shift # past argument with no value
+        ;;
+    -*)
+        echo "unknown arg: ${1}"
+        exit 1
+        ;;
+    *) ;;
+    esac
+done
+
 echo ""
 echo "Start dump UnitedStorage entries..."
 echo "  - workbooks"
@@ -15,6 +37,13 @@ echo "  - collections"
 echo "  - entries"
 echo "  - revisions"
 echo "  - links"
+
+if [ "${IS_CLEAR_DELETED}" = "true" ]; then
+    echo "+ clear deleted entries automatically..."
+fi
+if [ "${IS_CLEAR_E2E}" = "true" ]; then
+    echo "+ clear e2e entries automatically..."
+fi
 
 COMPOSE_FILE=$(readlink -f "${SCRIPT_DIR}/../../tests/docker-compose.e2e.yml")
 DUMP_FILE=$(readlink -f "${SCRIPT_DIR}/../../tests/data/us-e2e-data.sql")
@@ -32,6 +61,50 @@ docker --log-level error compose -f "${COMPOSE_FILE}" exec \
     sed -E 's|"db_name": "[^"]+"|"db_name": "{{POSTGRES_DB}}"|' |
     sed -E 's|"username": "[^"]+"|"username": "{{POSTGRES_USER}}"|' \
         >>"${DUMP_FILE}"
+
+if [ "${IS_CLEAR_DELETED}" = "true" ]; then
+    DUMP=$(cat "${DUMP_FILE}")
+    DELETED_ENTRIES=$(
+        echo "${DUMP}" |
+            grep '__trash/' |
+            grep 'public.entries' |
+            grep -oE '__trash/[0-9]+_' |
+            sed 's|__trash/||' |
+            sed 's|_||' |
+            tr -d ' ' |
+            sort |
+            uniq
+    )
+
+    IFS=$'\n'
+    for DELETED_ENTRY in ${DELETED_ENTRIES}; do
+        echo "  removing entry: ${DELETED_ENTRY}"
+        DUMP=$(echo "${DUMP}" | { grep -v ", ${DELETED_ENTRY}, " || true; } | { grep -v "(${DELETED_ENTRY}, " || true; })
+    done
+    unset
+fi
+
+if [ "${IS_CLEAR_E2E}" = "true" ]; then
+    DUMP=$(cat "${DUMP_FILE}")
+    E2E_ENTRIES=$(
+        echo "${DUMP}" |
+            grep 'e2e-entry-' |
+            grep 'public.entries' |
+            grep -oE ", '[0-9]+/" |
+            sed "s|, '||" |
+            sed 's|/||' |
+            tr -d ' ' |
+            sort |
+            uniq
+    )
+
+    IFS=$'\n'
+    for E2E_ENTRY in ${E2E_ENTRIES}; do
+        echo "  removing entry: ${E2E_ENTRY}"
+        DUMP=$(echo "${DUMP}" | { grep -v ", ${E2E_ENTRY}, " || true; } | { grep -v "(${E2E_ENTRY}, " || true; })
+    done
+    unset
+fi
 
 E2E_ENTRIES=$(cat "${DUMP_FILE}" | { grep "e2e-entry-" || true; })
 DELETED_ENTRIES=$(cat "${DUMP_FILE}" | { grep "__trash/" || true; })
