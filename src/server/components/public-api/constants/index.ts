b46bb3bac6951ee175f34d24ff40355acad4e7d6
Vladimir Stepanenko <endurance@yandex-team.ru>
2025-09-23 12:21:32 +0300

    Fixes

--- src/server/components/public-api/constants/index.ts
+++ src/server/components/public-api/constants/index.ts
@@ -1,7 +1,6 @@
 import type {PublicApiBaseConfig} from '../types';
 
 import {PUBLIC_API_V0_CONFIG} from './v0';
-import {PUBLIC_API_VNULL_CONFIG} from './vNull';
 
 export * from './common';
 
@@ -10,14 +9,11 @@ export const PUBLIC_API_URL = '/rpc/:action';
 export const PUBLIC_API_ROUTE = `${PUBLIC_API_HTTP_METHOD} ${PUBLIC_API_URL}`;
 
 export const PUBLIC_API_VERSION = {
-    vNull: 'vnull',
     v0: 'v0',
 } as const;
 
 export const PUBLIC_API_LATEST_VERSION = PUBLIC_API_VERSION.v0;
 
 export const PUBLIC_API_BASE_CONFIG = {
-    [PUBLIC_API_VERSION.vNull]: PUBLIC_API_VNULL_CONFIG,
-
     [PUBLIC_API_VERSION.v0]: PUBLIC_API_V0_CONFIG,
 } satisfies PublicApiBaseConfig;
