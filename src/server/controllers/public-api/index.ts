9a93938979e96bab3233b065b3da67df9108a95a
Vladimir Stepanenko 🏴‍☠️ <endurance@yandex-team.ru>
2025-09-29 13:03:23 +0300

    Add dashboards to public api (#2905)

--- src/server/controllers/public-api/index.ts
+++ src/server/controllers/public-api/index.ts
@@ -1,59 +1,30 @@
 import type {Request, Response} from '@gravity-ui/expresskit';
-import {AppError, REQUEST_ID_PARAM_NAME} from '@gravity-ui/nodekit';
+import {REQUEST_ID_PARAM_NAME} from '@gravity-ui/nodekit';
 
-import {getValidationSchema} from '../../../shared/schema/gateway-utils';
-import {registerActionToOpenApi} from '../../components/public-api';
 import {registry} from '../../registry';
-import type {AnyApiServiceActionConfig, DatalensGatewaySchemas} from '../../types/gateway';
+import type {DatalensGatewaySchemas} from '../../types/gateway';
 import Utils from '../../utils';
 
 import {PUBLIC_API_ERRORS, PublicApiError} from './constants';
-import {prepareError, validateRequestBody} from './utils';
+import {parseRequestApiVersion, prepareError, validateRequestBody} from './utils';
 
 export const createPublicApiController = () => {
     const {gatewayApi} = registry.getGatewayApi<DatalensGatewaySchemas>();
-    const schemasByScope = registry.getGatewaySchemasByScope();
-    const {proxyMap} = registry.getPublicApiConfig();
-
-    const actionToPathMap = new Map<Function, {serviceName: string; actionName: string}>();
-
-    Object.entries(gatewayApi).forEach(([serviceName, actions]) => {
-        Object.entries(actions).forEach(([actionName, action]) => {
-            actionToPathMap.set(action, {serviceName, actionName});
-        });
-    });
-
-    const actionToConfigMap = new Map<Function, AnyApiServiceActionConfig>();
-
-    Object.entries(proxyMap).forEach(([version, actions]) => {
-        Object.entries(actions).forEach(([actionName, {resolve, openApi}]) => {
-            const gatewayAction = resolve(gatewayApi);
-            const pathObject = actionToPathMap.get(gatewayAction);
-
-            if (!pathObject) {
-                throw new AppError('Public api proxyMap action not found in gatewayApi.');
-            }
-
-            const actionConfig =
-                schemasByScope.root[pathObject.serviceName].actions[pathObject.actionName];
-
-            actionToConfigMap.set(gatewayAction, actionConfig);
-
-            registerActionToOpenApi({actionConfig, actionName, version, openApi});
-        });
-    });
+    const {baseConfig} = registry.getPublicApiConfig();
 
     return async function publicApiController(req: Request, res: Response) {
         try {
-            const {version, action: actionName} = req.params;
+            const {action: actionName} = req.params;
 
-            if (!version || !actionName || !proxyMap[version] || !proxyMap[version][actionName]) {
+            const version = parseRequestApiVersion(req);
+
+            if (!actionName || !baseConfig[version].actions[actionName]) {
                 throw new PublicApiError(`Endpoint ${req.path} does not exist`, {
                     code: PUBLIC_API_ERRORS.ENDPOINT_NOT_FOUND,
                 });
             }
 
-            const action = proxyMap[version][actionName];
+            const action = baseConfig[version].actions[actionName];
 
             const {ctx} = req;
 
@@ -61,27 +32,8 @@ export const createPublicApiController = () => {
             const requestId = ctx.get(REQUEST_ID_PARAM_NAME) || '';
 
             const gatewayAction = action.resolve(gatewayApi);
-            const gatewayActionConfig = actionToConfigMap.get(gatewayAction);
 
-            if (!gatewayActionConfig) {
-                req.ctx.logError(`Couldn't find action config in actionToConfigMap`);
-                throw new PublicApiError(PUBLIC_API_ERRORS.ACTION_CONFIG_NOT_FOUND, {
-                    code: PUBLIC_API_ERRORS.ACTION_CONFIG_NOT_FOUND,
-                });
-            }
-
-            const validationSchema = getValidationSchema(gatewayActionConfig);
-
-            if (!validationSchema) {
-                req.ctx.logError(`Couldn't find action validation schema`);
-                throw new PublicApiError(PUBLIC_API_ERRORS.ACTION_VALIDATION_SCHEMA_NOT_FOUND, {
-                    code: PUBLIC_API_ERRORS.ACTION_VALIDATION_SCHEMA_NOT_FOUND,
-                });
-            }
-
-            const {paramsSchema} = validationSchema;
-
-            const validatedArgs = await validateRequestBody(paramsSchema, req.body);
+            const validatedArgs = await validateRequestBody(action.schemas.args, req.body);
 
             const result = await gatewayAction({
                 headers,
@@ -90,7 +42,7 @@ export const createPublicApiController = () => {
                 requestId,
             });
 
-            res.status(200).send(result.responseData);
+            res.status(200).send(result);
         } catch (err: unknown) {
             const {status, message, code, details} = prepareError(err);
 
