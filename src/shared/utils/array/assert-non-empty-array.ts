c44486de0eafd5687ac9649e79bb9046344320c6
Vladimir Stepanenko <endurance@yandex-team.ru>
2025-11-05 14:54:14 +0300

    Fix

--- src/shared/utils/array/assert-non-empty-array.ts
+++ src/shared/utils/array/assert-non-empty-array.ts
@@ -1,5 +1,5 @@
-export function assertNonEmptyArray<T>(arr: T[]): asserts arr is [T, ...T[]] {
+export function assertNonEmptyArray<T>(arr: T[], message?: string): asserts arr is [T, ...T[]] {
     if (arr.length === 0) {
-        throw new Error('Array for discriminated union must not be empty');
+        throw new Error(message ?? 'Array must not be empty!');
     }
 }
