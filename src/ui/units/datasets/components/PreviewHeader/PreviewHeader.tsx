4cbc81101b932745eb4a7e688d5b3980f10a6140
Arsen Afaunov <arsen-afaunov@yandex-team.ru>
2025-11-07 12:01:48 +0300

    Fix preview table header rows number input

--- src/ui/units/datasets/components/PreviewHeader/PreviewHeader.tsx
+++ src/ui/units/datasets/components/PreviewHeader/PreviewHeader.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import {ChevronsExpandUpRight, LayoutHeader, LayoutSideContent, Xmark} from '@gravity-ui/icons';
-import {Button, Icon, TextInput} from '@gravity-ui/uikit';
+import {Button, Icon, TextInput, type TextInputProps} from '@gravity-ui/uikit';
 import block from 'bem-cn-lite';
 import {i18n} from 'i18n';
 import _debounce from 'lodash/debounce';
@@ -27,13 +27,31 @@ interface Props {
     refetchPreviewDataset: () => void;
 }
 
-class PreviewHeader extends React.Component<Props> {
+interface State {
+    internalAmountPreviewRows: string;
+}
+
+const TEXT_INPUT_CONTROL_PROPS: TextInputProps['controlProps'] = {
+    min: 0,
+    step: 1,
+};
+
+class PreviewHeader extends React.Component<Props, State> {
     static defaultProps = {};
 
     private debouncedChangeAmountPreviewRows = _debounce(this.props.refetchPreviewDataset, 1000);
 
+    constructor(props: Props) {
+        super(props);
+
+        this.state = {
+            internalAmountPreviewRows: props.amountPreviewRows.toString(),
+        };
+    }
+
     render() {
-        const {view, amountPreviewRows} = this.props;
+        const {view} = this.props;
+        const {internalAmountPreviewRows} = this.state;
 
         return (
             <div className={b({view})}>
@@ -45,8 +63,9 @@ class PreviewHeader extends React.Component<Props> {
                     <TextInput
                         className={b('amount-preview-rows-inp')}
                         type="number"
-                        value={String(amountPreviewRows)}
+                        value={internalAmountPreviewRows}
                         onUpdate={this.changeAmountPreviewRows}
+                        controlProps={TEXT_INPUT_CONTROL_PROPS}
                     />
                     <span className={b('fade-text')}>
                         {i18n('dataset.dataset-editor.modify', 'label_max-amount-rows', {
@@ -106,18 +125,20 @@ class PreviewHeader extends React.Component<Props> {
         );
     }
 
-    changeAmountPreviewRows = (amountPreviewRows: string) => {
+    changeAmountPreviewRows = (amountPreviewRowsString: string) => {
         const {changeAmountPreviewRows} = this.props;
 
         if (this.debouncedChangeAmountPreviewRows) {
             this.debouncedChangeAmountPreviewRows.cancel();
         }
 
-        if (shouldFetchPreview(amountPreviewRows)) {
+        if (shouldFetchPreview(amountPreviewRowsString)) {
             this.debouncedChangeAmountPreviewRows();
         }
 
-        const amountPreviewRowsNumber = parseInt(amountPreviewRows, 10);
+        this.setState({internalAmountPreviewRows: amountPreviewRowsString});
+
+        const amountPreviewRowsNumber = parseInt(amountPreviewRowsString, 10);
 
         if (Number.isNaN(amountPreviewRowsNumber)) {
             return;
