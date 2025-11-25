824ae9f55bcb5b7aff46eae15a0c0594ef5e4e50
Sevastian Dvortsov <dvortsovs@yandex-team.ru>
2025-11-25 14:50:13 +0300

    Add remove dialog for shared connection (#3166)

--- src/ui/components/DialogSharedEntryBindings/DialogSharedEntryBindings.tsx
+++ src/ui/components/DialogSharedEntryBindings/DialogSharedEntryBindings.tsx
@@ -1,288 +1,120 @@
 import React from 'react';
 
-import {Button, Dialog, Divider, SegmentedRadioGroup as RadioButton} from '@gravity-ui/uikit';
+import {Dialog, Divider} from '@gravity-ui/uikit';
 import block from 'bem-cn-lite';
-import debounce from 'lodash/debounce';
-import {useDispatch} from 'react-redux';
-import {CollectionItemEntities} from 'shared';
-import type {GetEntryResponse, SharedEntry, SharedEntryBindingsItem} from 'shared/schema';
-import {getSdk} from 'ui/libs/schematic-sdk';
-import type {AppDispatch} from 'ui/store';
-import {showToast} from 'ui/store/actions/toaster';
 import {getSharedEntryMockText} from 'ui/units/collections/components/helpers';
 
-import {closeDialog, openDialog} from '../../store/actions/dialog';
 import DialogManager from '../DialogManager/DialogManager';
-import {DIALOG_SHARED_ENTRY_PERMISSIONS} from '../DialogSharedEntryPermissions/DialogSharedEntryPermissions';
-import {DIALOG_SHARED_ENTRY_UNBIND} from '../DialogSharedEntryUnbind/DialogSharedEntryUnbind';
 import {EntitiesList} from '../EntitiesList/EntitiesList';
-import {PlaceholderIllustration} from '../PlaceholderIllustration/PlaceholderIllustration';
-import {SharedBindingsList} from '../SharedBindingsList/SharedBindingsList';
+import {SmartLoader} from '../SmartLoader/SmartLoader';
 
-import type {AttachmentValue} from './constants';
-import {Attachment, ObjectsListTitles, SEARCH_DELAY} from './constants';
+import {DeleteAlert} from './components/DeleteAlert';
+import {DirectionControl} from './components/DirectionControl';
+import {Relations} from './components/Relations';
+import {SharedBindingsFooter} from './components/SharedBindingsFooter';
+import {SharedBindingsHeader} from './components/SharedBindingsHeader';
+import {DialogClassName} from './constants';
+import {useSharedEntryBindings} from './hooks/useSharedEntryBindings';
+import type {SharedEntry} from './types';
 
 import './DialogSharedEntryBindings.scss';
 
 type DialogSharedEntryBindingsProps = {
     open: boolean;
-    entry: GetEntryResponse;
+    entry: SharedEntry;
     onClose: () => void;
+    isDeleteDialog?: boolean;
+    onDeleteSuccess?: () => void;
 };
 
 export const DIALOG_SHARED_ENTRY_BINDINGS = Symbol('DIALOG_SHARED_ENTRY_BINDINGS');
-
-const CONCURRENT_ID = 'shared-entry-bindings';
-const cancelConcurrentRequest = () => getSdk().cancelRequest(CONCURRENT_ID);
 
 export interface OpenDialogSharedEntryBindingArgs {
     id: typeof DIALOG_SHARED_ENTRY_BINDINGS;
     props: DialogSharedEntryBindingsProps;
 }
 
-const sortEntities = (entities: SharedEntryBindingsItem[]) => {
-    return entities.sort((entity) => (entity.entity === CollectionItemEntities.WORKBOOK ? -1 : 1));
-};
-
-const b = block('dialog-shared-entries-binding');
-
-const getIsRelationUnbind = (
-    currentDirection: AttachmentValue,
-    item: SharedEntryBindingsItem,
-): item is SharedEntryBindingsItem & SharedEntry => {
-    return currentDirection === Attachment.TARGET && 'scope' in item;
-};
+const b = block(DialogClassName);
 
 export const DialogSharedEntryBindings: React.FC<DialogSharedEntryBindingsProps> = ({
     onClose,
     open,
     entry,
+    onDeleteSuccess,
+    isDeleteDialog = false,
 }) => {
-    const [currentDirection, setCurrentDirection] = React.useState<AttachmentValue>(
-        Attachment.SOURCE,
-    );
-    const dispatch: AppDispatch = useDispatch();
-    const [entities, setEntities] = React.useState<SharedEntryBindingsItem[]>([]);
-
-    const [searchFilter, setSearchFilter] = React.useState('');
-    const [isLoading, setIsLoading] = React.useState(true);
-    const [isSearchLoading, setIsSearchLoading] = React.useState(false);
-    const [isError, setIsError] = React.useState(false);
-
+    const {
+        isLoading,
+        isSearchLoading,
+        onSearch,
+        isError,
+        searchValue,
+        entities,
+        onDirectionChange,
+        currentDirection,
+        fetchEntityBindings,
+    } = useSharedEntryBindings({
+        entry,
+    });
     const showDirectionControl = entry.scope === 'dataset';
 
-    const handleDirectionChange = (value: AttachmentValue) => {
-        setCurrentDirection(value);
-    };
-
-    const fetchEntityBindings = React.useCallback(
-        (filter = '') => {
-            setIsLoading(true);
-            setIsError(false);
-            cancelConcurrentRequest();
-            getSdk()
-                .sdk.us.getSharedEntryBindings({
-                    entryId: entry.entryId,
-                    entryAs: currentDirection,
-                    filterString: filter ? filter : undefined,
-                    mode: 'all',
-                })
-                .then((response) => {
-                    setEntities(sortEntities(response.items));
-                    setIsLoading(false);
-                    setIsSearchLoading(false);
-                })
-                .catch((error) => {
-                    if (error.isCancelled) {
-                        return;
-                    }
-                    setIsError(true);
-                    setIsLoading(false);
-                    setIsSearchLoading(false);
-                });
-        },
-        [entry, currentDirection],
-    );
-
-    const debouncedSearch = React.useMemo(
-        () =>
-            debounce((value: string) => {
-                setIsSearchLoading(true);
-                fetchEntityBindings(value);
-            }, SEARCH_DELAY),
-        [fetchEntityBindings],
-    );
-
-    const onSearch = React.useCallback(
-        (value: string) => {
-            setSearchFilter(value);
-            debouncedSearch(value);
-        },
-        [debouncedSearch],
-    );
-
-    React.useEffect(() => {
-        return () => {
-            debouncedSearch.cancel();
-        };
-    }, [debouncedSearch]);
-
-    React.useEffect(() => {
-        setSearchFilter('');
-        fetchEntityBindings();
-    }, [fetchEntityBindings]);
-
-    const getListItemActions = React.useCallback(
-        (item: SharedEntryBindingsItem) => {
-            return [
-                {
-                    text: getSharedEntryMockText('shared-bindings-list-action-unbind'),
-                    action: () =>
-                        dispatch(
-                            openDialog({
-                                id: DIALOG_SHARED_ENTRY_UNBIND,
-                                props: {
-                                    entry: getIsRelationUnbind(currentDirection, item)
-                                        ? item
-                                        : entry,
-                                    onClose: () => dispatch(closeDialog()),
-                                    onApply: async () => {
-                                        try {
-                                            await getSdk().sdk.us.deleteSharedEntryBinding({
-                                                sourceId: entry.entryId,
-                                                targetId:
-                                                    item.entity === CollectionItemEntities.WORKBOOK
-                                                        ? item.workbookId!
-                                                        : item.entryId,
-                                            });
-                                            dispatch(closeDialog());
-                                            fetchEntityBindings();
-                                        } catch (error) {
-                                            dispatch(
-                                                showToast({
-                                                    title: error.message,
-                                                    error,
-                                                }),
-                                            );
-                                        }
-                                    },
-                                    open: true,
-                                    relation: getIsRelationUnbind(currentDirection, item)
-                                        ? undefined
-                                        : item,
-                                },
-                            }),
-                        ),
-                },
-                {
-                    text: getSharedEntryMockText('shared-bindings-list-action-change-permissions'),
-                    action: () =>
-                        dispatch(
-                            openDialog({
-                                id: DIALOG_SHARED_ENTRY_PERMISSIONS,
-                                props: {
-                                    entry,
-                                    onClose: () => dispatch(closeDialog()),
-                                    onApply: async (delegate) => {
-                                        try {
-                                            await getSdk().sdk.us.updateSharedEntryBinding({
-                                                sourceId: entry.entryId,
-                                                targetId:
-                                                    item.entity === CollectionItemEntities.WORKBOOK
-                                                        ? item.workbookId!
-                                                        : item.entryId,
-                                                delegation: delegate,
-                                            });
-                                            dispatch(closeDialog());
-                                            fetchEntityBindings();
-                                        } catch (error) {
-                                            dispatch(
-                                                showToast({
-                                                    title: error.message,
-                                                    error,
-                                                }),
-                                            );
-                                        }
-                                    },
-                                    open: true,
-                                    delegation: item.isDelegated,
-                                    relation: item,
-                                },
-                            }),
-                        ),
-                },
-            ];
-        },
-        [currentDirection, dispatch, entry, fetchEntityBindings],
-    );
-
-    const renderRelations = () => {
-        if (isError) {
-            const renderRetryAction = () => (
-                <Button className={b('button-retry')} size="l" view="action" onClick={() => {}}>
-                    {getSharedEntryMockText('bindings-dialog-retry-btn')}
-                </Button>
-            );
-
-            return (
-                <div className={b('error-state')}>
-                    <PlaceholderIllustration
-                        direction="column"
-                        name="error"
-                        title={getSharedEntryMockText('bindings-dialog-error')}
-                        renderAction={renderRetryAction}
-                    />
-                </div>
-            );
-        }
-
-        return (
-            <SharedBindingsList
-                entities={entities}
-                searchProps={{
-                    value: searchFilter,
-                    onUpdate: onSearch,
-                    placeholder: getSharedEntryMockText('entries-list-search-placeholder'),
-                    disabled: isLoading || isError,
-                    loading: isSearchLoading,
-                }}
-                title={
-                    showDirectionControl
-                        ? getSharedEntryMockText(ObjectsListTitles[currentDirection])
-                        : undefined
-                }
-                getListItemActions={getListItemActions}
-                isLoading={isLoading}
-            />
-        );
-    };
-
     return (
-        <Dialog open={open} onClose={onClose} className={b()}>
-            <Dialog.Header caption={getSharedEntryMockText('title-bindings-dialog')} />
+        <Dialog
+            open={open}
+            onClose={onClose}
+            className={b({'empty-list': isDeleteDialog && entities.length === 0})}
+        >
+            <SharedBindingsHeader isDeleteDialog={isDeleteDialog} entry={entry} />
             <Dialog.Body className={b('body')}>
                 <EntitiesList
                     entities={[entry]}
                     title={getSharedEntryMockText('label-current-entry')}
                     className={b('current-row')}
                 />
-                {!showDirectionControl && <Divider className={b('divider')} />}
-                {showDirectionControl && (
-                    <RadioButton
-                        className={b('direction')}
-                        value={currentDirection}
-                        onUpdate={handleDirectionChange}
-                        width="auto"
-                    >
-                        <RadioButton.Option value={Attachment.SOURCE}>
-                            {getSharedEntryMockText('label-attachment-source')}
-                        </RadioButton.Option>
-                        <RadioButton.Option value={Attachment.TARGET}>
-                            {getSharedEntryMockText('label-attachment-target')}
-                        </RadioButton.Option>
-                    </RadioButton>
+                {isLoading && !isSearchLoading ? (
+                    <SmartLoader showAfter={0} />
+                ) : (
+                    <>
+                        <DeleteAlert
+                            isDeleteDialog={isDeleteDialog}
+                            entry={entry}
+                            entities={entities}
+                            isError={isError}
+                        />
+                        {!showDirectionControl && !isDeleteDialog && (
+                            <Divider className={b('divider')} />
+                        )}
+                        {showDirectionControl && (
+                            <DirectionControl
+                                currentDirection={currentDirection}
+                                onUpdate={onDirectionChange}
+                            />
+                        )}
+                        <Relations
+                            isError={isError}
+                            entities={entities}
+                            currentDirection={currentDirection}
+                            isDeleteDialog={isDeleteDialog}
+                            isLoading={isLoading}
+                            isSearchLoading={isSearchLoading}
+                            onSearch={onSearch}
+                            showDirectionControl={showDirectionControl}
+                            searchValue={searchValue}
+                            entry={entry}
+                            fetchEntityBindings={fetchEntityBindings}
+                        />
+                    </>
                 )}
-                {renderRelations()}
+                {isDeleteDialog && (
+                    <SharedBindingsFooter
+                        onClose={onClose}
+                        entry={entry}
+                        onRefresh={() => fetchEntityBindings(searchValue)}
+                        isLoading={isLoading || isSearchLoading}
+                        emptyList={entities.length === 0}
+                        onDeleteSuccess={onDeleteSuccess}
+                    />
+                )}
             </Dialog.Body>
         </Dialog>
     );
---
+++ src/ui/components/DialogSharedEntryBindings/components/Relations.tsx
@@ -0,0 +1,195 @@
+import React from 'react';
+
+import {Button} from '@gravity-ui/uikit';
+import block from 'bem-cn-lite';
+import {useDispatch} from 'react-redux';
+import {CollectionItemEntities} from 'shared';
+import type {SharedEntryBindingsItem} from 'shared/schema';
+import {getSdk} from 'ui/libs/schematic-sdk';
+import type {AppDispatch} from 'ui/store';
+import {closeDialog, openDialog} from 'ui/store/actions/dialog';
+import {showToast} from 'ui/store/actions/toaster';
+import {getSharedEntryMockText} from 'ui/units/collections/components/helpers';
+
+import {DIALOG_SHARED_ENTRY_PERMISSIONS} from '../../DialogSharedEntryPermissions/DialogSharedEntryPermissions';
+import {DIALOG_SHARED_ENTRY_UNBIND} from '../../DialogSharedEntryUnbind/DialogSharedEntryUnbind';
+import {PlaceholderIllustration} from '../../PlaceholderIllustration/PlaceholderIllustration';
+import {SharedBindingsList} from '../../SharedBindingsList/SharedBindingsList';
+import type {AttachmentValue} from '../constants';
+import {DialogClassName, ObjectsListTitles} from '../constants';
+import type {SharedEntry} from '../types';
+import {getIsRelationUnbind} from '../utils';
+
+type RelationsProps = {
+    isError: boolean;
+    isDeleteDialog: boolean;
+    entities: SharedEntryBindingsItem[];
+    entry: SharedEntry;
+    currentDirection: AttachmentValue;
+    onSearch: (value: string) => void;
+    searchValue: string;
+    isLoading: boolean;
+    isSearchLoading: boolean;
+    showDirectionControl: boolean;
+    fetchEntityBindings: (filter?: string) => void;
+};
+
+const b = block(DialogClassName);
+
+export const Relations = ({
+    isError,
+    isDeleteDialog,
+    entities,
+    entry,
+    currentDirection,
+    onSearch,
+    searchValue,
+    isSearchLoading,
+    isLoading,
+    showDirectionControl,
+    fetchEntityBindings,
+}: RelationsProps) => {
+    const dispatch: AppDispatch = useDispatch();
+
+    const showErrorToast = React.useCallback(
+        (error) => {
+            dispatch(
+                showToast({
+                    title: error.message,
+                    error,
+                }),
+            );
+        },
+        [dispatch],
+    );
+
+    const getListItemActions = React.useCallback(
+        (item: SharedEntryBindingsItem) => {
+            return [
+                {
+                    text: getSharedEntryMockText('shared-bindings-list-action-unbind'),
+                    action: () =>
+                        dispatch(
+                            openDialog({
+                                id: DIALOG_SHARED_ENTRY_UNBIND,
+                                props: {
+                                    entry: getIsRelationUnbind(currentDirection, item)
+                                        ? item
+                                        : entry,
+                                    onClose: () => dispatch(closeDialog()),
+                                    onApply: async () => {
+                                        try {
+                                            await getSdk().sdk.us.deleteSharedEntryBinding({
+                                                sourceId: entry.entryId,
+                                                targetId:
+                                                    item.entity === CollectionItemEntities.WORKBOOK
+                                                        ? item.workbookId!
+                                                        : item.entryId,
+                                            });
+                                            dispatch(closeDialog());
+                                            fetchEntityBindings(isDeleteDialog ? '' : searchValue);
+                                        } catch (error) {
+                                            showErrorToast(error);
+                                        }
+                                    },
+                                    open: true,
+                                    relation: getIsRelationUnbind(currentDirection, item)
+                                        ? undefined
+                                        : item,
+                                },
+                            }),
+                        ),
+                },
+                {
+                    text: getSharedEntryMockText('shared-bindings-list-action-change-permissions'),
+                    action: () =>
+                        dispatch(
+                            openDialog({
+                                id: DIALOG_SHARED_ENTRY_PERMISSIONS,
+                                props: {
+                                    entry,
+                                    onClose: () => dispatch(closeDialog()),
+                                    onApply: async (delegate) => {
+                                        try {
+                                            await getSdk().sdk.us.updateSharedEntryBinding({
+                                                sourceId: entry.entryId,
+                                                targetId:
+                                                    item.entity === CollectionItemEntities.WORKBOOK
+                                                        ? item.workbookId!
+                                                        : item.entryId,
+                                                delegation: delegate,
+                                            });
+                                            dispatch(closeDialog());
+                                            fetchEntityBindings(searchValue);
+                                        } catch (error) {
+                                            showErrorToast(error);
+                                        }
+                                    },
+                                    open: true,
+                                    delegation: item.isDelegated,
+                                    relation: item,
+                                },
+                            }),
+                        ),
+                },
+            ];
+        },
+        [
+            currentDirection,
+            dispatch,
+            entry,
+            fetchEntityBindings,
+            showErrorToast,
+            searchValue,
+            isDeleteDialog,
+        ],
+    );
+
+    if (isError) {
+        const renderRetryAction = () => (
+            <Button
+                className={b('button-retry')}
+                size="l"
+                view="action"
+                onClick={() => fetchEntityBindings(searchValue)}
+            >
+                {getSharedEntryMockText('bindings-dialog-retry-btn')}
+            </Button>
+        );
+
+        return (
+            <div className={b('error-state')}>
+                <PlaceholderIllustration
+                    direction="column"
+                    name="error"
+                    title={getSharedEntryMockText('bindings-dialog-error')}
+                    renderAction={renderRetryAction}
+                />
+            </div>
+        );
+    }
+
+    if (isDeleteDialog && entities.length === 0) {
+        return null;
+    }
+
+    return (
+        <SharedBindingsList
+            entities={entities}
+            searchProps={{
+                value: searchValue,
+                onUpdate: onSearch,
+                placeholder: getSharedEntryMockText('entries-list-search-placeholder'),
+                disabled: isLoading || isError,
+                loading: isSearchLoading,
+            }}
+            title={
+                showDirectionControl
+                    ? getSharedEntryMockText(ObjectsListTitles[currentDirection])
+                    : undefined
+            }
+            getListItemActions={getListItemActions}
+            isLoading={isLoading}
+        />
+    );
+};
