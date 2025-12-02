cc8e1cc3432ab9f0c4eef8f12a9f15dedfa95a35
Irina V. Kuzmina <kuzmadom@yandex-team.ru>
2025-12-02 15:24:39 +0300

    Add test for custom color palette in dataset fields

--- tests/constants/test-entities/datasets.ts
+++ tests/constants/test-entities/datasets.ts
@@ -1,4 +1,4 @@
 export const DatasetsEntities = {
-    Basic: {url: '/v9ign8imt0qkk', name: 'Dataset'},
+    Basic: {id: 'v9ign8imt0qkk', url: '/v9ign8imt0qkk', name: 'Dataset'},
     Orders: {url: '/wiuimfkr0lful', name: 'Orders'},
 };
