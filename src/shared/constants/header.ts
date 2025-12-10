b8d680410b6b6c9b5c55e2ce3e1a025132ea8cbb
Vladimir Stepanenko <endurance@yandex-team.ru>
2025-12-10 17:43:51 +0300

    Add audit header to getDashboard

--- src/shared/constants/header.ts
+++ src/shared/constants/header.ts
@@ -42,5 +42,6 @@ export const SERVICE_USER_ACCESS_TOKEN_HEADER = 'x-dl-service-user-access-token'
 export const CSP_HEADER = 'content-security-policy';
 export const CSP_REPORT_TO_HEADER = 'report-to';
 export const SET_COOKIE_HEADER = 'set-cookie';
+export const AUDIT_MODE_HEADER = 'x-dl-audit-mode-enabled';
 
 export const SCR_USER_AGENT_HEADER_VALUE = 'StatScreenshooter';
