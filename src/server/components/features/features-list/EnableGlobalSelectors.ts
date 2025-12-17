942b27f2ebe7ec9d207e8d845331bac3cdffb819
Taya Leutina <leutinatasya@yandex-team.ru>
2025-12-17 11:43:14 +0300

    Disable flag

--- src/server/components/features/features-list/EnableGlobalSelectors.ts
+++ src/server/components/features/features-list/EnableGlobalSelectors.ts
@@ -4,8 +4,7 @@ import {createFeatureConfig} from '../utils';
 export default createFeatureConfig({
     name: Feature.EnableGlobalSelectors,
     state: {
-        // TODO: TURN TO FALSE
-        development: true,
-        production: true,
+        development: false,
+        production: false,
     },
 });
