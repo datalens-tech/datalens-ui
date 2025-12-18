48811923cea6bdbda0f27dfa503a592998809994
Taya Leutina <leutinatasya@yandex-team.ru>
2025-12-18 23:16:19 +0300

    Fix group setting default impact type

--- src/ui/components/ControlComponents/Sections/CommonSettingsSection/ImpactTypeSelect/ImpactTypeSelect.tsx
+++ src/ui/components/ControlComponents/Sections/CommonSettingsSection/ImpactTypeSelect/ImpactTypeSelect.tsx
@@ -104,6 +104,7 @@ export const ImpactTypeSelect = ({
     const currentImpactType = getImpactTypeByValue({
         selectorImpactType: isGroupSettings ? groupImpactType : selectorDialog.impactType,
         hasMultipleSelectors,
+        isGroupSettings,
     });
 
     // Create options based on whether there are multiple selectors
