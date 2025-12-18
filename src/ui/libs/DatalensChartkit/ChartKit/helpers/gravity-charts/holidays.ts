58fe3e2c7e43c13c9be8affabd7dd466ecafd2e9
Irina Kuzmina <kuzmadom@yandex-team.ru>
2025-12-18 17:18:20 +0300

    fix

--- src/ui/libs/DatalensChartkit/ChartKit/helpers/gravity-charts/holidays.ts
+++ src/ui/libs/DatalensChartkit/ChartKit/helpers/gravity-charts/holidays.ts
@@ -1,49 +1,53 @@
-import {AxisPlotBand, ChartData} from '@gravity-ui/chartkit/gravity-charts';
+import type {AxisPlotBand, ChartData} from '@gravity-ui/chartkit/gravity-charts';
 import {dateTimeUtc} from '@gravity-ui/date-utils';
-
-import {ChartKitHolidays} from 'ui/store/toolkit/chartkit/types';
-
-import {ChartKitAdapterProps} from '../../types';
-import min from 'lodash/min';
+import get from 'lodash/get';
 import max from 'lodash/max';
-import {get} from 'lodash';
+import min from 'lodash/min';
+import type {ChartKitHolidays} from 'ui/store/toolkit/chartkit/types';
 
-export function convertHolidaysToPlotBands({holidays, region = 'ru', loadedData}: {
-	holidays: ChartKitHolidays | undefined; 
-	region?: string;
-	loadedData: ChartKitAdapterProps['loadedData'];
+import type {ChartKitAdapterProps} from '../../types';
+
+export function convertHolidaysToPlotBands({
+    holidays,
+    region = 'ru',
+    loadedData,
+}: {
+    holidays: ChartKitHolidays | undefined;
+    region?: string;
+    loadedData: ChartKitAdapterProps['loadedData'];
 }): AxisPlotBand[] {
-	const plotBands: AxisPlotBand[] = [];
+    const plotBands: AxisPlotBand[] = [];
 
-	console.log({loadedData});
-	const chartData = loadedData?.data as ChartData;
-	if (chartData?.xAxis?.type !== 'datetime') {
-		return [];
-	}
+    const chartData = loadedData?.data as ChartData;
+    if (chartData?.xAxis?.type !== 'datetime') {
+        return [];
+    }
 
-	const chartSeriesDates = chartData.series.data.map(s => s.data.map(d => get(d, 'x'))).flat(2);
-	const minDate = min(chartSeriesDates);
-	const maxDate = max(chartSeriesDates);
+    const chartSeriesDates = chartData.series.data
+        .map((s) => s.data.map((d) => get(d, 'x')))
+        .flat(2);
+    const minDate = min(chartSeriesDates);
+    const maxDate = max(chartSeriesDates);
 
-	const items = {...holidays?.weekend[region], ...holidays?.holiday[region]};
+    const items = {...holidays?.weekend[region], ...holidays?.holiday[region]};
 
-	Object.keys(items).forEach(item => {
-		const weekendDate = dateTimeUtc({input: item, format: 'YYYYMMDD'});
-		const from = weekendDate.valueOf();
-		const to = weekendDate.add(1, 'day').valueOf();
+    Object.keys(items).forEach((item) => {
+        const weekendDate = dateTimeUtc({input: item, format: 'YYYYMMDD'});
+        const from = weekendDate.valueOf();
+        const to = weekendDate.add(1, 'day').valueOf();
 
-		if (from > maxDate || to < minDate) {
-			return;
-		}
+        if (from > maxDate || to < minDate) {
+            return;
+        }
 
-		const plotBand: AxisPlotBand = {
-			from,
-			to,
-			color: 'var(--g-color-base-generic)',
-			layerPlacement: 'before',
-		};
-		plotBands.push(plotBand);
-	});
+        const plotBand: AxisPlotBand = {
+            from,
+            to,
+            color: 'var(--g-color-base-generic)',
+            layerPlacement: 'before',
+        };
+        plotBands.push(plotBand);
+    });
 
-	return plotBands;
+    return plotBands;
 }
