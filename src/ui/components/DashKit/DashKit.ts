5534c115f56b10a352764341e058f25f42331322
Daria Larionova <darialari@yandex-team.ru>
2025-12-19 14:51:01 +0300

    Add bg-compatibility to types of getConfiguredDashKit (#3313)

--- src/ui/components/DashKit/DashKit.ts
+++ src/ui/components/DashKit/DashKit.ts
@@ -59,7 +59,8 @@ export const getConfiguredDashKit = (
         disableTitleHints?: boolean;
         scope?: string;
         globalWidgetSettings?: CommonGlobalWidgetSettings;
-    },
+        // TODO: Remove later
+    } & CommonGlobalWidgetSettings,
     shouldReconfigure?: boolean,
 ) => {
     if (currentDefaultsGetter !== pluginDefaultsGetter || !isConfigured || shouldReconfigure) {
