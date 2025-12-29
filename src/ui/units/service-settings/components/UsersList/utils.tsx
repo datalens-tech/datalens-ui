026bdab4fc9b1bd46c6f7af5d9b5d256d7a7916f
Aliya Safina <safina-aliia@yandex-team.ru>
2025-12-29 17:55:05 +0300

    New design for service settings page (#3316)

--- src/ui/units/service-settings/components/UsersList/utils.tsx
+++ src/ui/units/service-settings/components/UsersList/utils.tsx
@@ -1,42 +1,85 @@
 import React from 'react';
 
-import type {TableColumnConfig} from '@gravity-ui/uikit';
+import {type TableColumnConfig, User} from '@gravity-ui/uikit';
+import block from 'bem-cn-lite';
 import {I18n} from 'i18n';
+import {Feature} from 'shared';
 import type {ListUser} from 'shared/schema/auth/types/users';
+import {isEnabledFeature} from 'ui/utils/isEnabledFeature';
 
 import {LabelsList} from './LabelsList/LabelsList';
 
+import './UsersList.scss';
+
+const b = block('service-settings-users-list');
+
 const i18n = I18n.keyset('service-settings.users-list.view');
 
-export const getUsersListColumns = (): TableColumnConfig<ListUser>[] => [
-    {
-        id: 'firstName',
-        name: i18n('label_field-first-name'),
-    },
-    {
-        id: 'lastName',
-        name: i18n('label_field-last-name'),
-    },
-    {
-        id: 'userId',
-        name: i18n('label_field-id'),
-        template: ({userId}) => userId,
-        meta: {copy: ({userId}: ListUser) => userId},
-    },
-    {
-        id: 'email',
-        name: i18n('label_field-email'),
-        template: ({email}) => email,
-        meta: {copy: ({email}: ListUser) => email},
-    },
-    {
-        id: 'role',
-        name: i18n('label_field-roles'),
-        template: ({roles}) => <LabelsList items={roles} countVisibleElements={1} />,
-    },
-    {
-        id: 'login',
-        name: i18n('label_field-login'),
-        meta: {copy: ({login}: ListUser) => login},
-    },
-];
+const newServiceSettingsEnabled = isEnabledFeature(Feature.EnableNewServiceSettings);
+
+export const getUsersListColumns = (): TableColumnConfig<ListUser>[] =>
+    newServiceSettingsEnabled
+        ? [
+              {
+                  id: 'user',
+                  // TODO: name: i18n('user'),
+                  name: 'Пользователь',
+                  template: ({firstName, lastName, email}) => (
+                      <User
+                          className={b('user')}
+                          size="m"
+                          name={`${firstName ?? ''} ${lastName ?? ''}`.trim() || '—'}
+                          description={email}
+                      />
+                  ),
+                  width: '35%',
+              },
+              {
+                  id: 'role',
+                  name: i18n('label_field-roles'),
+                  template: ({roles}) => <LabelsList items={roles} countVisibleElements={1} />,
+              },
+              {
+                  id: 'login',
+                  name: i18n('label_field-login'),
+                  template: ({login}) => <div className={b('login')}>{login}</div>,
+                  meta: {copy: ({login}: ListUser) => login},
+              },
+              {
+                  id: 'userId',
+                  name: i18n('label_field-id'),
+                  meta: {copy: ({userId}: ListUser) => userId},
+              },
+          ]
+        : [
+              {
+                  id: 'firstName',
+                  name: i18n('label_field-first-name'),
+              },
+              {
+                  id: 'lastName',
+                  name: i18n('label_field-last-name'),
+              },
+              {
+                  id: 'userId',
+                  name: i18n('label_field-id'),
+                  template: ({userId}) => userId,
+                  meta: {copy: ({userId}: ListUser) => userId},
+              },
+              {
+                  id: 'email',
+                  name: i18n('label_field-email'),
+                  template: ({email}) => email,
+                  meta: {copy: ({email}: ListUser) => email},
+              },
+              {
+                  id: 'role',
+                  name: i18n('label_field-roles'),
+                  template: ({roles}) => <LabelsList items={roles} countVisibleElements={1} />,
+              },
+              {
+                  id: 'login',
+                  name: i18n('label_field-login'),
+                  meta: {copy: ({login}: ListUser) => login},
+              },
+          ];
