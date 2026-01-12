392d34851b857dcbf5a4b59a73589700af6613cc
Daniil Filippov <danilfilippov@yandex-team.ru>
2026-01-12 18:48:20 +0300

    Refactor useHistory to useEditHisotry

---
+++ src/ui/hooks/useEditHistoryActions.ts
@@ -0,0 +1,100 @@
+import React from 'react';
+
+import {ArrowUturnCcwLeft, ArrowUturnCwRight} from '@gravity-ui/icons';
+import {I18n} from 'i18n';
+import {useDispatch, useSelector} from 'react-redux';
+import type {AdditionalButtonTemplate} from 'ui/components/ActionPanel/components/ChartSaveControls/types';
+import {useAdditionalItems} from 'ui/components/ActionPanel/components/ChartSaveControls/useAdditionalItems';
+import {REDO_HOTKEY, UNDO_HOTKEY} from 'ui/constants/misc';
+import {useBindHotkey} from 'ui/hooks/useBindHotkey';
+import type {DatalensGlobalState} from 'ui/index';
+import {goBack, goForward} from 'ui/store/actions/editHistory';
+import {selectCanGoBack, selectCanGoForward} from 'ui/store/selectors/editHistory';
+
+export interface UseEditHistoryActionsOptions {
+    unitId: string;
+    hotkeyScope: string;
+    canGoBackSelector?: (state: DatalensGlobalState) => boolean;
+    canGoForwardSelector?: (state: DatalensGlobalState) => boolean;
+    iconSize?: number;
+}
+
+const ACTION_PANEL_ICON_SIZE = 16;
+const i18n = I18n.keyset('component.action-panel.view');
+
+export function useEditHistoryActions(options: UseEditHistoryActionsOptions) {
+    const {
+        unitId,
+        hotkeyScope,
+        canGoBackSelector: customCanGoBackSelector,
+        canGoForwardSelector: customCanGoForwardSelector,
+        iconSize = ACTION_PANEL_ICON_SIZE,
+    } = options;
+
+    const dispatch = useDispatch();
+
+    // Default selectors
+    const defaultCanGoBackSelector = React.useCallback(
+        (state: DatalensGlobalState) => selectCanGoBack(state, {unitId}),
+        [unitId],
+    );
+
+    const defaultCanGoForwardSelector = React.useCallback(
+        (state: DatalensGlobalState) => selectCanGoForward(state, {unitId}),
+        [unitId],
+    );
+
+    const canGoBack = useSelector(customCanGoBackSelector || defaultCanGoBackSelector);
+    const canGoForward = useSelector(customCanGoForwardSelector || defaultCanGoForwardSelector);
+
+    const handleGoBack = React.useCallback(() => {
+        if (canGoBack) {
+            dispatch(goBack({unitId}));
+        }
+    }, [canGoBack, dispatch, unitId]);
+
+    const handleGoForward = React.useCallback(() => {
+        if (canGoForward) {
+            dispatch(goForward({unitId}));
+        }
+    }, [canGoForward, dispatch, unitId]);
+
+    const items: AdditionalButtonTemplate[] = React.useMemo(() => {
+        return [
+            {
+                key: 'undo',
+                action: handleGoBack,
+                icon: {data: ArrowUturnCcwLeft, size: iconSize},
+                view: 'flat',
+                disabled: !canGoBack,
+                title: i18n('button_undo'),
+                hotkey: UNDO_HOTKEY.join('+'),
+            },
+            {
+                key: 'redo',
+                action: handleGoForward,
+                icon: {data: ArrowUturnCwRight, size: iconSize},
+                view: 'flat',
+                disabled: !canGoForward,
+                title: i18n('button_redo'),
+                hotkey: REDO_HOTKEY.join('+'),
+            },
+        ];
+    }, [canGoBack, canGoForward, handleGoBack, handleGoForward, iconSize]);
+
+    const actions = useAdditionalItems({items});
+
+    useBindHotkey({
+        key: UNDO_HOTKEY,
+        handler: handleGoBack,
+        options: {enabled: canGoBack, scopes: hotkeyScope},
+    });
+
+    useBindHotkey({
+        key: REDO_HOTKEY,
+        handler: handleGoForward,
+        options: {enabled: canGoForward, scopes: hotkeyScope},
+    });
+
+    return actions;
+}
--- src/ui/units/datasets/containers/Dataset/useHistoryActions.tsx
+++ src/ui/units/datasets/containers/Dataset/useHistoryActions.tsx
@@ -1,14 +1,6 @@
-import React from 'react';
-
-import {ArrowUturnCcwLeft, ArrowUturnCwRight} from '@gravity-ui/icons';
-import {i18n} from 'i18n';
-import {useDispatch, useSelector} from 'react-redux';
-import type {AdditionalButtonTemplate} from 'ui/components/ActionPanel/components/ChartSaveControls/types';
-import {useAdditionalItems} from 'ui/components/ActionPanel/components/ChartSaveControls/useAdditionalItems';
-import {HOTKEYS_SCOPES, REDO_HOTKEY, UNDO_HOTKEY} from 'ui/constants/misc';
-import {useBindHotkey} from 'ui/hooks/useBindHotkey';
+import {HOTKEYS_SCOPES} from 'ui/constants/misc';
+import {useEditHistoryActions} from 'ui/hooks/useEditHistoryActions';
 import type {DatalensGlobalState} from 'ui/index';
-import {goBack, goForward} from 'ui/store/actions/editHistory';
 import {selectCanGoBack, selectCanGoForward} from 'ui/store/selectors/editHistory';
 
 import {DATASETS_EDIT_HISTORY_UNIT_ID} from '../../constants';
@@ -38,56 +30,11 @@ const canGoForwardSelector = (state: DatalensGlobalState) => {
 };
 
 export function useHistoryActions() {
-    const dispatch = useDispatch();
-    const canGoBack = useSelector(canGoBackSelector);
-    const canGoForward = useSelector(canGoForwardSelector);
-
-    const handleGoBack = React.useCallback(() => {
-        if (canGoBack) {
-            dispatch(goBack({unitId: DATASETS_EDIT_HISTORY_UNIT_ID}));
-        }
-    }, [canGoBack, dispatch]);
-
-    const handleGoForward = React.useCallback(() => {
-        if (canGoForward) {
-            dispatch(goForward({unitId: DATASETS_EDIT_HISTORY_UNIT_ID}));
-        }
-    }, [canGoForward, dispatch]);
-
-    const items: AdditionalButtonTemplate[] = React.useMemo(() => {
-        return [
-            {
-                key: 'undo',
-                action: handleGoBack,
-                icon: {data: ArrowUturnCcwLeft, size: ACTION_PANEL_ICON_SIZE},
-                view: 'flat',
-                disabled: !canGoBack,
-                title: i18n('component.action-panel.view', 'button_undo'),
-                hotkey: UNDO_HOTKEY.join('+'),
-            },
-            {
-                key: 'redo',
-                action: handleGoForward,
-                icon: {data: ArrowUturnCwRight, size: ACTION_PANEL_ICON_SIZE},
-                view: 'flat',
-                disabled: !canGoForward,
-                title: i18n('component.action-panel.view', 'button_redo'),
-                hotkey: REDO_HOTKEY.join('+'),
-            },
-        ];
-    }, [canGoBack, canGoForward, handleGoBack, handleGoForward]);
-    const actions = useAdditionalItems({items});
-
-    useBindHotkey({
-        key: UNDO_HOTKEY,
-        handler: handleGoBack,
-        options: {enabled: canGoBack, scopes: HOTKEYS_SCOPES.DATASETS},
+    return useEditHistoryActions({
+        unitId: DATASETS_EDIT_HISTORY_UNIT_ID,
+        hotkeyScope: HOTKEYS_SCOPES.DATASETS,
+        canGoBackSelector,
+        canGoForwardSelector,
+        iconSize: ACTION_PANEL_ICON_SIZE,
     });
-    useBindHotkey({
-        key: REDO_HOTKEY,
-        handler: handleGoForward,
-        options: {enabled: canGoForward, scopes: HOTKEYS_SCOPES.DATASETS},
-    });
-
-    return actions;
 }
