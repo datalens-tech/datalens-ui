7fa76730936fb19b4b1f1de9ddb40ce3a90514fc
dvortsovs <dvortsovs@yandex-team.ru>
2026-01-15 12:22:02 +0300

    add shared entries creation test

--- src/shared/constants/qa/collections.ts
+++ src/shared/constants/qa/collections.ts
@@ -1,3 +1,15 @@
+export const CollectionActionsQa = {
+    CreateActionBtn: 'coll-actions-create-btn',
+    SharedObjectsMenuItem: 'coll-actions-shared-objects-menu-item',
+    SharedConnectionCreateBtn: 'coll-actions-shared-conn-create-btn',
+    SharedDatasetCreateBtn: 'coll-actions-shared-dataset-create-btn',
+} as const;
+
+export const CollectionTableRowQa = {
+    EntryDropdownBtn: 'coll-content-table-entry-dropdown-btn',
+    EntryDropdownDeleteBtn: 'coll-content-table-entry-dropdown-delete-btn',
+} as const;
+
 export const enum CollectionContentTableQa {
     Table = 'coll-content-table',
     CollectionLinkRow = 'coll-content-table-coll-link-row',
--- tests/page-objects/collections/CollectionsPage.ts
+++ tests/page-objects/collections/CollectionsPage.ts
@@ -1,7 +1,9 @@
-import {BasePage} from '../BasePage';
+import {CollectionActionsQa, EntryScope} from '../../../src/shared';
 import type {BasePageProps} from '../BasePage';
+import {BasePage} from '../BasePage';
 import {ContentTablePO} from './ContentTable';
 import {EmptyStatePO} from './EmptyState';
+import {slct} from '../../utils';
 
 type CollectionsPageProps = BasePageProps;
 
@@ -10,6 +12,34 @@ export class CollectionsPagePO extends BasePage {
 
     constructor({page}: CollectionsPageProps) {
         super({page});
+    }
+
+    async createSharedEntry({scope}: {scope: EntryScope.Dataset | EntryScope.Connection}) {
+        const createBtn = await this.page.waitForSelector(
+            slct(CollectionActionsQa.CreateActionBtn),
+        );
+        await createBtn.click();
+        const sharedObjectsMenuItem = this.page.locator(
+            slct(CollectionActionsQa.SharedObjectsMenuItem),
+        );
+        await sharedObjectsMenuItem.hover();
+
+        switch (scope) {
+            case EntryScope.Connection: {
+                const sharedConnectionCreateBtn = this.page.locator(
+                    slct(CollectionActionsQa.SharedConnectionCreateBtn),
+                );
+                await sharedConnectionCreateBtn.click();
+                break;
+            }
+            case EntryScope.Dataset: {
+                const sharedDatasetCreateBtn = this.page.locator(
+                    slct(CollectionActionsQa.SharedDatasetCreateBtn),
+                );
+                await sharedDatasetCreateBtn.click();
+                break;
+            }
+        }
     }
 
     get emptyState() {
