eee76ae4fdd446a63b142ed3c0a4d9b45d9bcd8d
Irina Kuzmina <kuzmadom@yandex-team.ru>
2026-01-26 15:45:50 +0300

    Fix empty category value for bar-y chart (#3437)

--- src/server/modes/charts/plugins/datalens/utils/misc-helpers.ts
+++ src/server/modes/charts/plugins/datalens/utils/misc-helpers.ts
@@ -433,7 +433,13 @@ export function getCategoryFormatter(args: {
         return (value: unknown) => wrapHtml(String(value));
     }
 
-    return (value: string | number) => String(value);
+    return (value: string | number) => {
+        if (value === '') {
+            return 'Null';
+        }
+
+        return String(value);
+    };
 }
 
 export function getSeriesTitleFormatter(args: {fields: (ServerField | undefined)[]}) {
