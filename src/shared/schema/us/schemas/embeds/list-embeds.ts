910c9b592ce784e9c1f29fe8f748ed1cea2f59c7
Vladimir Stepanenko 🏴‍☠️ <endurance@yandex-team.ru>
2026-02-16 22:34:52 +0300

    Add embeds endpoints descriptions (#3488)

--- src/shared/schema/us/schemas/embeds/list-embeds.ts
+++ src/shared/schema/us/schemas/embeds/list-embeds.ts
@@ -3,7 +3,7 @@ import z from 'zod';
 import {embedSchema} from './embed';
 
 export const listEmbedsArgsSchema = z.object({
-    entryId: z.string(),
+    entryId: z.string().describe('ID of the entry to list embeddings for.'),
 });
 
-export const listEmbedsResultSchema = z.array(embedSchema);
+export const listEmbedsResultSchema = z.array(embedSchema).describe('Array of embeddings.');
