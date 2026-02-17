6c0dc513f07c485f1aa69e87219a485d4f673ff8
Mr.Dr.Professor Patrick <alaev89@yandex.ru>
2026-02-17 13:38:54 +0100

    Fix gravity-charts bar-x labels formatting (#3532)
    Co-authored-by: Evgeny Alaev <alaev89@qavm-14443e8d.qemu>

---
+++ tests/opensource-suites/wizard/visualizations/bar-x/data-labels.test.ts
@@ -0,0 +1,123 @@
+import {expect} from '@playwright/test';
+
+import datalensTest from '../../../../utils/playwright/globalTestDefinition';
+import {openTestPage, slct} from '../../../../utils';
+import {
+    ChartKitQa,
+    DialogFieldLabelModeValuesQa,
+    DialogFieldMainSectionQa,
+    WizardPageQa,
+    WizardVisualizationId,
+} from '../../../../../src/shared';
+import WizardPage from '../../../../page-objects/wizard/WizardPage';
+import {PlaceholderName} from '../../../../page-objects/wizard/SectionVisualization';
+import {SMALL_SCREENSHOT_VIEWPORT_SIZE} from '../constants';
+
+datalensTest.describe('Wizard', () => {
+    datalensTest.describe('Bar-x chart', () => {
+        datalensTest.beforeEach(async ({page, config}) => {
+            await openTestPage(page, config.wizard.urls.WizardBasicDataset);
+            await page.setViewportSize(SMALL_SCREENSHOT_VIEWPORT_SIZE);
+            const wizardPage = new WizardPage({page});
+            await wizardPage.setVisualization(WizardVisualizationId.Column);
+        });
+
+        datalensTest('String dataLabels @screenshot', async ({page}) => {
+            const wizardPage = new WizardPage({page});
+            const preview = page.locator(slct(WizardPageQa.SectionPreview));
+            await wizardPage.sectionVisualization.addFieldByClick(PlaceholderName.X, 'segment');
+            await wizardPage.createNewFieldWithFormula('SalesSum', 'sum([Sales])');
+            await wizardPage.sectionVisualization.addFieldByClick(PlaceholderName.Y, 'SalesSum');
+            await wizardPage.sectionVisualization.addFieldByClick(
+                PlaceholderName.Labels,
+                'SalesSum',
+            );
+            // Put the mouse away so that the presence of hover elements does not interfere with taking screenshots
+            await page.mouse.move(-1, -1);
+            await expect(preview.locator(slct(ChartKitQa.Loader))).not.toBeVisible();
+            await expect(preview).toHaveScreenshot();
+        });
+
+        datalensTest('Markup dataLabels @screenshot', async ({page}) => {
+            const wizardPage = new WizardPage({page});
+            const preview = page.locator(slct(WizardPageQa.SectionPreview));
+            await wizardPage.sectionVisualization.addFieldByClick(PlaceholderName.X, 'segment');
+            await wizardPage.createNewFieldWithFormula('SalesSum', 'sum([Sales])');
+            await wizardPage.sectionVisualization.addFieldByClick(PlaceholderName.Y, 'SalesSum');
+            await wizardPage.createNewFieldWithFormula(
+                'label',
+                "color(str(int([SalesSum])), 'var(--g-color-text-danger)')",
+            );
+            await wizardPage.sectionVisualization.addFieldByClick(PlaceholderName.Labels, 'label');
+            // Put the mouse away so that the presence of hover elements does not interfere with taking screenshots
+            await page.mouse.move(-1, -1);
+            await expect(preview.locator(slct(ChartKitQa.Loader))).not.toBeVisible();
+            await expect(preview).toHaveScreenshot();
+        });
+
+        datalensTest('Datetime dataLabels @screenshot', async ({page}) => {
+            const wizardPage = new WizardPage({page});
+            const preview = page.locator(slct(WizardPageQa.SectionPreview));
+            await wizardPage.sectionVisualization.addFieldByClick(PlaceholderName.X, 'segment');
+            await wizardPage.createNewFieldWithFormula('SalesSum', 'sum([Sales])');
+            await wizardPage.sectionVisualization.addFieldByClick(PlaceholderName.Y, 'SalesSum');
+            await wizardPage.createNewFieldWithFormula('label', 'MAX([Order_date])');
+            await wizardPage.sectionVisualization.addFieldByClick(PlaceholderName.Labels, 'label');
+            // Put the mouse away so that the presence of hover elements does not interfere with taking screenshots
+            await page.mouse.move(-1, -1);
+            await expect(preview.locator(slct(ChartKitQa.Loader))).not.toBeVisible();
+            await expect(preview).toHaveScreenshot();
+        });
+
+        datalensTest('MeasureValues as dataLabels @screenshot', async ({page}) => {
+            const wizardPage = new WizardPage({page});
+            const preview = page.locator(slct(WizardPageQa.SectionPreview));
+            await wizardPage.createNewFieldWithFormula('SalesSum', 'sum([Sales])');
+            await wizardPage.sectionVisualization.addFieldByClick(PlaceholderName.Y, 'SalesSum');
+            await wizardPage.createNewFieldWithFormula('orderCount', 'count([order_id])');
+            await wizardPage.sectionVisualization.addFieldByClick(PlaceholderName.X, 'orderCount');
+            await wizardPage.sectionVisualization.addFieldByClick(
+                PlaceholderName.Labels,
+                'Measure Values',
+            );
+            // Put the mouse away so that the presence of hover elements does not interfere with taking screenshots
+            await page.mouse.move(-1, -1);
+            await expect(preview.locator(slct(ChartKitQa.Loader))).not.toBeVisible();
+            await expect(preview).toHaveScreenshot();
+        });
+    });
+
+    datalensTest.describe('Bar-x normalised chart', () => {
+        datalensTest.beforeEach(async ({page, config}) => {
+            await openTestPage(page, config.wizard.urls.WizardBasicDataset);
+            await page.setViewportSize(SMALL_SCREENSHOT_VIEWPORT_SIZE);
+            const wizardPage = new WizardPage({page});
+            await wizardPage.setVisualization(WizardVisualizationId.Column100p);
+        });
+
+        datalensTest('Percentage as labels @screenshot', async ({page}) => {
+            const wizardPage = new WizardPage({page});
+            const preview = page.locator(slct(WizardPageQa.SectionPreview));
+            await wizardPage.createNewFieldWithFormula('SalesSum', 'sum([Sales])');
+            await wizardPage.sectionVisualization.addFieldByClick(PlaceholderName.Y, 'SalesSum');
+            await wizardPage.sectionVisualization.addFieldByClick(
+                PlaceholderName.Colors,
+                'segment',
+            );
+            await wizardPage.sectionVisualization.addFieldByClick(
+                PlaceholderName.Labels,
+                'SalesSum',
+            );
+            await wizardPage.visualizationItemDialog.open(PlaceholderName.Labels, 'SalesSum');
+            await wizardPage.visualizationItemDialog.changeSelectorValue(
+                DialogFieldMainSectionQa.LabelModeSelector,
+                DialogFieldLabelModeValuesQa.Percent,
+            );
+            await wizardPage.visualizationItemDialog.clickOnApplyButton();
+            // Put the mouse away so that the presence of hover elements does not interfere with taking screenshots
+            await page.mouse.move(-1, -1);
+            await expect(preview.locator(slct(ChartKitQa.Loader))).not.toBeVisible();
+            await expect(preview).toHaveScreenshot();
+        });
+    });
+});
